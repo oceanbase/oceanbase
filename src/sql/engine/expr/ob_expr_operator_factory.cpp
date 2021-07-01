b7fe9e2eaa50eec8fa22003bd3746d3c24334d02
cbcwestwolf <1004626265@qq.com>
2021-07-01 19:09:27 +0800

    Code: Add new system function crc32()

--- src/sql/engine/expr/ob_expr_operator_factory.cpp
+++ src/sql/engine/expr/ob_expr_operator_factory.cpp
@@ -166,6 +166,7 @@
 #include "sql/engine/expr/ob_expr_reverse.h"
 #include "sql/engine/expr/ob_expr_right.h"
 #include "sql/engine/expr/ob_expr_md5.h"
+#include "sql/engine/expr/ob_expr_crc32.h"
 #include "sql/engine/expr/ob_expr_lrpad.h"
 #include "sql/engine/expr/ob_expr_conv.h"
 #include "sql/engine/expr/ob_expr_sign.h"
@@ -608,6 +609,7 @@ void ObExprOperatorFactory::register_expr_operators()
   REG_OP(ObExprIfNull);
   REG_OP(ObExprConcatWs);
   REG_OP(ObExprCmpMeta);
+  REG_OP(ObExprCrc32);
   REG_OP(ObExprQuote);
   REG_OP(ObExprPad);
   REG_OP(ObExprHostIP);
