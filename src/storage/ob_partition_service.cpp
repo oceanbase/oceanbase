ab743ff0d39a0bf1ca14488d178b6545e4736a6b
hf0 <hf0@oceanbase.com>
2021-08-11 20:30:29 +0800

    fix primary leader sync log timeout issue in max protect mode.

--- src/storage/ob_partition_service.cpp
+++ src/storage/ob_partition_service.cpp
@@ -8142,15 +8142,17 @@ int ObPartitionService::nonblock_get_strong_leader_from_loc_cache(
       int tmp_ret = OB_SUCCESS;
       const int64_t expire_renew_time = 0;
       if (OB_SUCCESS != (tmp_ret = location_cache_->nonblock_renew(pkey, expire_renew_time, cluster_id))) {
-        STORAGE_LOG(WARN, "nonblock_renew failed", K(pkey), K(tmp_ret));
+        STORAGE_LOG(WARN, "nonblock_renew failed", K(pkey), K(tmp_ret), K(cluster_id));
       } else {
-        STORAGE_LOG(DEBUG, "nonblock_renew success", K(pkey));
+        if (REACH_TIME_INTERVAL(1000 * 1000)) {
+          STORAGE_LOG(INFO, "nonblock_renew success", K(pkey), K(cluster_id));
+        }
       }
     }
     if (OB_FAIL(location_cache_->nonblock_get_strong_leader_without_renew(pkey, leader, cluster_id))) {
-      STORAGE_LOG(DEBUG, "nonblock_get_strong_leader_without_renew failed", K(pkey), K(ret));
+      STORAGE_LOG(DEBUG, "nonblock_get_strong_leader_without_renew failed", K(pkey), K(cluster_id), K(ret));
     } else {
-      STORAGE_LOG(DEBUG, "nonblock_get_leader_without_renew success", K(pkey), K(ret));
+      STORAGE_LOG(DEBUG, "nonblock_get_strong_leader_without_renew success", K(pkey), K(cluster_id), K(ret));
     }
   }
   return ret;
