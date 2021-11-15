b68eff8a02ff61513a514be5799ebf4311b7bc1c
zbf0 <zbf0@oceanbase.com>
2021-11-15 11:58:07 +0800

    placeholder for standby disconnect cluster

--- src/sql/resolver/ob_stmt_type.h
+++ src/sql/resolver/ob_stmt_type.h
@@ -248,6 +248,7 @@ OB_STMT_TYPE_DEF(T_BACKUP_BACKUPPIECE, get_sys_tenant_alter_system_priv, 261)
 OB_STMT_TYPE_DEF(T_ADD_RESTORE_SOURCE, get_sys_tenant_alter_system_priv, 262)
 OB_STMT_TYPE_DEF(T_CLEAR_RESTORE_SOURCE, get_sys_tenant_alter_system_priv, 263)
 OB_STMT_TYPE_DEF(T_SHOW_RESTORE_PREVIEW, err_stmt_type_priv, 264)
+OB_STMT_TYPE_DEF(T_DISCONNECT_CLUSTER, get_sys_tenant_super_priv, 270)
 OB_STMT_TYPE_DEF(T_MAX, err_stmt_type_priv, 500)
 #endif
 
