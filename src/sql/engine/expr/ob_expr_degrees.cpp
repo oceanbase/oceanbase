41465805e3d451a8dcb9fa5f181a3b29efa88f3b
1115839967 <1115839967@qq.com>
2021-12-06 10:24:42 +0800

    Add mulan(#665)

--- src/sql/engine/expr/ob_expr_degrees.cpp
+++ src/sql/engine/expr/ob_expr_degrees.cpp
@@ -1,6 +1,15 @@
-// Copyright (c) 2014-2021 Alibaba Inc. All Right Reserved.
-// Author:
-//  liuqifan.lqf@antgroup.com
+/**
+ * Copyright (c) 2021 OceanBase
+ * OceanBase CE is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+
 #define USING_LOG_PREFIX SQL_ENG
 #include "sql/engine/expr/ob_expr_degrees.h"
 #include "lib/number/ob_number_v2.h"
