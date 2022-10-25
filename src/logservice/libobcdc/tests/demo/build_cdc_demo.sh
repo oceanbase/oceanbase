45e229b80b177df5fe76bd2368f97e023c6ecadd
obdev <obdev@oceanbase.com>
2022-10-25 17:47:36 +0800

    [OBCDC] fix build error with OB_BUILD_CDC=ON

--- src/logservice/libobcdc/tests/demo/build_cdc_demo.sh
+++ src/logservice/libobcdc/tests/demo/build_cdc_demo.sh
@@ -3,10 +3,15 @@
 CLANG_PATH=$1/bin
 PROJECT_BUILD_DIR=$2
 
-mkdir -p `pwd`/lib/ &&
-libtool --quiet --mode=install cp $PROJECT_BUILD_DIR/src/logservice/libobcdc/src/libobcdc.so `pwd`/lib/ &&
-mkdir -p build_dir &&
+mkdir -p build_dir/lib &&
 cd build_dir &&
+libtool --quiet --mode=install cp $PROJECT_BUILD_DIR/src/logservice/libobcdc/src/libobcdc.so `pwd`/lib/
+
+if [ $# -eq 3 ]
+then
+  DEP_DIR=$3
+  libtool --quiet --mode=install cp $DEP_DIR/lib/mariadb/libmariadb.so.3 `pwd`/lib/
+fi
+
 $CLANG_PATH/clang++  ../obcdc_dlopen.cpp -o cdc_dl -ldl -std=c++11 -fpic &&
-./cdc_dl &&
-cd ../
+LD_LIBRARY_PATH=./lib ./cdc_dl
