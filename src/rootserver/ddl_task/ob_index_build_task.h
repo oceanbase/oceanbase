c537cf7dac066b959e637fbd383fd14085311d4b
YoungYang0820 <youngyang0820@gmail.com>
2022-11-17 04:05:41 +0000

    [CP] acquire snapshot for lob tablet when doing ddl

--- src/rootserver/ddl_task/ob_index_build_task.h
+++ src/rootserver/ddl_task/ob_index_build_task.h
@@ -123,6 +123,9 @@ private:
   int send_build_single_replica_request();
   int check_build_single_replica(bool &is_end);
   int check_need_verify_checksum(bool &need_verify);
+  int check_need_acquire_lob_snapshot(const ObTableSchema *data_table_schema,
+                                      const ObTableSchema *index_table_schema,
+                                      bool &need_acquire);
 private:
   static const int64_t OB_INDEX_BUILD_TASK_VERSION = 1;
   using ObDDLTask::is_inited_;
