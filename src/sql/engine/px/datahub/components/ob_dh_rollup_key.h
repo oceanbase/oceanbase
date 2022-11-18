d0cb7644575f1e3035531e92f35e26aa27f60c3f
qianchanger <qianchanger@gmail.com>
2022-11-18 08:07:57 +0000

    Fix sort impl mem leak

--- src/sql/engine/px/datahub/components/ob_dh_rollup_key.h
+++ src/sql/engine/px/datahub/components/ob_dh_rollup_key.h
@@ -94,7 +94,7 @@ public:
     : ObPieceMsgCtx(op_id, task_cnt, timeout_ts), received_(0),
                     tenant_id_(tenant_id), whole_msg_(), received_msgs_() {}
   ~ObRollupKeyPieceMsgCtx() = default;
-  void destroy()
+  virtual void destroy()
   {
     received_msgs_.reset();
   }
