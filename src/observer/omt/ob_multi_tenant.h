8dae58510b5b4c930245a3ba8b80f3f9685e803a
obdev <obdev@oceanbase.com>
2022-11-29 09:07:07 +0000

    break the indefinite retry in del_tenant when get tenant lock failed

--- src/observer/omt/ob_multi_tenant.h
+++ src/observer/omt/ob_multi_tenant.h
@@ -180,7 +180,7 @@ protected:
                                         const int64_t mem_limit,
                                         ObTenantMeta &meta);
   int create_virtual_tenants();
-  int remove_tenant(const uint64_t tenant_id);
+  int remove_tenant(const uint64_t tenant_id, bool &lock_succ);
   uint32_t get_tenant_lock_bucket_idx(const uint64_t tenant_id);
   int update_tenant_unit_no_lock(const share::ObUnitInfoGetter::ObTenantConfig &unit);
 
