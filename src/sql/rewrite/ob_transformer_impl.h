3f740377376d7e18a1bb781e6121cb3fed89a15f
obdev <obdev@oceanbase.com>
2022-11-29 12:35:42 +0000

    [CP] [CP] Add limitation for larget set stmts' cost based transformation.

--- src/sql/rewrite/ob_transformer_impl.h
+++ src/sql/rewrite/ob_transformer_impl.h
@@ -50,6 +50,7 @@ class ObTransformerImpl
 {
   static const int64_t DEFAULT_ITERATION_COUNT = 10;
   static const int64_t MAX_RULE_COUNT = 64;
+  static const uint64_t MAX_SET_STMT_SIZE_OF_COSTED_BASED_RELUES = 5;
 public:
   ObTransformerImpl(ObTransformerCtx *ctx)
     : ctx_(ctx),
@@ -146,7 +147,7 @@ public:
 private:
 
   int collect_trans_stat(const ObTransformRule &rule);
-
+  int get_stmt_trans_info(ObDMLStmt *stmt);
   void print_trans_stat();
 
   int finalize_exec_params(ObDMLStmt *stmt);
