5946224b038579cdf698e78add064ada434008ee
hiddenbomb <bwgui203@gmail.com>
2022-11-30 12:08:09 +0000

    fix migrate lob tablets issue

--- unittest/storage/test_tablet_helper.h
+++ unittest/storage/test_tablet_helper.h
@@ -45,6 +45,9 @@ int TestTabletHelper::create_tablet(ObLSTabletService &ls_tablet_svr, obrpc::ObB
   } else if (OB_FAIL(ls_tablet_svr.on_redo_create_tablets(arg, trans_flags))) {
     STORAGE_LOG(WARN, "failed to redo create tablets", K(ret), K(arg));
   } else if (FALSE_IT(trans_flags.scn_ = share::SCN::plus(trans_flags.scn_, 1))) {
+  } else if (OB_FAIL(ls_tablet_svr.on_tx_end_create_tablets(arg, trans_flags))) {
+    STORAGE_LOG(WARN, "failed to tx end create tablets", K(ret), K(arg));
+  } else if (FALSE_IT(trans_flags.scn_ = share::SCN::plus(trans_flags.scn_, 1))) {
   } else if (OB_FAIL(ls_tablet_svr.on_commit_create_tablets(arg, trans_flags))) {
     STORAGE_LOG(WARN, "failed to commit create tablets", K(ret), K(arg));
   }
