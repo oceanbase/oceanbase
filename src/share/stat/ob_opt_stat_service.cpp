ca5fd694714d57b038e1d86437dfd3778a456098
wangt1xiuyi <13547954130@163.com>
2022-12-16 15:07:40 +0000

    [CP] fix get histogram memory leak and some failed cases

--- src/share/stat/ob_opt_stat_service.cpp
+++ src/share/stat/ob_opt_stat_service.cpp
@@ -237,7 +237,10 @@ int ObOptStatService::load_column_stat_and_put_cache(const uint64_t tenant_id,
         LOG_WARN("puts column stat into cache failed.", K(ret));
       } else if (OB_FAIL(handles.push_back(handle))) {
         LOG_WARN("failed to push back", K(ret));
-      } else {/*do nothing*/}
+      } else {
+        key_column_stats.at(i).stat_->~ObOptColumnStat();
+        key_column_stats.at(i).stat_ = NULL;
+      }
     }
   }
   return ret;
