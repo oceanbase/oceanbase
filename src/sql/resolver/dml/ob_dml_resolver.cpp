5391bba6b30565b5e0c89372aae5d2e21c0eaa2a
obdev <obdev@oceanbase.com>
2023-01-11 17:09:11 +0800

    [CP] fix calc default expr of generate column core

--- src/sql/resolver/dml/ob_dml_resolver.cpp
+++ src/sql/resolver/dml/ob_dml_resolver.cpp
@@ -581,6 +581,7 @@ int ObDMLResolver::resolve_basic_column_item(const TableItem& table_item, const 
       ObString col_def;
       ObRawExpr* ref_expr = NULL;
       if (col_schema->is_generated_column()) {
+        column_item.set_default_value(ObObj()); // set null to generated default value
         if (OB_FAIL(col_schema->get_cur_default_value().get_string(col_def))) {
           LOG_WARN("get generated column definition failed", K(ret), K(*col_schema));
         } else if (OB_FAIL(resolve_generated_column_expr(
