716369aa222991d49c24514da567a10930f4f186
godyangfight <godyangfight@gmail.com>
2023-01-12 18:48:42 +0000

    [CP] Fix ls migration core at src get macro block range argument

--- src/storage/ob_storage_rpc.h
+++ src/storage/ob_storage_rpc.h
@@ -88,6 +88,7 @@ public:
   int assign(const ObCopyMacroBlockRangeArg &arg);
 
   TO_STRING_KV(K_(tenant_id), K_(ls_id), K_(table_key), K_(data_version), K_(backfill_tx_log_ts), K_(copy_macro_range_info));
+
   uint64_t tenant_id_;
   share::ObLSID ls_id_;
   storage::ObITable::TableKey table_key_;
