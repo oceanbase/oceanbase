67539708d89950cbe4be46abb6c6060a4ef10e83
jingtaoye35 <1255153887@qq.com>
2023-01-29 17:47:55 +0800

    [CP] Fix security risk about the build-in oraauditor and lbacsys user

--- src/rootserver/ob_ddl_operator.h
+++ src/rootserver/ob_ddl_operator.h
@@ -940,7 +940,7 @@ private:
                                     common::ObMySQLTransaction &trans);
   virtual int init_tenant_user(const uint64_t tenant_id,
                                const common::ObString &user_name,
-                               const char* password,
+                               const common::ObString &pwd_raw,
                                const uint64_t pure_user_id,
                                const common::ObString &user_comment,
                                common::ObMySQLTransaction &trans,
@@ -1098,6 +1098,7 @@ private:
                                        ObMySQLTransaction &trans);
   int init_tenant_spm_configure(uint64_t tenant_id, ObMySQLTransaction &trans);
 private:
+  static const int64_t ENCRYPT_KEY_LENGTH = 15;
   share::schema::ObMultiVersionSchemaService &schema_service_;
   common::ObMySQLProxy &sql_proxy_;
 };
