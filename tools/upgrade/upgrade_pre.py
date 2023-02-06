db4082d1c114b3d2909e8a022a0ba09e195ca1db
obdev <obdev@oceanbase.com>
2023-02-06 20:21:08 +0800

    modify upgrade script

--- tools/upgrade/upgrade_pre.py
+++ tools/upgrade/upgrade_pre.py
@@ -1215,6 +1215,8 @@
 #  if current_version != target_version:
 #    actions.set_parameter(cur, 'enable_ddl', 'False', timeout)
 #    actions.set_parameter(cur, 'enable_major_freeze', 'False', timeout)
+#    actions.set_parameter(cur, 'enable_rebalance', 'False', timeout)
+#    actions.set_parameter(cur, 'enable_rereplication', 'False', timeout)
 #####========******####======== actions begin ========####******========####
 #  return
 #####========******####========= actions end =========####******========####
