5567779f6306f988ba8b71ceb53b5d2226eb3255
obdev <obdev@oceanbase.com>
2023-02-08 16:10:45 +0800

    [foreign key] fix the logic of judging remote_tx

--- src/storage/memtable/ob_row_conflict_handler.cpp
+++ src/storage/memtable/ob_row_conflict_handler.cpp
@@ -117,14 +117,17 @@ int ObRowConflictHandler::post_row_read_conflict(ObMvccAccessCtx &acc_ctx,
   } else {
     int tmp_ret = OB_SUCCESS;
     ObTransID tx_id = acc_ctx.get_tx_id();
-    ObAddr scheduler_addr;
+    ObAddr conflict_scheduler_addr;
     if (OB_FAIL(ObTransDeadlockDetectorAdapter::
-                get_trans_scheduler_info_on_participant(conflict_tx_id, ls_id, scheduler_addr))) {
+                get_trans_scheduler_info_on_participant(conflict_tx_id, ls_id, conflict_scheduler_addr))) {
       TRANS_LOG(WARN, "get transaction scheduler info fail", K(ret), K(conflict_tx_id), K(tx_id), K(ls_id));
     }
-    ObTransIDAndAddr conflict_tx(conflict_tx_id, scheduler_addr);
+    ObTransIDAndAddr conflict_tx(conflict_tx_id, conflict_scheduler_addr);
     tx_desc->add_conflict_tx(conflict_tx);
-    bool remote_tx = scheduler_addr != GCTX.self_addr();
+    // The addr in tx_desc is the scheduler_addr of current trans,
+    // and GCTX.self_addr() will retrun the addr where the row is stored
+    // (i.e. where the trans is executing)
+    bool remote_tx = tx_desc->get_addr() != GCTX.self_addr();
     ObFunction<int(bool&, bool&)> recheck_func([&](bool &locked, bool &wait_on_row) -> int {
       int ret = OB_SUCCESS;
       lock_state.is_locked_ = false;
