8fc7f024013b1f612b655692cdc5ca9f713b805e
dimstars <liangjinrongcm@gmail.com>
2023-02-13 07:36:17 +0000

    fix trans cannot end when participant return OB_ERR_UNEXPECTED in 2pc prapare phase

--- src/storage/transaction/ob_trans_part_ctx.cpp
+++ src/storage/transaction/ob_trans_part_ctx.cpp
@@ -7404,6 +7404,11 @@ int ObPartTransCtx::handle_2pc_prepare_request_raw_(int status)
         // it is logging now. just discard message
         TRANS_LOG(DEBUG, "participant is logging now, discard message", K_(self), K_(trans_id), K(state));
       } else {
+        if (OB_UNLIKELY(!stmt_info_.is_task_match())) {
+          set_status_(OB_TRANS_ROLLBACKED);
+          int tmp_ret = OB_ERR_UNEXPECTED;
+          TRANS_LOG(ERROR, "sql task is not match", K(tmp_ret), "context", *this, K_(stmt_info));
+        }
         bool unused = false;
         if (is_xa_local_trans() && is_redo_prepared_) {
           if (OB_SUCCESS != get_status_() || (OB_SUCCESS != status && OB_TRANS_ROLLBACKED != status)) {
