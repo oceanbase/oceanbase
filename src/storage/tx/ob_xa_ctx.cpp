8aedbde0517ac338e796d044b0383fb432da9d50
obdev <obdev@oceanbase.com>
2023-02-24 15:04:57 +0000

    fix core when try_heartbeat

--- src/storage/tx/ob_xa_ctx.cpp
+++ src/storage/tx/ob_xa_ctx.cpp
@@ -2459,7 +2459,9 @@ int ObXACtx::try_heartbeat()
   int tmp_ret = OB_SUCCESS;
   ObLatchWGuard guard(lock_, common::ObLatchIds::XA_CTX_LOCK);
   const int64_t now = ObTimeUtility::current_time();
-  if (original_sche_addr_ != GCTX.self_addr()) {
+  if (is_exiting_ || is_terminated_) {
+    // do nothing
+  } else if (original_sche_addr_ != GCTX.self_addr()) {
     // temproray scheduler, do nothing
   } else if (OB_ISNULL(xa_branch_info_)
       && (ObXATransState::IDLE < xa_trans_state_ || ObXATransState::UNKNOWN == xa_trans_state_)) {
