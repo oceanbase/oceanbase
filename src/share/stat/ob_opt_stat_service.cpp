00cba8155ed11bdcc7ee4f08bd2688ccd48a5c01
wangt1xiuyi <13547954130@163.com>
2023-03-08 03:46:11 +0000

    fix case and patch add the ignore error code when fetch table failed for sys table and virtual table

--- src/share/stat/ob_opt_stat_service.cpp
+++ src/share/stat/ob_opt_stat_service.cpp
@@ -149,7 +149,9 @@ int ObOptStatService::load_table_stat_and_put_cache(const uint64_t tenant_id,
     ret = OB_NOT_INIT;
     LOG_WARN("table statistics service is not initialized. ", K(ret), K(key));
   } else if (OB_FAIL(sql_service_.fetch_table_stat(tenant_id, key, all_part_stats))) {
-    if (OB_ENTRY_NOT_EXIST != ret) {
+    if (OB_ENTRY_NOT_EXIST != ret &&
+        !is_sys_table(key.table_id_) &&
+        !is_virtual_table(key.table_id_)) {//sys table and virtual table failed, use default
       LOG_WARN("fetch table stat failed. ", K(ret), K(key));
     } else {
       // it's not guaranteed that table stat exists.
