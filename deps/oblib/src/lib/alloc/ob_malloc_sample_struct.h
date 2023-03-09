52c3cde4eb2fe595c4b144e3f75654f360316ca7
obdev <obdev@oceanbase.com>
2023-03-09 12:41:56 +0000

    fix core when run dbms_debug_sqlqa.debug_test3_oracle

--- deps/oblib/src/lib/alloc/ob_malloc_sample_struct.h
+++ deps/oblib/src/lib/alloc/ob_malloc_sample_struct.h
@@ -169,7 +169,9 @@ inline bool ObMallocSampleKey::operator==(const ObMallocSampleKey &other) const
 #define ob_malloc_sample_backtrace(obj, size)                                                       \
   {                                                                                                 \
     if (OB_UNLIKELY(obj->on_malloc_sample_)) {                                                      \
-      int bt_len = backtrace(reinterpret_cast<void**>(&obj->data_[size]), AOBJECT_BACKTRACE_COUNT); \
+      void *addrs[100] = {nullptr};                                                                 \
+      int bt_len = backtrace(addrs, ARRAYSIZEOF(addrs));                                            \
+      MEMCPY(&obj->data_[size], (char*)addrs, AOBJECT_BACKTRACE_SIZE);                              \
       if (AOBJECT_BACKTRACE_COUNT > bt_len) {                                                       \
         reinterpret_cast<void*&>(obj->data_[size + bt_len * sizeof(void*)]) = nullptr;              \
       }                                                                                             \
