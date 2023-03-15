f419c12e6c780dfb5569ccd2c06061b536b28805
chinaxing <chen.yack@gmail.com>
2023-03-15 12:41:06 +0000

    [master][tx-route] mark static changed when tx switch from IDLE to ABORT

--- src/storage/tx/ob_trans_service_v4.cpp
+++ src/storage/tx/ob_trans_service_v4.cpp
@@ -557,6 +557,10 @@ int ObTransService::abort_tx_(ObTxDesc &tx, const int cause, const bool cleanup)
     TRANS_LOG(WARN, "try abort tx which has decided",
               K(ret), K(tx), K(cause));
   } else {
+    // TODO: refactor state switch and put this to there
+    if (ObTxDesc::State::IDLE == tx.state_) {
+      tx.state_change_flags_.STATIC_CHANGED_ = 1;
+    }
     tx.state_ = ObTxDesc::State::IN_TERMINATE;
     tx.abort_cause_ = cause;
     abort_participants_(tx);
