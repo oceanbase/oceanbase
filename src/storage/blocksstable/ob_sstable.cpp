941ae3b80f0f8e6619ce0c7c8bc770bb2a68dcdb
Tyshawn <tuyunshan@gmail.com>
2023-03-22 08:38:56 +0000

    [BUG.FIX] reduce print log

--- src/storage/blocksstable/ob_sstable.cpp
+++ src/storage/blocksstable/ob_sstable.cpp
@@ -871,7 +871,7 @@ int ObSSTable::deserialize(common::ObIAllocator &allocator,
     LOG_WARN("sstable meta is not valid", K(ret), K_(meta));
   } else {
     pos = new_pos;
-    FLOG_INFO("succeed to deserialize sstable", KPC(this));
+    LOG_DEBUG("succeed to deserialize sstable", KPC(this));
   }
   if (OB_FAIL(ret)) {
     LOG_WARN("fail to deserialize sstable", K(ret), K(*this));
@@ -891,7 +891,7 @@ int ObSSTable::deserialize_post_work()
   } else if (SSTABLE_WRITE_BUILDING != meta_.get_basic_meta().status_ && OB_FAIL(check_valid_for_reading())) {
     LOG_WARN("fail to check state", K(ret));
   } else {
-    FLOG_INFO("succeed to do post work for sstable deserialize", KPC(this));
+    LOG_DEBUG("succeed to do post work for sstable deserialize", KPC(this));
   }
   return ret;
 }
