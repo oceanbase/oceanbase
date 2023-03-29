921e85e6fc26354b561143377a120f9ab5075001
SanmuWangZJU <sanmuwang.ws@gmail.com>
2023-03-29 06:41:20 +0000

    [OBCDC] Fix obcdc exit while handling dml from dropping tenant

--- src/logservice/libobcdc/src/ob_log_meta_manager.cpp
+++ src/logservice/libobcdc/src/ob_log_meta_manager.cpp
@@ -741,9 +741,14 @@ int ObLogMetaManager::build_table_meta_(
       table_meta = tmp_table_meta;
     } else {
       int tmp_ret = OB_SUCCESS;
-      if (NULL != tb_schema_info) {
-        if (OB_SUCCESS != (tmp_ret = free_table_schema_info_(tb_schema_info))) {
-          LOG_ERROR("free_table_schema_info_ fail", K(tmp_ret), K(tb_schema_info));
+      if (OB_NOT_NULL(tb_schema_info)) {
+        if (OB_TMP_FAIL(try_erase_table_schema_(
+            table_schema->get_tenant_id(),
+            table_schema->get_table_id(),
+            table_schema->get_schema_version()))) {
+          LOG_ERROR("try_erase_table_schema_ failed", KR(tmp_ret), K(tb_schema_info), KPC(table_schema));
+        } else if (OB_TMP_FAIL(free_table_schema_info_(tb_schema_info))) {
+          LOG_ERROR("free_table_schema_info_ fail", KR(tmp_ret), K(tb_schema_info), KPC(table_schema));
         }
       }
     }
@@ -1763,6 +1768,35 @@ int ObLogMetaManager::set_table_schema_(
     } else {
       LOG_INFO("set_table_schema succ", "schema_version", version, K(tenant_id),
           K(table_id), K(table_name), K(tb_schema_info));
+    }
+  }
+
+  return ret;
+}
+
+int ObLogMetaManager::try_erase_table_schema_(
+    const uint64_t tenant_id,
+    const uint64_t table_id,
+    const int64_t version)
+{
+  int ret = OB_SUCCESS;
+
+  if (OB_UNLIKELY(! inited_)) {
+    ret = OB_NOT_INIT;
+    LOG_ERROR("meta manager has not inited", KR(ret));
+  } else if (OB_UNLIKELY(OB_INVALID_TENANT_ID == tenant_id)
+      || OB_UNLIKELY(OB_INVALID_ID == table_id)
+      || OB_UNLIKELY(OB_INVALID_VERSION == version)) {
+    ret = OB_INVALID_ARGUMENT;
+    LOG_ERROR("invalid argument", KR(ret), K(tenant_id), K(table_id), K(version));
+  } else {
+    MulVerTableKey table_key(version, tenant_id, table_id);
+
+    if (OB_FAIL(tb_schema_info_map_.erase(table_key))) {
+      if (OB_ENTRY_NOT_EXIST != ret) {
+        LOG_WARN("erase table_key from tb_schema_info_map_ failed, may not affect main process, ignore", KR(ret), K(table_key));
+      }
+      ret = OB_SUCCESS;
     }
   }
 
