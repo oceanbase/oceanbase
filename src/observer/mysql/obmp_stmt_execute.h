941a6da4949ab4dd1dc589a094735a2cefe2644e
obdev <obdev@oceanbase.com>
2023-04-14 03:15:16 +0000

    ps protocol analysis add defense check

--- src/observer/mysql/obmp_stmt_execute.h
+++ src/observer/mysql/obmp_stmt_execute.h
@@ -43,6 +43,46 @@ enum ObPSCursorType
   ObPrexecutePsCursorType
 };
 
+class ObPSAnalysisChecker
+{
+public:
+  ObPSAnalysisChecker()
+    : pos_(nullptr), begin_pos_(nullptr), end_pos_(nullptr),
+      data_len_(0), need_check_(true)
+  {}
+  void init(const char*& pos, const int64_t len)
+  {
+    pos_ = &pos;
+    begin_pos_ = pos;
+    end_pos_ = pos + len;
+    data_len_ = len;
+    need_check_ = true;
+  }
+  int detection(const int64_t len);
+  inline int64_t remain_len()
+  { return end_pos_ - (*pos_); }
+
+public:
+  const char** pos_;
+  const char* begin_pos_;
+  const char* end_pos_;
+  int64_t data_len_;
+  bool need_check_;
+};
+
+#define PS_DEFENSE_CHECK(len)                              \
+  if (OB_FAIL(ret)) {                                      \
+  } else if (OB_FAIL(analysis_checker_.detection(len))) {  \
+    LOG_WARN("memory access out of bounds", K(ret));       \
+  } else
+
+#define PS_STATIC_DEFENSE_CHECK(checker, len)              \
+  if (OB_FAIL(ret)) {                                      \
+  } else if (nullptr != checker                            \
+    && OB_FAIL(checker->detection(len))) {                 \
+    LOG_WARN("memory access out of bounds", K(ret));       \
+  } else
+
 class ObMPStmtExecute : public ObMPBase
 {
 public:
@@ -62,21 +102,25 @@ public:
                                     const char *& data,
                                     const common::ObTimeZoneInfo *tz_info,
                                     ObObj &param,
-                                    bool is_complex_element = false);
+                                    bool is_complex_element = false,
+                                    ObPSAnalysisChecker *checker = nullptr);
   static int parse_mysql_timestamp_value(const obmysql::EMySQLFieldType field_type,
                                     const char *&data,
                                     ObObj &param,
-                                    const common::ObTimeZoneInfo *tz_info);
+                                    const common::ObTimeZoneInfo *tz_info,
+                                    ObPSAnalysisChecker *checker = nullptr);
   static int parse_integer_value(const uint32_t type,
                                  const char *&data,
                                  ObObj &param,
                                  ObIAllocator &allocator,
-                                 bool is_complex_element = false);
+                                 bool is_complex_element = false,
+                                 ObPSAnalysisChecker *checker = nullptr);
   static int parse_oracle_timestamp_value(const obmysql::EMySQLFieldType field_type, const char *&data,
-                                    const ObTimeConvertCtx &cvrt_ctx, ObObj &param);
-  static int parse_mysql_time_value(const char *&data, ObObj &param);
-  static int parse_oracle_interval_ym_value(const char *&data, ObObj &param);
-  static int parse_oracle_interval_ds_value(const char *&data, ObObj &param);
+                                    const ObTimeConvertCtx &cvrt_ctx, ObObj &param,
+                                    ObPSAnalysisChecker *checker = nullptr);
+  static int parse_mysql_time_value(const char *&data, ObObj &param, ObPSAnalysisChecker *checker = nullptr);
+  static int parse_oracle_interval_ym_value(const char *&data, ObObj &param, ObPSAnalysisChecker *checker = nullptr);
+  static int parse_oracle_interval_ds_value(const char *&data, ObObj &param, ObPSAnalysisChecker *checker = nullptr);
   int64_t get_single_process_timestamp() const { return single_process_timestamp_; }
   int64_t get_exec_start_timestamp() const { return exec_start_timestamp_; }
   int64_t get_exec_end_timestamp() const { return exec_end_timestamp_; }
@@ -275,6 +319,7 @@ private:
   int get_pl_type_by_type_info(ObIAllocator &allocator,
                         const sql::TypeInfo *type_info,
                         const pl::ObUserDefinedType *&pl_type);
+  bool is_contain_complex_element(const sql::ParamTypeArray &param_types) const;
 
   virtual int before_process();
   int response_query_header(sql::ObSQLSessionInfo &session, pl::ObDbmsCursorInfo &cursor);
@@ -314,6 +359,7 @@ protected:
   int64_t params_value_len_;
   char *params_value_;
   int64_t curr_sql_idx_; // only for arraybinding
+  ObPSAnalysisChecker analysis_checker_;
 private:
   DISALLOW_COPY_AND_ASSIGN(ObMPStmtExecute);
 
