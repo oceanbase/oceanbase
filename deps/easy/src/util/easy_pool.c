cfc92498692ddc002869bc2086fd57d5bb6e67ab
obdev <obdev@oceanbase.com>
2023-04-18 12:43:36 +0000

    [CP] add size as diagnose info when easy cost more than 10ms in socket read/write or allocate memory

--- deps/easy/src/util/easy_pool.c
+++ deps/easy/src/util/easy_pool.c
@@ -93,7 +93,7 @@ void easy_pool_destroy(easy_pool_t *pool)
 
 void *easy_pool_alloc_ex(easy_pool_t *pool, uint32_t size, int align)
 {
-    EASY_STAT_TIME_GUARD(ev_malloc_count, ev_malloc_time);
+    EASY_STAT_TIME_GUARD_WITH_SIZE(ev_malloc_count, ev_malloc_time, size);
     uint8_t                 *m;
     easy_pool_t             *p;
     int                     dsize;
