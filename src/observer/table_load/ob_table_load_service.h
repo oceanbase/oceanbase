b26bcda522e150fde07ae43716b45209f83686de
suz-yang <suz.yang@foxmail.com>
2023-05-25 00:41:34 +0000

    Fix direct load hang unit delete

--- src/observer/table_load/ob_table_load_service.h
+++ src/observer/table_load/ob_table_load_service.h
@@ -40,8 +40,22 @@ private:
   void abort_all_ctx();
   void release_all_ctx();
 private:
+  static const int64_t CHECK_TENANT_INTERVAL = 1LL * 1000 * 1000; // 1s
   static const int64_t GC_INTERVAL = 30LL * 1000 * 1000; // 30s
   static const int64_t RELEASE_INTERVAL = 1LL * 1000 * 1000; // 1s
+  class ObCheckTenantTask : public common::ObTimerTask
+  {
+  public:
+    ObCheckTenantTask(ObTableLoadService &service)
+      : service_(service), tenant_id_(common::OB_INVALID_ID), is_inited_(false) {}
+    virtual ~ObCheckTenantTask() = default;
+    int init(uint64_t tenant_id);
+    void runTimerTask() override;
+  private:
+    ObTableLoadService &service_;
+    uint64_t tenant_id_;
+    bool is_inited_;
+  };
   class ObGCTask : public common::ObTimerTask
   {
   public:
@@ -71,6 +85,7 @@ private:
 private:
   ObTableLoadManager manager_;
   common::ObTimer gc_timer_;
+  ObCheckTenantTask check_tenant_task_;
   ObGCTask gc_task_;
   ObReleaseTask release_task_;
   volatile bool is_stop_;
