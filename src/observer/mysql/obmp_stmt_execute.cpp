de8fcb7674227cf22b334f8789d536a4c245bbda
obdev <obdev@oceanbase.com>
2023-05-31 14:17:30 +0000

    Fix incorrect compatibility mode

--- src/observer/mysql/obmp_stmt_execute.cpp
+++ src/observer/mysql/obmp_stmt_execute.cpp
@@ -1776,6 +1776,8 @@ int ObMPStmtExecute::process()
     int64_t tenant_version = 0;
     int64_t sys_version = 0;
     THIS_WORKER.set_session(sess);
+    lib::CompatModeGuard g(sess->get_compatibility_mode() == ORACLE_MODE ?
+                             lib::Worker::CompatMode::ORACLE : lib::Worker::CompatMode::MYSQL);
     ObSQLSessionInfo::LockGuard lock_guard(session.get_query_lock());
     session.set_current_trace_id(ObCurTraceId::get_trace_id());
     session.get_raw_audit_record().request_memory_used_ = 0;
