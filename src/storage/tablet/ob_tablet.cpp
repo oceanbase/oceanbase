5f04d8ee959870540b4ee5c95260a4fddd9a0ab0
Fengjingkun <dlutfjk@163.com>
2023-07-05 07:42:19 +0000

    fix defensive logic about choosing storage schema

--- src/storage/tablet/ob_tablet.cpp
+++ src/storage/tablet/ob_tablet.cpp
@@ -2914,30 +2914,45 @@ int ObTablet::choose_and_save_storage_schema(
     const ObStorageSchema &param_schema)
 {
   int ret = OB_SUCCESS;
+  const share::ObLSID &ls_id = tablet_meta_.ls_id_;
+  const ObTabletID &tablet_id = tablet_meta_.tablet_id_;
+
   const ObStorageSchema *chosen_schema = &tablet_schema;
-  const ObStorageSchema *other_schema = &param_schema;
-  int64_t chosen_schema_stored_col_cnt = 0;
-  int64_t other_schema_stored_col_cnt = 0;
+
+  int64_t tablet_schema_version = 0;
+  int64_t param_schema_version = 0;
+  int64_t tablet_schema_stored_col_cnt = 0;
+  int64_t param_schema_stored_col_cnt = 0;
   if (OB_UNLIKELY(!tablet_schema.is_valid()) || OB_UNLIKELY(!param_schema.is_valid())) {
     ret = OB_INVALID_ARGUMENT;
-    LOG_WARN("input schema is invalid", K(ret), K(tablet_schema), K(param_schema));
-  } else if (tablet_schema.schema_version_ > param_schema.schema_version_) {
-    LOG_INFO("tablet storage schema version is no smaller than that in param",
-        "tablet_schema_version", tablet_schema.get_schema_version(),
-        "param_schema_version", param_schema.schema_version_);
-  } else {
-    chosen_schema = &param_schema;
-    other_schema = &tablet_schema;
-  }
-  if (OB_FAIL(ret)) {
-  } else if (OB_FAIL(chosen_schema->get_stored_column_count_in_sstable(chosen_schema_stored_col_cnt))) {
-    LOG_WARN("failed to get stored column count from schema", KR(ret), KPC(chosen_schema));
-  } else if (OB_FAIL(other_schema->get_stored_column_count_in_sstable(other_schema_stored_col_cnt))) {
-    LOG_WARN("failed to get stored column count from schema", KR(ret), KPC(other_schema));
-  } else if (OB_UNLIKELY(chosen_schema_stored_col_cnt < other_schema_stored_col_cnt)) {
+    LOG_WARN("input schema is invalid", K(ret), K(ls_id), K(tablet_id), K(tablet_schema), K(param_schema));
+  } else if (FALSE_IT(tablet_schema_version = tablet_schema.schema_version_)) {
+  } else if (FALSE_IT(param_schema_version = param_schema.schema_version_)) {
+  } else if (OB_FAIL(tablet_schema.get_stored_column_count_in_sstable(tablet_schema_stored_col_cnt))) {
+    LOG_WARN("failed to get stored column count from schema", KR(ret), K(ls_id), K(tablet_id), K(tablet_schema));
+  } else if (OB_FAIL(param_schema.get_stored_column_count_in_sstable(param_schema_stored_col_cnt))) {
+    LOG_WARN("failed to get stored column count from schema", KR(ret), K(ls_id), K(tablet_id), K(param_schema));
+  } else if ((tablet_schema_version > param_schema_version && tablet_schema_stored_col_cnt < param_schema_stored_col_cnt)
+          || (tablet_schema_version < param_schema_version && tablet_schema_stored_col_cnt > param_schema_stored_col_cnt)) {
     ret = OB_ERR_UNEXPECTED;
-    LOG_ERROR("chosen schema have less column cnt, but with large schema version", KR(ret),
-      K(tablet_schema), K(param_schema));
+    LOG_ERROR("less column cnt with larger schema version", K(ret), K(ls_id), K(tablet_id), K(tablet_schema_version),
+        K(param_schema_version), K(tablet_schema_stored_col_cnt), K(param_schema_stored_col_cnt), K(tablet_schema), K(param_schema));
+  } else if (tablet_schema_version > param_schema_version
+         || (tablet_schema_version == param_schema_version && tablet_schema_stored_col_cnt >= param_schema_stored_col_cnt)) {
+    // use tablet schema
+    LOG_INFO("tablet storage schema is no smaller than that in param",
+        "ls_id", ls_id,
+        "tablet_id", tablet_id,
+        "tablet_schema_version", tablet_schema.get_schema_version(),
+        "param_schema_version", param_schema.schema_version_,
+        "tablet_schema_column_cnt", tablet_schema_stored_col_cnt,
+        "param_schema_column_cnt", param_schema_stored_col_cnt);
+  } else {
+    // use param schema
+    chosen_schema = &param_schema;
+  }
+
+  if (OB_FAIL(ret)) {
   } else {
     ALLOC_AND_INIT(allocator, storage_schema_addr_, *chosen_schema, true/*skip_column_info*/);
   }
