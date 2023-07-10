335db9ca4d092d81abd5428bf38251551b3f6c79
2149 <260391947@qq.com>
2023-07-10 09:11:56 +0000

    revert commit ‘Fix generated column bug’

--- src/sql/engine/expr/ob_expr_cast.cpp
+++ src/sql/engine/expr/ob_expr_cast.cpp
@@ -894,8 +894,7 @@ int ObExprCast::is_valid_for_generated_column(const ObRawExpr*expr, const common
       }
     } else if (ObTimeType == src && ObTimeType != dst && ob_is_temporal_type(dst)) {
       is_valid = false;
-    } else if ((ObTimestampType == src && ObTimestampType != dst)
-               || (ObTimestampType == dst && ObTimestampType != src)) {
+    } else if (ObTimestampType == src && ObTimestampType != dst) {
       is_valid = false;
     }
   }
