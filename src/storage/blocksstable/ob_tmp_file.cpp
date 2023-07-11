bf30b21d64f0945c96c72059583edfce1732a755
godchen0212 <chenqx303@163.com>
2023-07-11 04:33:25 +0000

    [CP] Fix tmp file read performance regression

--- src/storage/blocksstable/ob_tmp_file.cpp
+++ src/storage/blocksstable/ob_tmp_file.cpp
@@ -196,9 +196,6 @@ void ObTmpFileIOHandle::reset()
   expect_read_size_ = 0;
   last_read_offset_ = -1;
   update_offset_in_file_ = false;
-  last_extent_id_ = 0;
-  last_extent_min_offset_ = 0;
-  last_extent_max_offset_ = INT64_MAX;
 }
 
 bool ObTmpFileIOHandle::is_valid()
@@ -219,6 +216,8 @@ int64_t ObTmpFileIOHandle::get_extent_idx_from_cache(const int64_t offset) const
   int64_t ith_extent = -1;
   if (offset >= last_extent_min_offset_ && offset < last_extent_max_offset_) {
     ith_extent = last_extent_id_;
+  } else if (offset == last_extent_max_offset_) {
+    ith_extent = last_extent_id_ + 1;
   }
   return ith_extent;
 }
