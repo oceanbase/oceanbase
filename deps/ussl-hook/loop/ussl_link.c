1069c1a6ff0d867c520ea709b90f06c29e5ee6ab
496148326@qq.com <496148326@qq.com>
2023-07-12 05:12:18 +0000

    add mulan license for ussl-hook

--- deps/ussl-hook/loop/ussl_link.c
+++ deps/ussl-hook/loop/ussl_link.c
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2021 OceanBase
+ * OceanBase CE is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+
 extern inline void ussl_link_init(ussl_link_t* n);
 extern inline int ussl_link_is_empty(ussl_link_t* n);
 extern inline ussl_link_t* ussl_link_insert(ussl_link_t* prev, ussl_link_t* t);
