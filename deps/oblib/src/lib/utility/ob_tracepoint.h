bdb6f49c729d0b0c2c55b99fedc40745a965fb79
tushicheng <18829573815@163.com>
2023-07-13 12:42:19 +0000

    [CP] fix errsim core

--- deps/oblib/src/lib/utility/ob_tracepoint.h
+++ deps/oblib/src/lib/utility/ob_tracepoint.h
@@ -231,11 +231,12 @@ struct EventItem
   int call() const
   {
     int ret = 0;
-    if (OB_LIKELY(trigger_freq_ == 0)) {
+    int64_t trigger_freq = trigger_freq_;
+    if (OB_LIKELY(trigger_freq == 0)) {
       ret = 0;
     } else if (get_tp_switch()) { // true means skip errsim
       ret = 0;
-    } else if (trigger_freq_ == 1) {
+    } else if (trigger_freq == 1) {
       ret = static_cast<int>(error_code_);
 #ifdef NDEBUG
       if (REACH_TIME_INTERVAL(1 * 1000 * 1000))
@@ -244,9 +245,9 @@ struct EventItem
         COMMON_LOG(WARN, "[ERRSIM] sim error", K(ret));
       }
     } else {
-      if (rand() % trigger_freq_ == 0) {
+      if (rand() % trigger_freq == 0) {
         ret = static_cast<int>(error_code_);
-        COMMON_LOG(WARN, "[ERRSIM] sim error", K(ret), K_(error_code), K_(trigger_freq), KCSTRING(lbt()));
+        COMMON_LOG(WARN, "[ERRSIM] sim error", K(ret), K_(error_code), K(trigger_freq), KCSTRING(lbt()));
       } else {
         ret = 0;
       }
