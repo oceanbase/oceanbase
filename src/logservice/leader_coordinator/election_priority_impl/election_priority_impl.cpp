63d1fb2cb829134dac33d012f3d2d88937c1d8c6
fengdeyiji <546976189@qq.com>
2023-07-13 12:54:28 +0000

    [Election] adjust invalid priority compare bahavior

--- src/logservice/leader_coordinator/election_priority_impl/election_priority_impl.cpp
+++ src/logservice/leader_coordinator/election_priority_impl/election_priority_impl.cpp
@@ -210,11 +210,11 @@ int ElectionPriorityImpl::compare_with(const ElectionPriority &rhs,
     result = 0;
     COORDINATOR_LOG(WARN, "compare between invalid priority");
   } else if (functor1.get_closest_priority()->is_valid() && !functor2.get_closest_priority()->is_valid()) {
-    result = 0;
+    result = 1;
     (void) reason.assign("compare with invalid rhs priority");
     COORDINATOR_LOG(WARN, "rhs priority is invalid", KR(ret), K(MTL_ID()), K(*this), K(rhs), K(compare_version), K(result), K(reason));
   } else if (!functor1.get_closest_priority()->is_valid() && functor2.get_closest_priority()->is_valid()) {
-    result = 0;
+    result = -1;
     (void) reason.assign("compare with invalid lhs priority");
     COORDINATOR_LOG(WARN, "lhs priority is invalid", KR(ret), K(MTL_ID()), K(*this), K(rhs), K(compare_version), K(result), K(reason));
   } else if (CLICK_FAIL(functor1.get_closest_priority()->compare(*functor2.get_closest_priority(), result, reason))) {
