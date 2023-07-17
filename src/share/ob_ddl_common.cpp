63ec91d4c45a6792d77ec2a3703c072284540447
Monk-Liu <1152761042@qq.com>
2023-07-17 14:42:14 +0000

    [bugfix][inner_sql]: mis-convert '`' to '``' in create index's inner sql.

--- src/share/ob_ddl_common.cpp
+++ src/share/ob_ddl_common.cpp
@@ -717,11 +717,7 @@ int ObDDLUtil::generate_build_replica_sql(
         }
       }
 
-      if (OB_SUCC(ret) && use_schema_version_hint_for_src_table) {
-        if (OB_FAIL(generate_ddl_schema_hint_str(source_table_name, schema_version, oracle_mode, src_table_schema_version_hint_sql_string))) {
-          LOG_WARN("failed to generated ddl schema hint", K(ret));
-        }
-      }
+
 
       if (OB_SUCC(ret)) {
         ObArenaAllocator allocator("ObDDLTmp");
@@ -759,14 +755,13 @@ int ObDDLUtil::generate_build_replica_sql(
               oracle_mode))) {
           LOG_WARN("fail to generate new name with escape character",
                     K(ret), K(source_table_name));
-        } else if (OB_FAIL(sql::ObSQLUtils::generate_new_name_with_escape_character(
-              allocator,
-              src_table_schema_version_hint_sql_string.string(),
-              new_table_schema_version_hint,
-              oracle_mode
-        ))) {
-          LOG_WARN("fail to generate new name with escape character",
-                    K(ret), K(src_table_schema_version_hint_sql_string));
+        } else if (use_schema_version_hint_for_src_table) {
+          if (OB_FAIL(generate_ddl_schema_hint_str(new_source_table_name, schema_version, oracle_mode, src_table_schema_version_hint_sql_string))) {
+            LOG_WARN("failed to generated ddl schema hint", K(ret));
+          }
+        }
+
+        if (OB_FAIL(ret)) {
         } else if (oracle_mode) {
           if (OB_FAIL(sql_string.assign_fmt("INSERT /*+ monitor enable_parallel_dml parallel(%ld) opt_param('ddl_execution_id', %ld) opt_param('ddl_task_id', %ld) opt_param('enable_newsort', 'false') use_px */INTO \"%.*s\".\"%.*s\"(%.*s) SELECT /*+ index(\"%.*s\" primary) %.*s */ %.*s from \"%.*s\".\"%.*s\" as of scn %ld %.*s",
               real_parallelism, execution_id, task_id,
