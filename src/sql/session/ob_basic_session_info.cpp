2a8a8611008e2c4f40a677db417fdba73e0d5baf
yaojing624 <jingfeng.jf@oceanbase.com>
2023-08-11 17:58:08 +0000

    [CP] Fix: session sync use new type to sync last_schema_version

--- src/sql/session/ob_basic_session_info.cpp
+++ src/sql/session/ob_basic_session_info.cpp
@@ -3667,6 +3667,7 @@ bool ObBasicSessionInfo::is_sync_sys_var(share::ObSysVarClassType sys_var_id) co
     case SYS_VAR_SERVER_UUID:
     case SYS_VAR_OB_PROXY_PARTITION_HIT:
     case SYS_VAR_VERSION_COMMENT:
+    case SYS_VAR_OB_LAST_SCHEMA_VERSION:
       not_need_serialize = true;
       break;
     default:
