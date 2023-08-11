415ff7bbea6a19236fbd900f077fe34dc761f7a1
obdev <obdev@oceanbase.com>
2023-08-11 12:20:48 +0000

    [CP] [RS] Fix errcode missing in unit_manager and server_balancer.

--- src/rootserver/ob_unit_manager.cpp
+++ src/rootserver/ob_unit_manager.cpp
@@ -10041,7 +10041,7 @@ int ObUnitManager::delete_units_in_zones(
              || OB_UNLIKELY(to_be_removed_zones.count() <= 0)) {
     ret = OB_INVALID_ARGUMENT;
     LOG_WARN("invalid argument", K(ret), K(resource_pool_id), K(to_be_removed_zones));
-  } else if (get_units_by_pool(resource_pool_id, units)) {
+  } else if (OB_FAIL(get_units_by_pool(resource_pool_id, units))) {
     LOG_WARN("fail to get units by pool", K(ret), K(resource_pool_id));
   } else if (OB_UNLIKELY(NULL == units)) {
     ret = OB_ERR_UNEXPECTED;
