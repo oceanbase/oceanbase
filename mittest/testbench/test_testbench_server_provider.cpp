a7ef33cff4b239bea92f66da1379514dccc765c8
Huidong Zhang <huiddongzhang@gmail.com>
2023-09-14 22:18:43 +0800

    [bugfix] memory leak in testbench server provider hashmap

--- mittest/testbench/test_testbench_server_provider.cpp
+++ mittest/testbench/test_testbench_server_provider.cpp
@@ -43,16 +43,31 @@ TEST_F(TestServerProvider, refresh) {
   EXPECT_EQ(OB_SUCCESS, server_provider.end_refresh());
   common::ObSEArray<uint64_t, 16> tenant_list;
   common::ObSEArray<testbench::ObTenantName, 16> tenant_name_list;
-  int64_t count = server_provider.get_server_count();
   EXPECT_EQ(OB_SUCCESS, server_provider.get_tenant_ids(tenant_list));
   EXPECT_EQ(OB_SUCCESS, server_provider.get_tenants(tenant_name_list));
   TESTBENCH_LOG(INFO, "get tenant ids", K(tenant_list));
   TESTBENCH_LOG(INFO, "get tenant names", K(tenant_name_list));
-  EXPECT_EQ(count, tenant_list.count());
-  EXPECT_EQ(count, tenant_name_list.count());
-  ObSEArray<ObAddr, 16> server_list;
-  for (int64_t tenant_idx = 0; tenant_idx < count; ++tenant_idx) {
+  int64_t server_count = server_provider.get_server_count();
+  int64_t tenant_count = tenant_list.count();
+  common::ObSEArray<common::ObAddr, 16> server_list(
+      "OBMySQLConnPool", OB_MALLOC_NORMAL_BLOCK_SIZE);
+  for (int64_t tenant_idx = 0; tenant_idx < tenant_count; ++tenant_idx) {
+    uint64_t tenant_id = OB_INVALID_TENANT_ID;
+    EXPECT_EQ(OB_SUCCESS, tenant_list.at(tenant_idx, tenant_id));
+    EXPECT_EQ(OB_SUCCESS,
+              server_provider.get_tenant_servers(tenant_id, server_list));
+    EXPECT_EQ(server_count, server_list.count());
+    TESTBENCH_LOG(INFO, "tenant get servers", K(tenant_id), K(server_list));
   }
+  tenant_list.destroy();
+  tenant_name_list.destroy();
+  server_list.destroy();
 }
 } // namespace unittest
 } // namespace oceanbase
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  OB_LOGGER.set_log_level("INFO");
+  return RUN_ALL_TESTS();
+}
