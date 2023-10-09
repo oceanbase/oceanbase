af32d254f4c9f123479b3b59298c738a907022c6
obdev <obdev@oceanbase.com>
2023-10-09 04:45:34 +0000

    [to #52433249] fix collection deleted element serialize/deserialize

--- src/pl/ob_pl_user_type.cpp
+++ src/pl/ob_pl_user_type.cpp
@@ -2238,7 +2238,6 @@ int ObCollectionType::deserialize(
       OZ (ObSPIService::spi_set_collection(
         OB_INVALID_ID, &resolve_ctx, *table->get_allocator(), *table, count, true));
     }
-    // CK (OB_NOT_NULL(table->get_data()));
 
     if (OB_SUCC(ret)) {
       char *table_data = reinterpret_cast<char*>(table->get_data());
@@ -2251,6 +2250,7 @@ int ObCollectionType::deserialize(
           ObPLComposite* composite = reinterpret_cast<ObPLComposite*>(obj->get_ext());
           CK (OB_NOT_NULL(composite));
           if (OB_SUCC(ret) && composite->get_type() == PL_INVALID_TYPE) {
+            obj->set_extend(obj->get_ext(), PL_INVALID_TYPE);
             obj->set_type(ObMaxType);
           }
         }
@@ -3500,17 +3500,18 @@ int ObPLCollection::deep_copy(ObPLCollection *src, ObIAllocator *allocator, bool
         } else {
           if (old_objs[i].is_invalid_type() && src->is_of_composite()) {
             old_obj.set_type(ObExtendType);
-            CK (old_obj.is_pl_extend());
           }
           OX (new (&new_objs[k])ObObj());
-          OZ (ObPLComposite::copy_element(old_obj,
-                                          new_objs[k],
-                                          *coll_allocator,
-                                          NULL, /*ns*/
-                                          NULL, /*session*/
-                                          NULL, /*dest_type*/
-                                          true, /*need_new_allocator*/
-                                          ignore_del_element));
+          if (OB_SUCC(ret) && ((src->is_of_composite() && old_obj.is_pl_extend()) || !src->is_of_composite())) {
+            OZ (ObPLComposite::copy_element(old_obj,
+                                            new_objs[k],
+                                            *coll_allocator,
+                                            NULL, /*ns*/
+                                            NULL, /*session*/
+                                            NULL, /*dest_type*/
+                                            true, /*need_new_allocator*/
+                                            ignore_del_element));
+          }
           OX (++k);
           if (old_objs[i].is_invalid_type() && src->is_of_composite()) {
             new_objs[i].set_type(ObMaxType);
