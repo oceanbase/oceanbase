58c406669a2bbb6c56c9cb67e2f1097cb5307362
coolfishchen <coolfishchen@gmail.com>
2023-10-31 21:46:35 +0000

    fix core of ObSetTPExecutor

--- src/sql/engine/cmd/ob_alter_system_executor.cpp
+++ src/sql/engine/cmd/ob_alter_system_executor.cpp
@@ -1213,13 +1213,14 @@ int ObSetTPExecutor::execute(ObExecContext &ctx, ObSetTPStmt &stmt)
 {
   int ret = OB_SUCCESS;
   ObTaskExecutorCtx *task_exec_ctx = GET_TASK_EXECUTOR_CTX(ctx);
+  obrpc::ObCommonRpcProxy *common_rpc_proxy = nullptr;
   if (OB_ISNULL(task_exec_ctx)) {
     ret = OB_NOT_INIT;
     LOG_WARN("get task executor context failed");
-  } else if (OB_ISNULL(task_exec_ctx->get_common_rpc())) {
+  } else if (OB_ISNULL(common_rpc_proxy = task_exec_ctx->get_common_rpc())) {
     ret = OB_NOT_INIT;
     LOG_WARN("get common rpc proxy failed", K(task_exec_ctx));
-  } else if (OB_FAIL(task_exec_ctx->get_common_rpc()->admin_set_tracepoint(
+  } else if (OB_FAIL(common_rpc_proxy->admin_set_tracepoint(
                        stmt.get_rpc_arg()))) {
     LOG_WARN("set tracepoint rpc failed", K(ret), "rpc_arg", stmt.get_rpc_arg());
   }
