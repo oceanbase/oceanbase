4070a75828e024f6cad340d2726e4f70016a79b5
nroskill <nroskill@gmail.com>
2023-11-02 08:52:38 +0000

    [CP] fix auto & lambda

--- deps/oblib/src/lib/trace/ob_trace.h
+++ deps/oblib/src/lib/trace/ob_trace.h
@@ -144,7 +144,7 @@ struct ObTagCtxBase
   virtual int tostring(char* buf, const int64_t buf_len, int64_t& pos)
   {
     int ret = OB_SUCCESS;
-    const auto l = strlen(__tag_name_mapper[tag_type_]);
+    const size_t l = strlen(__tag_name_mapper[tag_type_]);
     if (pos + l + 7 >= buf_len) {
       buf[std::min(pos - 1, buf_len - 1)] = '\0';
       ret = OB_BUF_NOT_ENOUGH;
@@ -320,7 +320,7 @@ struct ObTrace
     } else {
       v = value;
     }
-    auto l = v.length();
+    int32_t l = v.length();
     if (offset_ + sizeof(ObTagCtx<void*>) + l + 1 - sizeof(void*) >= buffer_size_) {
       // do nothing
     } else {
