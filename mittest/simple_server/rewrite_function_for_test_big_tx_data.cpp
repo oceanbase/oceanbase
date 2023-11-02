00398fcab26c06d73967c71d3f53600210da90d1
dimstars <liangjinrongcm@gmail.com>
2023-11-02 04:42:18 +0000

    [CP] license opt: Identify external and internal code and modify their open source licenses correctly.

--- mittest/simple_server/rewrite_function_for_test_big_tx_data.cpp
+++ mittest/simple_server/rewrite_function_for_test_big_tx_data.cpp
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2021 OceanBase
+ * OceanBase CE is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+
 #include "storage/tx_table/ob_tx_table_iterator.h"
 #include "storage/tx/ob_tx_data_define.h"
 #include "storage/tx/ob_trans_part_ctx.h"
--- src/sql/plan_cache/ob_prepare_stmt_struct.h
+++ src/sql/plan_cache/ob_prepare_stmt_struct.h
@@ -1,10 +1,13 @@
-/*
- * (C) 2007-2010 Alibaba Group Holding Limited.
- *
- *  Version: $Id: ob_prepare_stmt_struct.h 10/27/2016 04:13:54 PM
- *
- *  Authors:
- *     hualong <>
+/**
+ * Copyright (c) 2021 OceanBase
+ * OceanBase CE is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
  */
 
 #ifndef OCEANBASE_SQL_PLAN_CACHE_OB_PREPARE_STMT_STRUCT_H_
