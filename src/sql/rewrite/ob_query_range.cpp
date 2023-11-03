5793876abadb32480104ee3946ecf870bde3124c
obdev <obdev@oceanbase.com>
2023-11-03 08:13:26 +0000

    [CP] fix s2adapter leak

--- src/sql/rewrite/ob_query_range.cpp
+++ src/sql/rewrite/ob_query_range.cpp
@@ -8757,7 +8757,7 @@ int ObQueryRange::get_geo_intersects_keypart(uint32_t input_srid,
                                              ObKeyPart *out_key_part)
 {
   INIT_SUCC(ret);
-  common::ObArenaAllocator tmp_alloc;
+  common::ObArenaAllocator tmp_alloc(lib::ObLabel("GisIndex"));
   ObS2Cellids cells;
   ObS2Cellids cells_with_ancestors;
   ObSpatialMBR mbr_filter(op_type);
@@ -8893,6 +8893,9 @@ int ObQueryRange::get_geo_intersects_keypart(uint32_t input_srid,
         }
       }
     }
+  }
+  if (OB_NOT_NULL(s2object)) {
+    s2object->~ObS2Adapter();
   }
 
   return ret;
@@ -9070,6 +9073,9 @@ int ObQueryRange::get_geo_coveredby_keypart(uint32_t input_srid,
         }
       }
     }
+  }
+  if (OB_NOT_NULL(s2object)) {
+    s2object->~ObS2Adapter();
   }
 
   return ret;
