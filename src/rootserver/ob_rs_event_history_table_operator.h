4deeee3a3afc123774c6ae343432301a36af3391
LeonChaoHi <15201161716@163.com>
2023-11-16 02:14:14 +0000

    fix __all_rs_event_history insert failure

--- src/rootserver/ob_rs_event_history_table_operator.h
+++ src/rootserver/ob_rs_event_history_table_operator.h
@@ -38,6 +38,7 @@ private:
 
 #define ROOTSERVICE_EVENT_INSTANCE (::oceanbase::rootserver::ObRsEventHistoryTableOperator::get_instance())
 #define ROOTSERVICE_EVENT_ADD(args...)                                         \
-  ROOTSERVICE_EVENT_INSTANCE.add_event(args)
-
+  ROOTSERVICE_EVENT_INSTANCE.add_event<false>(args)
+#define ROOTSERVICE_EVENT_ADD_TRUNCATE(args...)                                \
+  ROOTSERVICE_EVENT_INSTANCE.add_event<true>(args)
 #endif // OCEANBASE_ROOTSERVER_OB_RS_EVENT_HISTORY_TABLE_OPERATOR_H_
