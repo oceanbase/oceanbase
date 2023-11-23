f6083f5351ca0a8309584516e08148d8ecd2bbb9
z404289981 <z404289981@163.com>
2023-11-23 02:41:03 +0000

    opt whole scnner io buf

--- src/storage/access/ob_sstable_row_whole_scanner.cpp
+++ src/storage/access/ob_sstable_row_whole_scanner.cpp
@@ -42,17 +42,16 @@ ObSSTableRowWholeScanner::~ObSSTableRowWholeScanner()
   }
 }
 
-int ObSSTableRowWholeScanner::alloc_io_buf()
+int ObSSTableRowWholeScanner::alloc_io_buf(compaction::ObCompactionBuffer &io_buf, int64_t buf_size)
 {
   int ret = OB_SUCCESS;
   int64_t size = common::OB_DEFAULT_MACRO_BLOCK_SIZE * PREFETCH_DEPTH;
-  if (OB_ISNULL(buf_ = reinterpret_cast<char*>(io_allocator_.alloc(size)))) { //continuous memory
-    ret = OB_ALLOCATE_MEMORY_FAILED;
-    STORAGE_LOG(WARN, "failed to alloc ObSSTableRowWholeScanner read info buffer", K(ret), K(size));
-  } else {
-    for (int64_t i = 0; OB_SUCC(ret) && i < PREFETCH_DEPTH; ++i) {
-      io_buf_[i] = buf_ + common::OB_DEFAULT_MACRO_BLOCK_SIZE * i;
+  if (OB_LIKELY(io_buf.is_inited())) {
+    if (OB_FAIL(io_buf.reserve(buf_size))) {
+      LOG_WARN("fail to reserve io buf", K(ret), K(io_buf), K(buf_size));
     }
+  } else if (OB_FAIL(io_buf.init(common::OB_DEFAULT_MACRO_BLOCK_SIZE, buf_size))) {
+    LOG_WARN("fail to init io buf", K(ret), K(io_buf), K(buf_size));
   }
   return ret;
 }
@@ -77,8 +76,6 @@ void ObSSTableRowWholeScanner::reset()
     micro_scanner_ = nullptr;
   }
   allocator_.reset();
-  io_allocator_.reset();
-  buf_ = nullptr;
   is_inited_ = false;
   last_micro_block_recycled_ = false;
   last_mvcc_row_already_output_ = false;
@@ -86,7 +83,7 @@ void ObSSTableRowWholeScanner::reset()
 
 void ObSSTableRowWholeScanner::reuse()
 {
-  ObStoreRowIterator::reuse();
+  ObStoreRowIterator::reset();
   iter_param_ = nullptr;
   access_ctx_ = nullptr;
   sstable_ = nullptr;
@@ -104,8 +101,6 @@ void ObSSTableRowWholeScanner::reuse()
     micro_scanner_ = nullptr;
   }
   allocator_.reuse();
-  io_allocator_.reuse();
-  buf_ = nullptr;
   is_inited_ = false;
   last_micro_block_recycled_ = false;
   last_mvcc_row_already_output_ = false;
@@ -208,8 +203,6 @@ int ObSSTableRowWholeScanner::inner_open(
       rowkey_read_info = iter_param.read_info_;
     }
     if (OB_FAIL(ret)) {
-    } else if (OB_FAIL(alloc_io_buf())) {
-      LOG_WARN("alloc io buffers failed", K(ret));
     } else if (OB_FAIL(init_micro_scanner(range))) {
       LOG_WARN("Failed to init micro scanner", K(ret));
     } else if (OB_FAIL(macro_block_iter_.open(
@@ -272,8 +265,8 @@ int ObSSTableRowWholeScanner::open(
     MacroScanHandle &scan_handle = scan_handles_[0];
     scan_handle.reset();
 
-    if (OB_FAIL(alloc_io_buf())) {
-      LOG_WARN("alloc io buffers failed", K(ret));
+    if (OB_FAIL(alloc_io_buf(io_buf_[0], sstable_->get_macro_read_size()))) {
+      LOG_WARN("alloc io buffers failed", K(ret), K(sstable_->get_macro_read_size()));
     } else if (OB_FAIL(init_micro_scanner(&query_range))) {
       LOG_WARN("Fail to init micro scanner", K(ret));
     } else {
@@ -287,7 +280,7 @@ int ObSSTableRowWholeScanner::open(
       read_info.size_ = sstable_->get_macro_read_size();
       read_info.io_desc_.set_wait_event(ObWaitEventIds::DB_FILE_COMPACT_READ);
       read_info.io_desc_.set_group_id(ObIOModule::SSTABLE_WHOLE_SCANNER_IO);
-      read_info.buf_ = io_buf_[0];
+      read_info.buf_ = io_buf_[0].data();
 
       if (OB_FAIL(ret)) {
       } else if (OB_FAIL(ObBlockManager::async_read_block(read_info, scan_handle.macro_io_handle_))) {
@@ -424,6 +417,8 @@ int ObSSTableRowWholeScanner::prefetch()
       } else {
         LOG_WARN("Fail to get_next_macro_block ", K(ret), K(macro_block_iter_));
       }
+    } else if (OB_FAIL(alloc_io_buf(io_buf_[io_index], sstable_->get_macro_read_size()))) {
+      LOG_WARN("alloc io buffers failed", K(ret), K(sstable_->get_macro_read_size()));
     } else {
       scan_handle.is_left_border_ = (0 == prefetch_macro_cursor_);
       scan_handle.is_right_border_ = false; // set right border correctly when open macro block
@@ -433,7 +428,7 @@ int ObSSTableRowWholeScanner::prefetch()
       read_info.size_ = sstable_->get_macro_read_size();
       read_info.io_desc_.set_wait_event(common::ObWaitEventIds::DB_FILE_COMPACT_READ);
       read_info.io_desc_.set_group_id(ObIOModule::SSTABLE_WHOLE_SCANNER_IO);
-      read_info.buf_ = io_buf_[io_index];
+      read_info.buf_ = io_buf_[io_index].data();
       if (OB_FAIL(ret)) {
       } else if (OB_FAIL(ObBlockManager::async_read_block(read_info, scan_handle.macro_io_handle_))) {
         LOG_WARN("Fail to read macro block, ", K(ret), K(read_info));
--- src/storage/compaction/ob_compaction_memory_context.h
+++ src/storage/compaction/ob_compaction_memory_context.h
@@ -264,6 +264,96 @@ private:
   common::ObSpinLock lock_;
 };
 
+class ObCompactionBuffer
+{
+public:
+  ObCompactionBuffer(const lib::ObLabel &label = "compaction_buf", const int64_t page_size = DEFAULT_MIDDLE_BLOCK_SIZE)
+    : allocator_(MTL_ID(), label),
+      is_inited_(false),
+      capacity_(0),
+      buffer_size_(0),
+      len_(0),
+      data_(nullptr),
+      reset_memory_threshold_(0),
+      memory_reclaim_cnt_(0),
+      has_expand_(false)
+  {}
+  virtual ~ObCompactionBuffer() { reset(); };
+  int init(const int64_t capacity, const int64_t reserve_size = DEFAULT_MIDDLE_BLOCK_SIZE);
+  inline bool is_inited() const { return is_inited_; }
+  inline int64_t remain() const { return capacity_ - len_; }
+  inline int64_t remain_buffer_size() const { return buffer_size_ - len_; }
+  inline int64_t size() const { return buffer_size_; } //curr buffer size
+  inline bool has_expand() const { return has_expand_; }
+  inline char *data() { return data_; }
+  inline char *current() { return data_ + len_; }
+  int reserve(const int64_t size);
+  int ensure_space(const int64_t append_size);
+  inline void pop_back(const int64_t size) { len_ = MAX(0, len_ - size); }
+  int write_nop(const int64_t size, bool is_zero = false);
+  int write(const void *buf, int64_t size);
+
+  template<typename T>
+  typename std::enable_if<!HAS_MEMBER(T, serialize), int>::type write(const T &value)
+  {
+    int ret = OB_SUCCESS;
+    static_assert(std::is_pod<T>::value, "invalid type");
+    if (OB_FAIL(ensure_space(sizeof(T)))) {
+      if (ret != OB_BUF_NOT_ENOUGH) {
+        STORAGE_LOG(WARN, "failed to ensure space", K(ret), K(sizeof(T)));
+      }
+    } else {
+      *((T *)(data_ + len_)) = value;
+      len_ += sizeof(T);
+    }
+    return ret;
+  }
+
+  template<typename T>
+  typename std::enable_if<HAS_MEMBER(T, serialize), int>::type write(const T &value)
+  {
+    int ret = OB_SUCCESS;
+    if (OB_FAIL(ensure_space(value.get_serialize_size()))) {
+      if (ret != OB_BUF_NOT_ENOUGH) {
+        STORAGE_LOG(WARN, "failed to ensure space", K(ret), K(value.get_serialize_size()));
+      }
+    } else if (OB_FAIL(value.serialize(data_, buffer_size_, len_))) {
+      STORAGE_LOG(WARN, "fail to serialize", K(ret), K(buffer_size_), K(len_));
+    }
+    return ret;
+  }
+  int advance(const int64_t size);
+  int set_length(const int64_t len);
+
+  void reuse();
+  void reset();
+  inline int64_t length() const { return len_; }
+  TO_STRING_KV(K_(capacity), K_(buffer_size), K_(len), K_(data), K_(default_reserve), K_(reset_memory_threshold),
+      K_(memory_reclaim_cnt), K_(has_expand));
+protected:
+  bool check_could_expand() { return capacity_ > buffer_size_; }
+  int expand(const int64_t size);
+private:
+  compaction::ObLocalAllocator<common::DefaultPageAllocator> allocator_;
+  bool is_inited_;
+  int64_t capacity_;
+  int64_t buffer_size_; //curr buffer size
+  int64_t len_; //curr pos
+  char *data_;
+
+  // for reclaim memory
+  int64_t default_reserve_;
+  int64_t reset_memory_threshold_;
+  int64_t memory_reclaim_cnt_;
+  bool has_expand_;
+
+protected:
+  static const int64_t MIN_BUFFER_SIZE = 1 << 12; //4kb
+  static const int64_t MAX_DATA_BUFFER_SIZE = 2 * common::OB_DEFAULT_MACRO_BLOCK_SIZE; // 4m
+  static const int64_t DEFAULT_MIDDLE_BLOCK_SIZE = 1 << 16; //64K
+  static const int64_t DEFAULT_RESET_MEMORY_THRESHOLD = 5;
+};
+
 } // compaction
 } // oceanbase
 
