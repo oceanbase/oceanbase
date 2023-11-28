b5ab11662b6aa41bd17a05530a5dfe4df68b6c87
sdc <njucssdc@gmail.com>
2023-11-28 09:59:11 +0000

    refresh alive_server_set_ of px_target_mgr when find server not alive

--- src/sql/engine/px/ob_px_target_mgr.cpp
+++ src/sql/engine/px/ob_px_target_mgr.cpp
@@ -152,6 +152,9 @@ void ObPxTargetMgr::run1()
           LOG_INFO("found a server is not longer alive, so refresh all", K(it->first));
         }
       }
+      if (px_res_refresh_funtor.need_refresh_all_) {
+        alive_server_set_.clear();
+      }
     }
   }
 }
