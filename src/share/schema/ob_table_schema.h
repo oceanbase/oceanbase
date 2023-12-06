1516b54e923f89dd68cc4a7f9845d342bd764800
yangqise7en <877793735@qq.com>
2023-12-06 12:42:31 +0000

    skip hidden schema in rs major check & fix default time guard warning threshold

--- src/share/schema/ob_table_schema.h
+++ src/share/schema/ob_table_schema.h
@@ -1921,10 +1921,10 @@ inline bool ObSimpleTableSchemaV2::should_check_major_merge_progress() const
 {
   // only include primary table except aux lob tables
   // aux lob tables only need to perform replicas verification, but don't need to be verified with primary table
-  return is_sys_table()
+  return is_normal_schema() && (is_sys_table()
           || is_user_table()
           || is_tmp_table()
-          || is_aux_lob_table();
+          || is_aux_lob_table());
 }
 
 inline int64_t ObTableSchema::get_id_hash_array_mem_size(const int64_t column_cnt) const
