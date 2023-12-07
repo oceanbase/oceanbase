cfa0e9cf239786ce32404bfb749a91c096a1e12c
wudidapaopao <664920313@qq.com>
2023-12-07 06:17:31 +0000

    [CP] fix sanity crash when printing log

--- src/storage/access/ob_index_tree_prefetcher.h
+++ src/storage/access/ob_index_tree_prefetcher.h
@@ -371,8 +371,8 @@ public:
                        K_(is_prefetch_end), K_(cur_range_fetch_idx), K_(cur_range_prefetch_idx), K_(max_range_prefetching_cnt),
                        K_(cur_micro_data_fetch_idx), K_(micro_data_prefetch_idx), K_(max_micro_handle_cnt),
                        K_(iter_type), K_(cur_level), K_(index_tree_height), K_(prefetch_depth),
-                       K_(total_micro_data_cnt), KP_(query_range), K_(tree_handles), K_(border_rowkey),
-                       K_(can_blockscan), K_(need_check_prefetch_depth));
+                       K_(total_micro_data_cnt), KP_(query_range), K_(tree_handles), K_(can_blockscan),
+                       K_(need_check_prefetch_depth));
 private:
   int init_basic_info(
       const int iter_type,
