0423901da6d10c4a26a7406130640e5fffbeb0f9
skylhd <dickylhd@gmail.com>
2023-12-08 05:43:32 +0000

    fix repeat expr longtext max len

--- src/sql/engine/expr/ob_expr_replace.h
+++ src/sql/engine/expr/ob_expr_replace.h
@@ -43,7 +43,8 @@ public:
                      const common::ObString &text,
                      const common::ObString &from,
                      const common::ObString &to,
-                     common::ObExprStringBuf &string_buf);
+                     common::ObExprStringBuf &string_buf,
+                     const int64_t max_len = OB_MAX_VARCHAR_LENGTH);
 private:
   // disallow copy
   DISALLOW_COPY_AND_ASSIGN(ObExprReplace);
