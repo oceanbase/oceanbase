da3f6784e9ebe8d7f2a31c1b781606c72098714e
oceanoverflow <oceanoverflow@gmail.com>
2023-12-20 11:14:32 +0000

    [CP] fix backup check tablet continuity performance issue

--- src/storage/backup/ob_backup_task.cpp
+++ src/storage/backup/ob_backup_task.cpp
@@ -561,7 +561,8 @@ int ObLSBackupDataDagNet::inner_init_before_run_()
   int64_t batch_size = 0;
   if (OB_FAIL(param_.convert_to(backup_param))) {
     LOG_WARN("failed to convert param", K(param_));
-  } else if (OB_FAIL(ls_backup_ctx_.open(backup_param, backup_data_type_, *param_.report_ctx_.sql_proxy_))) {
+  } else if (OB_FAIL(ls_backup_ctx_.open(backup_param, backup_data_type_,
+      *param_.report_ctx_.sql_proxy_, OB_BACKUP_INDEX_CACHE))) {
     LOG_WARN("failed to open log stream backup ctx", K(ret), K(backup_param));
   } else if (OB_FAIL(prepare_backup_tablet_provider_(backup_param, backup_data_type_, ls_backup_ctx_,
       OB_BACKUP_INDEX_CACHE, *param_.report_ctx_.sql_proxy_, provider_))) {
@@ -1963,7 +1964,9 @@ ObPrefetchBackupInfoTask::ObPrefetchBackupInfoTask()
       index_kv_cache_(NULL),
       macro_index_store_for_inc_(),
       macro_index_store_for_turn_(),
-      index_rebuild_dag_(NULL)
+      index_rebuild_dag_(NULL),
+      next_prefetch_task_id_(-1),
+      next_backup_task_id_(-1)
 {}
 
 ObPrefetchBackupInfoTask::~ObPrefetchBackupInfoTask()
@@ -1984,6 +1987,7 @@ int ObPrefetchBackupInfoTask::init(const ObLSBackupDagInitParam &param, const sh
     LOG_WARN("failed to assign param", K(ret), K(param));
   } else {
     report_ctx_ = report_ctx;
+    prefetch_task_id_ = ls_backup_ctx.get_prefetch_task_id();
     backup_data_type_ = backup_data_type;
     ls_backup_ctx_ = &ls_backup_ctx;
     provider_ = &provider;
@@ -2005,6 +2009,7 @@ int ObPrefetchBackupInfoTask::process()
 {
   int ret = OB_SUCCESS;
   int tmp_ret = OB_SUCCESS;
+  const int64_t start_ts = ObTimeUtility::current_time();
   bool need_report_error = false;
   if (IS_NOT_INIT) {
     ret = OB_NOT_INIT;
@@ -2042,6 +2047,8 @@ int ObPrefetchBackupInfoTask::process()
   if (OB_NOT_NULL(ls_backup_ctx_) && need_report_error) {
     REPORT_TASK_RESULT(this->get_dag()->get_dag_id(), ls_backup_ctx_->get_result_code());
   }
+  const int64_t cost_us = ObTimeUtility::current_time() - start_ts;
+  record_server_event_(cost_us);
   return ret;
 }
 
@@ -2377,7 +2384,6 @@ int ObPrefetchBackupInfoTask::generate_next_prefetch_dag_()
   int ret = OB_SUCCESS;
   int tmp_ret = OB_SUCCESS;
   ObPrefetchBackupInfoDag *child_dag = NULL;
-  int64_t prefetch_task_id = 0;
   ObTenantDagScheduler *scheduler = MTL(ObTenantDagScheduler *);
   ObIDagNet *dag_net = NULL;
   if (OB_ISNULL(scheduler) || OB_ISNULL(ls_backup_ctx_)) {
@@ -2388,9 +2394,8 @@ int ObPrefetchBackupInfoTask::generate_next_prefetch_dag_()
     LOG_WARN("dag net should not be NULL", K(ret), K(*this));
   } else if (OB_FAIL(scheduler->alloc_dag(child_dag))) {
     LOG_WARN("failed to alloc child dag", K(ret));
-  } else if (OB_FAIL(ls_backup_ctx_->get_prefetch_task_id(prefetch_task_id))) {
-    LOG_WARN("failed to get prefetch task id", K(ret));
-  } else if (OB_FAIL(child_dag->init(prefetch_task_id,
+  } else if (FALSE_IT(next_prefetch_task_id_ = ls_backup_ctx_->get_prefetch_task_id())) {
+  } else if (OB_FAIL(child_dag->init(next_prefetch_task_id_,
                  param_,
                  backup_data_type_,
                  report_ctx_,
@@ -2415,7 +2420,7 @@ int ObPrefetchBackupInfoTask::generate_next_prefetch_dag_()
       LOG_WARN("may exist same dag", K(ret));
     }
   } else {
-    LOG_INFO("success to alloc next prefetch dag", K(ret), K(prefetch_task_id), K_(param));
+    LOG_INFO("success to alloc next prefetch dag", K(ret), K_(param));
   }
   if (OB_FAIL(ret) && OB_NOT_NULL(scheduler) && OB_NOT_NULL(child_dag)) {
     scheduler->free_dag(*child_dag);
@@ -2473,6 +2478,7 @@ int ObPrefetchBackupInfoTask::generate_backup_dag_(
         LOG_WARN("may exist same dag", K(ret));
       }
     } else {
+      next_backup_task_id_ = task_id;
       LOG_INFO("success to alloc backup dag", K(ret), K(task_id), K_(backup_data_type), K(items));
     }
   }
@@ -2480,6 +2486,35 @@ int ObPrefetchBackupInfoTask::generate_backup_dag_(
     scheduler->free_dag(*child_dag);
   }
   return ret;
+}
+
+void ObPrefetchBackupInfoTask::record_server_event_(const int64_t cost_us)
+{
+  const char *prefetch_data_event = NULL;
+  if (backup_data_type_.is_sys_backup()) {
+    prefetch_data_event = "prefetch_sys_data";
+  } else if (backup_data_type_.is_minor_backup()) {
+    prefetch_data_event = "prefetch_minor_data";
+  } else if (backup_data_type_.is_major_backup()) {
+    prefetch_data_event = "prefetch_major_data";
+  }
+  int64_t pos = 0;
+  const int64_t buf_len = MAX_ROOTSERVICE_EVENT_EXTRA_INFO_LENGTH;
+  char buf[buf_len] = {};
+  if (-1 != next_prefetch_task_id_) {
+    (void)common::databuff_printf(buf, buf_len, pos, "prefetch_task_id:%ld -> next_prefetch_task_id:%ld",
+                                  prefetch_task_id_, next_prefetch_task_id_);
+  } else {
+    (void)common::databuff_printf(buf, buf_len, pos, "prefetch_task_id:%ld -> backup_task_id:%ld",
+                                  prefetch_task_id_, next_backup_task_id_);
+  }
+  SERVER_EVENT_ADD("backup", prefetch_data_event,
+      "tenant_id", param_.tenant_id_,
+      "backup_set_id", param_.backup_set_desc_.backup_set_id_,
+      "ls_id", param_.ls_id_.id(),
+      "turn_id", param_.turn_id_,
+      "retry_id", param_.retry_id_,
+      "cost_us", cost_us, buf);
 }
 
 /* ObLSBackupDataTask */
@@ -2501,7 +2536,8 @@ ObLSBackupDataTask::ObLSBackupDataTask()
       allocator_(),
       backup_items_(),
       finished_tablet_list_(),
-      index_rebuild_dag_(NULL)
+      index_rebuild_dag_(NULL),
+      next_prefetch_task_id_(-1)
 {}
 
 ObLSBackupDataTask::~ObLSBackupDataTask()
@@ -2590,6 +2626,8 @@ int ObLSBackupDataTask::process()
     LOG_WARN("failed to check ls valid for backup", K(ret), K_(param));
   } else if (OB_FAIL(do_write_file_header_())) {
     LOG_WARN("failed to do write file header", K(ret));
+  } else if (OB_FAIL(do_check_tablet_valid_())) {
+    LOG_WARN("failed to check tablet valid", K(ret));
   } else if (OB_FAIL(do_backup_macro_block_data_())) {
     LOG_WARN("failed to do backup macro block data", K(ret));
   } else if (OB_FAIL(do_backup_meta_data_())) {
@@ -2687,6 +2725,51 @@ int ObLSBackupDataTask::do_write_file_header_()
     LOG_WARN("failed to build backup file header", K(ret));
   } else if (OB_FAIL(backup_data_ctx_.write_backup_file_header(file_header))) {
     LOG_WARN("failed to write backup file header", K(ret), K(file_header));
+  }
+  return ret;
+}
+
+int ObLSBackupDataTask::get_check_tablet_list_(common::ObIArray<ObTabletID> &tablet_list)
+{
+  int ret = OB_SUCCESS;
+  tablet_list.reset();
+  ARRAY_FOREACH_X(backup_items_, idx, cnt, OB_SUCC(ret)) {
+    const ObBackupProviderItem &item = backup_items_.at(idx);
+    if (PROVIDER_ITEM_CHECK == item.get_item_type()) {
+      if (OB_FAIL(tablet_list.push_back(item.get_tablet_id()))) {
+        LOG_WARN("failed to push back", K(ret));
+      }
+    }
+  }
+  return ret;
+}
+
+int ObLSBackupDataTask::do_check_tablet_valid_()
+{
+  int ret = OB_SUCCESS;
+  ObArray<ObTabletID> tablet_list;
+  ObBackupTabletChecker *checker = NULL;
+  if (OB_ISNULL(ls_backup_ctx_)) {
+    ret = OB_ERR_UNEXPECTED;
+    LOG_WARN("ls backup ctx should not be null", K(ret));
+  } else if (OB_FALSE_IT(checker = &ls_backup_ctx_->tablet_checker_)) {
+  } else if (!backup_data_type_.is_major_backup()) {
+    // do nothing
+  } else if (OB_FAIL(get_check_tablet_list_(tablet_list))) {
+    LOG_WARN("failed to get check tablet list", K(ret));
+  } else {
+    ARRAY_FOREACH_X(tablet_list, idx, cnt, OB_SUCC(ret)) {
+      const ObTabletID &tablet_id = tablet_list.at(idx);
+      ObTabletHandle tablet_handle;
+      if (OB_ISNULL(checker)) {
+        ret = OB_ERR_UNEXPECTED;
+        LOG_WARN("checker should not be null", K(ret));
+      } else if (OB_FAIL(get_tablet_handle_(tablet_id, tablet_handle))) {
+        LOG_WARN("failed to get tablet handle", K(ret), K(tablet_id));
+      } else if (OB_FAIL(checker->check_tablet_valid(param_.tenant_id_, param_.ls_id_, tablet_id, tablet_handle))) {
+        LOG_WARN("failed to check tablet valid", K(ret), K_(param), K(tablet_id));
+      }
+    }
   }
   return ret;
 }
@@ -3236,7 +3319,6 @@ int ObLSBackupDataTask::do_generate_next_backup_dag_()
   ObLSBackupStage stage = LOG_STREAM_BACKUP_MAJOR;
   ObTenantDagScheduler *scheduler = MTL(ObTenantDagScheduler *);
   ObIDagNet *dag_net = NULL;
-  int64_t prefetch_task_id = 0;
   if (OB_ISNULL(scheduler) || OB_ISNULL(ls_backup_ctx_)) {
     ret = OB_ERR_UNEXPECTED;
     LOG_WARN("unexpected null MTL scheduler", K(ret), KP(scheduler), KP_(ls_backup_ctx));
@@ -3247,9 +3329,8 @@ int ObLSBackupDataTask::do_generate_next_backup_dag_()
   } else if (OB_ISNULL(dag_net = this->get_dag()->get_dag_net())) {
     ret = OB_ERR_UNEXPECTED;
     LOG_WARN("dag net should not be NULL", K(ret), K(*this));
-  } else if (OB_FAIL(ls_backup_ctx_->get_prefetch_task_id(prefetch_task_id))) {
-    LOG_WARN("failed to get prefetch task id", K(ret));
-  } else if (OB_FAIL(next_dag->init(prefetch_task_id,
+  } else if (FALSE_IT(next_prefetch_task_id_ = ls_backup_ctx_->get_prefetch_task_id())) {
+  } else if (OB_FAIL(next_dag->init(next_prefetch_task_id_,
                  dag_param,
                  backup_data_type_,
                  report_ctx_,
@@ -3310,20 +3391,18 @@ void ObLSBackupDataTask::record_server_event_(const int64_t cost_us) const
   } else if (backup_data_type_.is_major_backup()) {
     backup_data_event = "backup_major_data";
   }
-  SERVER_EVENT_ADD("backup",
-      backup_data_event,
-      "tenant_id",
-      param_.tenant_id_,
-      "backup_set_id",
-      param_.backup_set_desc_.backup_set_id_,
-      "ls_id",
-      param_.ls_id_.id(),
-      "retry_id",
-      param_.retry_id_,
-      "file_id",
-      task_id_,
-      "cost_us",
-      cost_us);
+  int64_t pos = 0;
+  const int64_t buf_len = MAX_ROOTSERVICE_EVENT_EXTRA_INFO_LENGTH;
+  char buf[buf_len] = {};
+  (void)common::databuff_printf(buf, buf_len, pos, "backup_task_id:%ld -> next_prefetch_task_id:%ld",
+                                task_id_, next_prefetch_task_id_);
+  SERVER_EVENT_ADD("backup", backup_data_event,
+      "tenant_id", param_.tenant_id_,
+      "backup_set_id", param_.backup_set_desc_.backup_set_id_,
+      "ls_id", param_.ls_id_.id(),
+      "turn_id", param_.turn_id_,
+      "retry_id", param_.retry_id_,
+      "cost_us", cost_us, buf);
 }
 
 int ObLSBackupDataTask::finish_backup_items_()
@@ -3954,8 +4033,7 @@ int ObLSBackupPrepareTask::process()
     } else if (OB_ISNULL(dag_net = this->get_dag()->get_dag_net())) {
       ret = OB_ERR_UNEXPECTED;
       LOG_WARN("dag net should not be NULL", K(ret), K(*this));
-    } else if (OB_FAIL(ls_backup_ctx_->get_prefetch_task_id(prefetch_task_id))) {
-      LOG_WARN("failed to get prefetch task id", K(ret));
+    } else if (FALSE_IT(prefetch_task_id = ls_backup_ctx_->prefetch_task_id_)) {
     } else if (OB_FAIL(child_dag->init(prefetch_task_id,
                    param_,
                    backup_data_type_,
