48f60666e0f6ccdf506c79c31d2b487af593883a
godyangfight <godyangfight@gmail.com>
2023-12-20 11:44:00 +0000

    [CP] [CP] Fix ls which is in migration is gc with all ls replica is not exist.

--- src/storage/ls/ob_ls.cpp
+++ src/storage/ls/ob_ls.cpp
@@ -2387,5 +2387,44 @@ bool ObLS::is_in_gc()
   return bret;
 }
 
+int ObLS::set_ls_migration_gc(
+    bool &allow_gc)
+{
+  int ret = OB_SUCCESS;
+  allow_gc = false;
+  const bool write_slog = true;
+  const ObMigrationStatus change_status = ObMigrationStatus::OB_MIGRATION_STATUS_GC;
+  ObMigrationStatus curr_status = ObMigrationStatus::OB_MIGRATION_STATUS_MAX;
+  ObLSRebuildInfo rebuild_info;
+  int64_t read_lock = LSLOCKLS;
+  int64_t write_lock = LSLOCKLOGMETA;
+  ObLSLockGuard lock_myself(this, lock_, read_lock, write_lock);
+
+  if (IS_NOT_INIT) {
+    ret = OB_NOT_INIT;
+    STORAGE_LOG(WARN, "ls is not inited", K(ret), K(ls_meta_));
+  } else if (!can_update_ls_meta(ls_meta_.get_ls_create_status())) {
+    ret = OB_STATE_NOT_MATCH;
+    STORAGE_LOG(WARN, "state not match, cannot update ls meta", K(ret), K(ls_meta_));
+  } else if (OB_FAIL(ls_meta_.get_rebuild_info(rebuild_info))) {
+    LOG_WARN("failed to get rebuild info", K(ret), K(ls_meta_));
+  } else if (rebuild_info.is_in_rebuild()) {
+    allow_gc = false;
+  } else if (OB_FAIL(ls_meta_.get_migration_status(curr_status))) {
+    LOG_WARN("failed to get migration status", K(ret), K(ls_meta_));
+  } else if (ObMigrationStatusHelper::check_allow_gc_abandoned_ls(curr_status)) {
+    allow_gc = true;
+  } else if (ObMigrationStatus::OB_MIGRATION_STATUS_NONE != curr_status) {
+    allow_gc = false;
+  } else if (OB_FAIL(ls_meta_.set_migration_status(change_status, write_slog))) {
+    LOG_WARN("failed to set migration status", K(ret), K(change_status));
+  } else {
+    ls_tablet_svr_.disable_to_read();
+    allow_gc = true;
+  }
+  return ret;
+}
+
+
 }
 }
