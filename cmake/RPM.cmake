d71fcd0bf6aaacbfe8e66c8924cd45bd4dceee34
obdev <obdev@oceanbase.com>
2023-12-25 06:13:22 +0000

    [CP] [CP] Upgrade obstack

--- cmake/RPM.cmake
+++ cmake/RPM.cmake
@@ -78,7 +78,7 @@ install(PROGRAMS
   ${CMAKE_BINARY_DIR}/tools/ob_admin/ob_admin
   tools/ob_admin/io_bench/bench_io.sh
   ${CMAKE_BINARY_DIR}/src/observer/observer
-  $<$<STREQUAL:"${ARCHITECTURE}","x86_64">:${DEVTOOLS_DIR}/bin/obstack>
+  ${DEVTOOLS_DIR}/bin/obstack
   DESTINATION bin
   COMPONENT server)
 endif()
