07e5f3ec05cac4df7fcfc6e1c223d7bb1f58f147
liucc1997 <1192520566@qq.com>
2024-01-18 07:14:58 +0000

    [CP] net.add_rpc_unix_listen remove OB_USE_BABASSL

--- src/observer/ob_srv_network_frame.cpp
+++ src/observer/ob_srv_network_frame.cpp
@@ -157,13 +157,9 @@ int ObSrvNetworkFrame::init()
     LOG_ERROR("high prio rpc net register fail", K(ret));
   } else if (OB_FAIL(ingress_service_.init(GCONF.cluster_id))) {
     LOG_ERROR("endpoint ingress service init fail", K(ret));
-  }
-#ifdef OB_USE_BABASSL
-  else if (OB_FAIL(net_.add_rpc_unix_listen(rpc_unix_path, rpc_handler_))) {
+  } else if (OB_FAIL(net_.add_rpc_unix_listen(rpc_unix_path, rpc_handler_))) {
     LOG_ERROR("listen rpc unix path fail");
-  }
-#endif
-  else {
+  } else {
     if (OB_FAIL(obrpc::global_poc_server.start(rpc_port, io_cnt, &deliver_))) {
       LOG_ERROR("poc rpc server start fail", K(ret));
     } else {
