859cfbd8277ecbdf20b8f93ab7d8d821e13aec95
obdev <obdev@oceanbase.com>
2024-01-18 12:18:17 +0000

    [CP] [CP] [GIS] fix s2 visitor exception process

--- deps/oblib/src/lib/geo/ob_geo_to_s2_visitor.cpp
+++ deps/oblib/src/lib/geo/ob_geo_to_s2_visitor.cpp
@@ -26,26 +26,44 @@ bool ObWkbToS2Visitor::prepare(ObGeometry *geo)
   return bret;
 }
 
-void ObWkbToS2Visitor::add_cell_from_point(S2Point point)
+int ObWkbToS2Visitor::add_cell_from_point(S2Point point)
 {
+  int ret = OB_SUCCESS;
   S2CellId cell_id = S2CellId(point).parent(options_.max_level());
-  S2cells_.push_back(cell_id);
+  if (OB_FAIL(vector_push_back<S2CellId>(S2cells_, cell_id))) {
+    LOG_WARN("failed to add cell id", K(ret));
+  }
+  return ret;
 }
 
-void ObWkbToS2Visitor::add_cell_from_point(S2LatLng point)
+int ObWkbToS2Visitor::add_cell_from_point(S2LatLng point)
 {
+  int ret = OB_SUCCESS;
   S2CellId cell_id = S2CellId(point).parent(options_.max_level());
-  S2cells_.push_back(cell_id);
+  if (OB_FAIL(vector_push_back<S2CellId>(S2cells_, cell_id))) {
+    LOG_WARN("failed to add cell id", K(ret));
+  }
+  return ret;
 }
 
 template<typename T_IBIN>
-S2Cell* ObWkbToS2Visitor::MakeS2Point(T_IBIN *geo)
+int ObWkbToS2Visitor::MakeS2Point(T_IBIN *geo, S2Cell *&res)
 {
+  int ret = OB_SUCCESS;
   S2LatLng latlng = S2LatLng::FromDegrees(geo->y(), geo->x());
-  add_cell_from_point(latlng);
-  mbr_ = mbr_.is_empty() ? S2LatLngRect(latlng, latlng) : mbr_.Union(S2LatLngRect(latlng, latlng));
-  S2Cell* p = new S2Cell(latlng);
-  return p;
+  if (OB_FAIL(add_cell_from_point(latlng))) {
+    LOG_WARN("failed to add cell from point", K(ret));
+  } else {
+    mbr_ = mbr_.is_empty() ? S2LatLngRect(latlng, latlng) : mbr_.Union(S2LatLngRect(latlng, latlng));
+    S2Cell* p = new S2Cell(latlng);
+    if (OB_ISNULL(p)) {
+      ret = OB_ERR_UNEXPECTED;
+      LOG_WARN("failed to alloc s2cell", K(ret));
+    } else {
+      res = p;
+    }
+  }
+  return ret;
 }
 
 double ObWkbToS2Visitor::stToUV(double s)
@@ -94,53 +112,90 @@ S2Point ObWkbToS2Visitor::MakeS2PointFromXy(double x, double y)
 }
 
 template<typename T_IBIN>
-S2Cell* ObWkbToS2Visitor::MakeProjS2Point(T_IBIN *geo)
+int ObWkbToS2Visitor::MakeProjS2Point(T_IBIN *geo, S2Cell *&res)
 {
+  int ret = OB_SUCCESS;
   S2Point point = MakeS2PointFromXy(geo->x(), geo->y());
   S2Cell* p = NULL;
   if (!invalid_) {
-    p = new S2Cell(point);
-    add_cell_from_point(point);
+    if (OB_FAIL(add_cell_from_point(point))) {
+      LOG_WARN("failed to add cell from point", K(ret));
+    } else {
+      p = new S2Cell(point);
+      if (OB_ISNULL(p)) {
+        ret = OB_ERR_UNEXPECTED;
+        LOG_WARN("failed to alloc s2cell", K(ret));
+      } else {
+        res = p;
+      }
+    }
   }
-  return p;
+  return ret;
 }
 
-template<typename T_IBIN, typename T_BIN>
-S2Polyline* ObWkbToS2Visitor::MakeS2Polyline(T_IBIN *geo)
+template<typename T_IBIN>
+int ObWkbToS2Visitor::MakeS2Polyline(T_IBIN *geo, S2Polyline *&res)
 {
+  int ret = OB_SUCCESS;
   std::vector<S2LatLng> vertices;
-  const T_BIN *line = reinterpret_cast<const T_BIN *>(geo->val());
-  typename T_BIN::iterator iter = line->begin();
-  for ( ; iter != line->end(); iter++) {
+  const typename T_IBIN::value_type *line = reinterpret_cast<const typename T_IBIN::value_type *>(geo->val());
+  typename T_IBIN::value_type::iterator iter = line->begin();
+  for ( ; iter != line->end() && OB_SUCC(ret); iter++) {
     S2LatLng latlng = S2LatLng::FromDegrees(iter->template get<1>(),
                                             iter->template get<0>());
-    add_cell_from_point(latlng);
-    vertices.push_back(latlng);
+    if (OB_FAIL(add_cell_from_point(latlng))) {
+      LOG_WARN("failed to add cell from point", K(ret));
+    } else if (OB_FAIL(vector_push_back<S2LatLng>(vertices, latlng))) {
+      LOG_WARN("failed to add vertice", K(ret));
+    }
   }
-  S2Polyline* ptr = new S2Polyline(vertices);
-  return ptr;
+  if (OB_SUCC(ret)) {
+    S2Polyline* ptr = new S2Polyline(vertices);
+    if (OB_ISNULL(ptr)) {
+      ret = OB_ERR_UNEXPECTED;
+      LOG_WARN("failed to alloc s2cell", K(ret));
+    } else {
+      res = ptr;
+    }
+  }
+
+  return ret;
 }
 
-template<typename T_IBIN, typename T_BIN>
-S2Polyline* ObWkbToS2Visitor::MakeProjS2Polyline(T_IBIN *geo)
+template<typename T_IBIN>
+int ObWkbToS2Visitor::MakeProjS2Polyline(T_IBIN *geo, S2Polyline *&res)
 {
+  int ret = OB_SUCCESS;
   std::vector<S2Point> vertices;
-  const T_BIN *line = reinterpret_cast<const T_BIN *>(geo->val());
-  typename T_BIN::iterator iter = line->begin();
-  for ( ; iter != line->end(); iter++) {
+  const typename T_IBIN::value_type *line = reinterpret_cast<const typename T_IBIN::value_type *>(geo->val());
+  typename T_IBIN::value_type::iterator iter = line->begin();
+  for ( ; iter != line->end() && OB_SUCC(ret); iter++) {
     S2Point p = MakeS2PointFromXy(iter->template get<0>(),
                                   iter->template get<1>());
-    add_cell_from_point(p);
-    vertices.push_back(p);
+    if (OB_FAIL(add_cell_from_point(p))) {
+      LOG_WARN("failed to add cell from point", K(ret));
+    } else if (OB_FAIL(vector_push_back<S2Point>(vertices, p))) {
+      LOG_WARN("failed to add vertice", K(ret));
+    }
   }
-  S2Polyline* ptr = new S2Polyline(vertices);
-  return ptr;
+
+ if (OB_SUCC(ret)) {
+    S2Polyline* ptr = new S2Polyline(vertices);
+    if (OB_ISNULL(ptr)) {
+      ret = OB_ERR_UNEXPECTED;
+      LOG_WARN("failed to alloc s2cell", K(ret));
+    } else {
+      res = ptr;
+    }
+  }
+  return ret;
 }
 
 template<typename T_IBIN, typename T_BIN,
          typename T_BIN_RING, typename T_BIN_INNER_RING>
-S2Polygon* ObWkbToS2Visitor::MakeS2Polygon(T_IBIN *geo)
+int ObWkbToS2Visitor::MakeS2Polygon(T_IBIN *geo, S2Polygon *&res)
 {
+  int ret = OB_SUCCESS;
   T_BIN& poly = *(T_BIN *)(geo->val());
   T_BIN_RING& exterior = poly.exterior_ring();
   T_BIN_INNER_RING& inner_rings = poly.inner_rings();
@@ -148,38 +203,71 @@ S2Polygon* ObWkbToS2Visitor::MakeS2Polygon(T_IBIN *geo)
   if (poly.size() != 0) {
     std::vector<S2Point> vertices;
     typename T_BIN_RING::iterator iter = exterior.begin();
-    for (; iter != exterior.end(); ++iter) {
+    for (; iter != exterior.end() && OB_SUCC(ret); ++iter) {
       S2LatLng latlng = S2LatLng::FromDegrees(iter->template get<1>(), iter->template get<0>());
-      add_cell_from_point(latlng);
-      vertices.push_back(S2Point(latlng));
+      S2Point tmp = S2Point(latlng);
+      if (OB_FAIL(add_cell_from_point(latlng))) {
+        LOG_WARN("failed to add cell from point", K(ret));
+      } else if (OB_FAIL(vector_push_back<S2Point>(vertices, tmp))) {
+        LOG_WARN("failed to add vertice", K(ret));
+      }
     }
-    S2Loop *loop = new S2Loop(vertices);
-    loop->Normalize();
-    s2poly.emplace_back(loop);
+    if (OB_SUCC(ret)) {
+      S2Loop *loop = new S2Loop(vertices);
+      if (OB_ISNULL(loop)) {
+        ret = OB_ERR_UNEXPECTED;
+        LOG_WARN("failed to alloc s2cell", K(ret));
+      } else {
+        loop->Normalize();
+        if (OB_FAIL(vector_emplace_back(s2poly, loop))) {
+          LOG_WARN("failed to add loop", K(ret));
+        }
+      }
+    }
   }
 
   typename T_BIN_INNER_RING::iterator iterInnerRing = inner_rings.begin();
-  for (; iterInnerRing != inner_rings.end(); ++iterInnerRing) {
+  for (; iterInnerRing != inner_rings.end() && OB_SUCC(ret); ++iterInnerRing) {
     std::vector<S2Point> vertices;
     typename T_BIN_RING::iterator iter = (*iterInnerRing).begin();
-    for (; iter != (*iterInnerRing).end(); ++iter) {
+    for (; iter != (*iterInnerRing).end() && OB_SUCC(ret); ++iter) {
       S2LatLng latlng = S2LatLng::FromDegrees(iter->template get<1>(), iter->template get<0>());
-      add_cell_from_point(latlng);
-      vertices.push_back(S2Point(latlng));
+      S2Point tmp = S2Point(latlng);
+      if (OB_FAIL(add_cell_from_point(latlng))) {
+        LOG_WARN("failed to add cell from point", K(ret));
+      } else if (OB_FAIL(vector_push_back<S2Point>(vertices, tmp))) {
+        LOG_WARN("failed to add vertice", K(ret));
+      }
     }
-    S2Loop *loop = new S2Loop(vertices);
-    loop->Normalize();
-    s2poly.emplace_back(loop);
+    if (OB_SUCC(ret)) {
+      S2Loop *loop = new S2Loop(vertices);
+      if (OB_ISNULL(loop)) {
+        ret = OB_ERR_UNEXPECTED;
+        LOG_WARN("failed to alloc s2cell", K(ret));
+      } else {
+        loop->Normalize();
+        if (OB_FAIL(vector_emplace_back(s2poly, loop))) {
+          LOG_WARN("failed to add loop", K(ret));
+        }
+      }
+    }
   }
 
   S2Polygon* py = new S2Polygon(std::move(s2poly));
-  return py;
+  if (OB_ISNULL(py)) {
+    ret = OB_ERR_UNEXPECTED;
+    LOG_WARN("failed to alloc s2cell", K(ret));
+  } else {
+    res = py;
+  }
+  return ret;
 }
 
 template<typename T_IBIN, typename T_BIN,
          typename T_BIN_RING, typename T_BIN_INNER_RING>
-S2Polygon* ObWkbToS2Visitor::MakeProjS2Polygon(T_IBIN *geo)
+int ObWkbToS2Visitor::MakeProjS2Polygon(T_IBIN *geo, S2Polygon *&res)
 {
+  int ret = OB_SUCCESS;
   T_BIN& poly = *(T_BIN *)(geo->val());
   T_BIN_RING& exterior = poly.exterior_ring();
   T_BIN_INNER_RING& inner_rings = poly.inner_rings();
@@ -187,43 +275,76 @@ S2Polygon* ObWkbToS2Visitor::MakeProjS2Polygon(T_IBIN *geo)
   if (poly.size() != 0) {
     std::vector<S2Point> vertices;
     typename T_BIN_RING::iterator iter = exterior.begin();
-    for (; iter != exterior.end(); ++iter) {
+    for (; iter != exterior.end() && OB_SUCC(ret); ++iter) {
       S2Point tmp = MakeS2PointFromXy(iter->template get<0>(), iter->template get<1>());
-      add_cell_from_point(tmp);
-      vertices.push_back(tmp);
+      if (OB_FAIL(add_cell_from_point(tmp))) {
+        LOG_WARN("failed to add cell from point", K(ret));
+      } else if (OB_FAIL(vector_push_back<S2Point>(vertices, tmp))) {
+        LOG_WARN("failed to add vertice", K(ret));
+      }
     }
-    S2Loop *loop = new S2Loop(vertices);
-    loop->Normalize();
-    s2poly.emplace_back(loop);
+    if (OB_SUCC(ret)) {
+      S2Loop *loop = new S2Loop(vertices);
+      if (OB_ISNULL(loop)) {
+        ret = OB_ERR_UNEXPECTED;
+        LOG_WARN("failed to alloc s2cell", K(ret));
+      } else {
+        loop->Normalize();
+        if (OB_FAIL(vector_emplace_back(s2poly, loop))) {
+          LOG_WARN("failed to add loop", K(ret));
+        }
+      }
+    }
   }
 
   typename T_BIN_INNER_RING::iterator iterInnerRing = inner_rings.begin();
-  for (; iterInnerRing != inner_rings.end(); ++iterInnerRing) {
+  for (; iterInnerRing != inner_rings.end() && OB_SUCC(ret); ++iterInnerRing) {
     std::vector<S2Point> vertices;
     typename T_BIN_RING::iterator iter = (*iterInnerRing).begin();
-    for (; iter != (*iterInnerRing).end(); ++iter) {
+    for (; iter != (*iterInnerRing).end() && OB_SUCC(ret); ++iter) {
       S2Point tmp = MakeS2PointFromXy(iter->template get<0>(), iter->template get<1>());
-      add_cell_from_point(tmp);
-      vertices.push_back(tmp);
+      if (OB_FAIL(add_cell_from_point(tmp))) {
+        LOG_WARN("failed to add cell from point", K(ret));
+      } else if (OB_FAIL(vector_push_back<S2Point>(vertices, tmp))) {
+        LOG_WARN("failed to add vertice", K(ret));
+      }
     }
-    S2Loop *loop = new S2Loop(vertices);
-    loop->Normalize();
-    s2poly.emplace_back(loop);
+    if (OB_SUCC(ret)) {
+      S2Loop *loop = new S2Loop(vertices);
+      if (OB_ISNULL(loop)) {
+        ret = OB_ERR_UNEXPECTED;
+        LOG_WARN("failed to alloc s2cell", K(ret));
+      } else {
+        loop->Normalize();
+        if (OB_FAIL(vector_emplace_back(s2poly, loop))) {
+          LOG_WARN("failed to add loop", K(ret));
+        }
+      }
+    }
   }
 
   S2Polygon* py = new S2Polygon(std::move(s2poly));
-  return py;
+  if (OB_ISNULL(py)) {
+    ret = OB_ERR_UNEXPECTED;
+    LOG_WARN("failed to alloc s2cell", K(ret));
+  } else {
+    res = py;
+  }
+  return ret;
 }
 
 
 int ObWkbToS2Visitor::visit(ObIWkbGeogPoint *geo)
 {
   INIT_SUCC(ret);
+  S2Cell *res = NULL;
   if (geo->length() < (WKB_GEO_BO_SIZE + WKB_GEO_TYPE_SIZE)) {
     ret = OB_ERR_GIS_INVALID_DATA;
     LOG_WARN("invalid swkb length", K(ret), K(geo->length()));
-  } else {
-    s2v_.emplace_back(MakeS2Point<ObIWkbGeogPoint>(geo));
+  } else if (OB_FAIL(MakeS2Point<ObIWkbGeogPoint>(geo, res))) {
+    LOG_WARN("failed to make s2 point", K(ret));
+  } else if (OB_FAIL(vector_emplace_back<S2Cell>(s2v_, res))) {
+    LOG_WARN("failed to add s2 cell", K(ret));
   }
   return ret;
 }
@@ -231,8 +352,13 @@ int ObWkbToS2Visitor::visit(ObIWkbGeogPoint *geo)
 int ObWkbToS2Visitor::visit(ObIWkbGeomPoint *geo)
 {
   INIT_SUCC(ret);
+  S2Cell *cell = nullptr;
   if (!invalid_) {
-    s2v_.emplace_back(MakeProjS2Point(geo));
+    if (OB_FAIL(MakeProjS2Point(geo, cell))) {
+      LOG_WARN("failed to make s2 point", K(ret));
+    } else if (OB_FAIL(vector_emplace_back<S2Cell>(s2v_, cell))) {
+      LOG_WARN("failed to add s2 cell", K(ret));
+    }
   }
   return ret;
 }
@@ -240,12 +366,15 @@ int ObWkbToS2Visitor::visit(ObIWkbGeomPoint *geo)
 int ObWkbToS2Visitor::visit(ObIWkbGeogLineString *geo)
 {
   INIT_SUCC(ret);
+  S2Polyline *polyline = nullptr;
   if (geo->length() < WKB_COMMON_WKB_HEADER_LEN) {
     ret = OB_ERR_GIS_INVALID_DATA;
     LOG_WARN("invalid swkb length", K(ret), K(geo->length()));
+  } else if (OB_FAIL(MakeS2Polyline<ObIWkbGeogLineString>(geo, polyline))) {
+    LOG_WARN("failed to make s2 poly line", K(ret), K(geo->length()));
+  } else if (OB_FAIL(vector_emplace_back<S2Polyline>(s2v_, polyline))) {
+    LOG_WARN("failed to add s2 cell", K(ret));
   } else {
-    S2Polyline *polyline = MakeS2Polyline<ObIWkbGeogLineString, ObWkbGeogLineString>(geo);
-    s2v_.emplace_back(polyline);
     mbr_ = mbr_.is_empty() ? polyline->GetRectBound() : mbr_.Union(polyline->GetRectBound());
   }
   return ret;
@@ -255,7 +384,12 @@ int ObWkbToS2Visitor::visit(ObIWkbGeomLineString *geo)
 {
   INIT_SUCC(ret);
   if (!invalid_) {
-    s2v_.emplace_back(MakeProjS2Polyline<ObIWkbGeomLineString, ObWkbGeomLineString>(geo));
+    S2Polyline *line = nullptr;
+    if (OB_FAIL(MakeProjS2Polyline<ObIWkbGeomLineString>(geo, line))) {
+      LOG_WARN("failed to make s2 poly line", K(ret));
+    } else if (OB_FAIL(vector_emplace_back<S2Polyline>(s2v_, line))) {
+      LOG_WARN("failed to add s2 cell", K(ret));
+    }
   }
   return ret;
 }
@@ -263,13 +397,16 @@ int ObWkbToS2Visitor::visit(ObIWkbGeomLineString *geo)
 int ObWkbToS2Visitor::visit(ObIWkbGeogPolygon *geo)
 {
   INIT_SUCC(ret);
+  S2Polygon *polygon = nullptr;
   if (geo->length() < WKB_COMMON_WKB_HEADER_LEN) {
     ret = OB_ERR_GIS_INVALID_DATA;
     LOG_WARN("invalid swkb length", K(ret), K(geo->length()));
+  } else if ((ret = MakeS2Polygon<ObIWkbGeogPolygon, ObWkbGeogPolygon,
+                                  ObWkbGeogLinearRing, ObWkbGeogPolygonInnerRings>(geo, polygon)) != OB_SUCCESS) {
+    LOG_WARN("failed to make s2 poly", K(ret), K(geo->length()));
+  } else if (OB_FAIL(vector_emplace_back<S2Polygon>(s2v_, polygon))) {
+    LOG_WARN("failed to add s2 polygon", K(ret));
   } else {
-    S2Polygon *polygon = MakeS2Polygon<ObIWkbGeogPolygon, ObWkbGeogPolygon,
-                                    ObWkbGeogLinearRing, ObWkbGeogPolygonInnerRings>(geo);
-    s2v_.emplace_back(polygon);
     mbr_ = mbr_.is_empty() ? polygon->GetRectBound() : mbr_.Union(polygon->GetRectBound());
   }
 
@@ -280,12 +417,15 @@ int ObWkbToS2Visitor::visit(ObIWkbGeomPolygon *geo)
 {
   INIT_SUCC(ret);
   if (!invalid_) {
+    S2Polygon *poly = nullptr;
     if (geo->length() < WKB_COMMON_WKB_HEADER_LEN) {
       ret = OB_ERR_GIS_INVALID_DATA;
       LOG_WARN("invalid swkb length", K(ret), K(geo->length()));
-    } else {
-      s2v_.emplace_back(MakeProjS2Polygon<ObIWkbGeomPolygon, ObWkbGeomPolygon,
-                                          ObWkbGeomLinearRing, ObWkbGeomPolygonInnerRings>(geo));
+    } else if ((ret = MakeProjS2Polygon<ObIWkbGeomPolygon, ObWkbGeomPolygon,
+                                        ObWkbGeomLinearRing, ObWkbGeomPolygonInnerRings>(geo, poly)) != OB_SUCCESS) {
+      LOG_WARN("failed to make s2 poly", K(ret), K(geo->length()));
+    } else if (OB_FAIL(vector_emplace_back<S2Polygon>(s2v_, poly))) {
+      LOG_WARN("failed to add s2 polygon", K(ret));
     }
   }
   return ret;
@@ -385,6 +525,39 @@ void ObWkbToS2Visitor::reset()
   has_reset_ = true;
 }
 
+template <typename ElementType>
+int ObWkbToS2Visitor::vector_push_back(std::vector<ElementType> &vector, ElementType &element)
+{
+  int ret = OB_SUCCESS;
+  try {
+    vector.push_back(element);
+  } catch(...) {
+    ret = ob_boost_geometry_exception_handle();
+  }
+  return ret;
+}
+
+int ObWkbToS2Visitor::vector_emplace_back(std::vector<std::unique_ptr<S2Loop>> &vector, S2Loop *element)
+{
+  int ret = OB_SUCCESS;
+  try {
+    vector.emplace_back(element);
+  } catch(...) {
+    ret = ob_boost_geometry_exception_handle();
+  }
+  return ret;
+}
+template <typename ElementType>
+int ObWkbToS2Visitor::vector_emplace_back(std::vector<std::unique_ptr<S2Region>> &vector, ElementType *element)
+{
+  int ret = OB_SUCCESS;
+  try {
+    vector.emplace_back(element);
+  } catch(...) {
+    ret = ob_boost_geometry_exception_handle();
+  }
+  return ret;
+}
 
 } // namespace common
 } // namespace oceanbase
