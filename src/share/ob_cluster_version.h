e3cd6986c4f9b924fc6e5b2c502aeded9270edb3
obdev <obdev@oceanbase.com>
2024-01-30 05:15:38 +0000

    升级序列增加4.2.1.3版本

--- src/share/ob_cluster_version.h
+++ src/share/ob_cluster_version.h
@@ -172,6 +172,7 @@ cal_version(const uint64_t major, const uint64_t minor, const uint64_t major_pat
 #define CLUSTER_VERSION_4_2_1_0 (oceanbase::common::cal_version(4, 2, 1, 0))
 #define CLUSTER_VERSION_4_2_1_1 (oceanbase::common::cal_version(4, 2, 1, 1))
 #define CLUSTER_VERSION_4_2_1_2 (oceanbase::common::cal_version(4, 2, 1, 2))
+#define CLUSTER_VERSION_4_2_1_3 (oceanbase::common::cal_version(4, 2, 1, 3))
 #define CLUSTER_VERSION_4_2_2_0 (oceanbase::common::cal_version(4, 2, 2, 0))
 #define CLUSTER_VERSION_4_2_3_0 (oceanbase::common::cal_version(4, 2, 3, 0))
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -193,6 +194,7 @@ cal_version(const uint64_t major, const uint64_t minor, const uint64_t major_pat
 #define DATA_VERSION_4_2_1_0 (oceanbase::common::cal_version(4, 2, 1, 0))
 #define DATA_VERSION_4_2_1_1 (oceanbase::common::cal_version(4, 2, 1, 1))
 #define DATA_VERSION_4_2_1_2 (oceanbase::common::cal_version(4, 2, 1, 2))
+#define DATA_VERSION_4_2_1_3 (oceanbase::common::cal_version(4, 2, 1, 3))
 #define DATA_VERSION_4_2_2_0 (oceanbase::common::cal_version(4, 2, 2, 0))
 #define DATA_VERSION_4_2_3_0 (oceanbase::common::cal_version(4, 2, 3, 0))
 
--- src/share/ob_upgrade_utils.h
+++ src/share/ob_upgrade_utils.h
@@ -173,7 +173,7 @@ public:
              const uint64_t cluster_version,
              uint64_t &data_version);
 public:
-  static const int64_t DATA_VERSION_NUM = 10;
+  static const int64_t DATA_VERSION_NUM = 11;
   static const uint64_t UPGRADE_PATH[];
 };
 
@@ -224,6 +224,7 @@ private:
   int post_upgrade_for_dbms_scheduler();
 };
 DEF_SIMPLE_UPGRARD_PROCESSER(4, 2, 1, 2)
+DEF_SIMPLE_UPGRARD_PROCESSER(4, 2, 1, 3)
 
 class ObUpgradeFor4220Processor : public ObBaseUpgradeProcessor
 {
