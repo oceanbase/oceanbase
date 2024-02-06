d4868b98f5598602776f195d7b44d1569bd5880f
qiuyg3 <101090778+qiuyg3@users.noreply.github.com>
2024-02-06 15:51:19 +0800

    Update test_ob_log_miner_record_converter.cpp
    oblogminer json output format support

--- unittest/logminer/test_ob_log_miner_record_converter.cpp
+++ unittest/logminer/test_ob_log_miner_record_converter.cpp
@@ -22,30 +22,18 @@ namespace oceanbase
 namespace oblogminer
 {
 
-TEST(test_ob_log_miner_record_converter, CsvConverterWriteType)
+TEST(test_ob_log_miner_record_converter, JsonConverterWriteType)
 {
-  ObLogMinerRecordCsvConverter converter;
+  ObLogMinerRecordJsonConverter converter;
   ObConcurrentFIFOAllocator alloc;
   ObStringBuffer str_buf(&alloc);
   KeyArray key_arr;
   EXPECT_EQ(OB_SUCCESS, alloc.init(1 << 20, 1 << 20, 1 << 13));
-  EXPECT_EQ(key_arr.push_back("aaa"), OB_SUCCESS);
-  EXPECT_EQ(key_arr.push_back("bbb"), OB_SUCCESS);
-  EXPECT_EQ(key_arr.push_back("ccc"), OB_SUCCESS);
-  EXPECT_EQ(OB_SUCCESS, converter.write_keys_(key_arr, str_buf));
-  EXPECT_STREQ("\"aaa/bbb/ccc\"", str_buf.ptr());
+  EXPECT_EQ(OB_SUCCESS, converter.write_json_key_("aaa", str_buf));
+  EXPECT_STREQ("\"aaa\":", str_buf.ptr());
   str_buf.reset();
-  EXPECT_EQ(OB_SUCCESS, converter.write_signed_number_(1111, str_buf));
-  EXPECT_STREQ("1111", str_buf.ptr());
-  str_buf.reset();
-  EXPECT_EQ(OB_SUCCESS, converter.write_signed_number_(-12345, str_buf));
-  EXPECT_STREQ("-12345", str_buf.ptr());
-  str_buf.reset();
-  EXPECT_EQ(OB_SUCCESS, converter.write_string_no_escape_("aaaaabbbb'", str_buf));
-  EXPECT_STREQ("\"aaaaabbbb'\"", str_buf.ptr());
-  str_buf.reset();
-  EXPECT_EQ(OB_SUCCESS, converter.write_string_escape_("'aaaa\"\"bbbbb'", str_buf));
-  EXPECT_STREQ("\"'aaaa\"\"\"\"bbbbb'\"", str_buf.ptr());
+  EXPECT_EQ(OB_SUCCESS, converter.write_json_string_escape_("'aaaa\"\"bbbbb'", str_buf));
+  EXPECT_STREQ("\"'aaaa\\\"\\\"bbbbb'\"", str_buf.ptr());
   str_buf.reset();
 }
 
@@ -213,6 +201,85 @@ TEST(test_ob_log_miner_record_converter, UndoSqlConverterWriteRecord)
   is_written = false;
   EXPECT_EQ(OB_SUCCESS, converter.write_record(*rec, str_buf, is_written));
   EXPECT_EQ(false, is_written);
+  destroy_miner_record(rec);
+}
+
+TEST(test_ob_log_miner_record_converter, JsonConverterWriteRecord)
+{
+  ObLogMinerRecordJsonConverter converter;
+  ObConcurrentFIFOAllocator alloc;
+  bool is_written = false;
+  EXPECT_EQ(OB_SUCCESS, alloc.init(1 << 20, 1 << 20, 1 << 13));
+  ObStringBuffer str_buf(&alloc);
+  ObLogMinerRecord *rec = nullptr;
+  EXPECT_EQ(OB_SUCCESS, converter.set_timezone("+8:00"));
+  const char *pkarr1[] = {"aaa", "bbb"};
+  const char *ukarr1[] = {"ccc"};
+  rec = build_logminer_record(alloc, lib::Worker::CompatMode::MYSQL,
+        1002, 1, "test_tenant", "test", "sbtest1", 345, pkarr1, sizeof(pkarr1)/ sizeof(const char*),
+        ukarr1, sizeof(ukarr1)/sizeof(const char*), "a/b/c/d/e", EINSERT, 1645539742222222,
+        "INSERT INTO `test`.`sbtest1` (`aaa`, `bbb`, `ccc`) VALUES ('1', '2', '3');",
+        "DELETE FROM `test`.`sbtest1` WHERE `aaa`='1' AND `bbb`='2' AND `ccc`='3' LIMIT 1;");
+  EXPECT_EQ(OB_SUCCESS, converter.write_record(*rec, str_buf, is_written));
+  EXPECT_EQ(true, is_written);
+  EXPECT_STREQ(
+    "{\"TENANT_ID\":1002,\"TRANS_ID\":345,\"PRIMARY_KEY\":\"aaa/bbb\",\"TENANT_NAME\":\"test_tenant\",\"DATABASE_NAME\":\"test\","
+    "\"TABLE_NAME\":\"sbtest1\",\"OPERATION\":\"INSERT\",\"OPERATION_CODE\":1,\"COMMIT_SCN\":1645539742222222000,"
+    "\"COMMIT_TIMESTAMP\":\"2022-02-22 22:22:22.222222\","
+    "\"SQL_REDO\":\"INSERT INTO `test`.`sbtest1` (`aaa`, `bbb`, `ccc`) VALUES ('1', '2', '3');\","
+    "\"SQL_UNDO\":\"DELETE FROM `test`.`sbtest1` WHERE `aaa`='1' AND `bbb`='2' AND `ccc`='3' LIMIT 1;\","
+    "\"ORG_CLUSTER_ID\":1}\n", str_buf.ptr());
+  str_buf.reset();
+  destroy_miner_record(rec);
+  rec = build_logminer_record(alloc, lib::Worker::CompatMode::MYSQL,
+        1002, 1, "test_tenant", "test", "sbtest2", 345, pkarr1, sizeof(pkarr1)/ sizeof(const char*),
+        ukarr1, sizeof(ukarr1)/sizeof(const char*), "a/b/c/d/e", EUPDATE, 0,
+        "UPDATE `test`.`sbtest2` SET `aaa`='44', `bbb`='55', `ccc`='66' WHERE `aaa`='11' AND `bbb`='22' AND `ccc`='33' LIMIT 1;",
+        "UPDATE `test`.`sbtest2` SET `aaa`='11', `bbb`='22', `ccc`='33' WHERE `aaa`='44' AND `bbb`='55' AND `ccc`='66' LIMIT 1;");
+  is_written = false;
+  EXPECT_EQ(OB_SUCCESS, converter.write_record(*rec, str_buf, is_written));
+  EXPECT_EQ(true, is_written);
+  EXPECT_STREQ(
+    "{\"TENANT_ID\":1002,\"TRANS_ID\":345,\"PRIMARY_KEY\":\"aaa/bbb\",\"TENANT_NAME\":\"test_tenant\","
+    "\"DATABASE_NAME\":\"test\",\"TABLE_NAME\":\"sbtest2\",\"OPERATION\":\"UPDATE\",\"OPERATION_CODE\":2,"
+    "\"COMMIT_SCN\":0,\"COMMIT_TIMESTAMP\":\"1970-01-01 08:00:00.000000\","
+    "\"SQL_REDO\":\"UPDATE `test`.`sbtest2` SET `aaa`='44', `bbb`='55', `ccc`='66' WHERE `aaa`='11' AND `bbb`='22' AND `ccc`='33' LIMIT 1;\","
+    "\"SQL_UNDO\":\"UPDATE `test`.`sbtest2` SET `aaa`='11', `bbb`='22', `ccc`='33' WHERE `aaa`='44' AND `bbb`='55' AND `ccc`='66' LIMIT 1;\","
+    "\"ORG_CLUSTER_ID\":1}\n", str_buf.ptr());
+  str_buf.reset();
+  destroy_miner_record(rec);
+  rec = build_logminer_record(alloc, lib::Worker::CompatMode::MYSQL,
+        1002, 1, "test_tenant", "test", "", 345, nullptr,0,
+        nullptr, 0, nullptr, EDDL, 4611686018427387,
+        "CREATE TABLE `sbtest2` (\n  `aaa` varchar(100) NOT NULL,\n  `bbb` varchar(100) NOT NULL,\n  `ccc` varchar(100) DEFAULT NULL\n);",
+        nullptr);
+  is_written = false;
+  EXPECT_EQ(OB_SUCCESS, converter.write_record(*rec, str_buf, is_written));
+  EXPECT_EQ(true, is_written);
+  EXPECT_STREQ(
+    "{\"TENANT_ID\":1002,\"TRANS_ID\":345,\"PRIMARY_KEY\":\"\",\"TENANT_NAME\":\"test_tenant\",\"DATABASE_NAME\":\"test\","
+    "\"TABLE_NAME\":\"\",\"OPERATION\":\"DDL\",\"OPERATION_CODE\":4,\"COMMIT_SCN\":4611686018427387000,\"COMMIT_TIMESTAMP\":\"2116-02-21 07:53:38.427387\","
+    "\"SQL_REDO\":\"CREATE TABLE `sbtest2` (\\n  `aaa` varchar(100) NOT NULL,\\n  `bbb` varchar(100) NOT NULL,\\n  `ccc` varchar(100) DEFAULT NULL\\n);\",\"SQL_UNDO\":\"\","
+    "\"ORG_CLUSTER_ID\":1}\n", str_buf.ptr());
+  str_buf.reset();
+  destroy_miner_record(rec);
+  rec = build_logminer_record(alloc, lib::Worker::CompatMode::MYSQL,
+        1002, 1, "test_tenant", "test", "sbtest1", 345, pkarr1, sizeof(pkarr1)/ sizeof(const char*),
+        ukarr1, sizeof(ukarr1)/sizeof(const char*), "a/b/c/d/e", EDELETE, 0,
+        "DELETE FROM `test`.`sbtest1` WHERE `aaa`='1' AND `bbb`='2' AND `ccc`='3' LIMIT 1;",
+        "INSERT INTO `test`.`sbtest1` (`aaa`, `bbb`, `ccc`) VALUES ('1', '2', '3');"
+        );
+  is_written = false;
+  EXPECT_EQ(OB_SUCCESS, converter.write_record(*rec, str_buf, is_written));
+  EXPECT_EQ(true, is_written);
+    EXPECT_STREQ(
+    "{\"TENANT_ID\":1002,\"TRANS_ID\":345,\"PRIMARY_KEY\":\"aaa/bbb\",\"TENANT_NAME\":\"test_tenant\",\"DATABASE_NAME\":\"test\","
+    "\"TABLE_NAME\":\"sbtest1\",\"OPERATION\":\"DELETE\",\"OPERATION_CODE\":3,\"COMMIT_SCN\":0,"
+    "\"COMMIT_TIMESTAMP\":\"1970-01-01 08:00:00.000000\","
+    "\"SQL_REDO\":\"DELETE FROM `test`.`sbtest1` WHERE `aaa`='1' AND `bbb`='2' AND `ccc`='3' LIMIT 1;\","
+    "\"SQL_UNDO\":\"INSERT INTO `test`.`sbtest1` (`aaa`, `bbb`, `ccc`) VALUES ('1', '2', '3');\","
+    "\"ORG_CLUSTER_ID\":1}\n", str_buf.ptr());
+  str_buf.reset();
   destroy_miner_record(rec);
 }
 
