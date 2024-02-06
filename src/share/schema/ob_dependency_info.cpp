30e8204f09229ade32dd7a9341b181281ec8ff0d
obdev <obdev@oceanbase.com>
2024-02-06 12:04:19 +0000

    [CP] fix circular dependencies lead to inconsistent schema status

--- src/share/schema/ob_dependency_info.cpp
+++ src/share/schema/ob_dependency_info.cpp
@@ -405,6 +405,15 @@ int ObDependencyInfo::collect_all_dep_objs(uint64_t tenant_id,
                                            common::ObISQLClient &sql_proxy,
                                            common::ObIArray<std::pair<uint64_t, share::schema::ObObjectType>> &objs)
 {
+  return collect_all_dep_objs_inner(tenant_id, ref_obj_id, ref_obj_id, sql_proxy, objs);
+}
+
+int ObDependencyInfo::collect_all_dep_objs_inner(uint64_t tenant_id,
+                                                 uint64_t root_obj_id,
+                                                 uint64_t ref_obj_id,
+                                                 common::ObISQLClient &sql_proxy,
+                                                 common::ObIArray<std::pair<uint64_t, share::schema::ObObjectType>> &objs)
+{
   int ret = OB_SUCCESS;
   ObSqlString sql;
   const uint64_t exec_tenant_id = gen_meta_tenant_id(tenant_id);
@@ -433,7 +442,7 @@ int ObDependencyInfo::collect_all_dep_objs(uint64_t tenant_id,
                       || tmp_type >= static_cast<int64_t> (share::schema::ObObjectType::MAX_TYPE)) {
             ret = OB_ERR_UNEXPECTED;
             LOG_WARN("get wrong obj type", K(ret));
-          } else if (ref_obj_id == tmp_obj_id) {
+          } else if (ref_obj_id == tmp_obj_id || root_obj_id == tmp_obj_id) {
             // skip
           } else if (has_exist_in_array(objs, {static_cast<uint64_t> (tmp_obj_id), static_cast<share::schema::ObObjectType> (tmp_type)})) {
             // dedpulicate
@@ -459,7 +468,7 @@ int ObDependencyInfo::collect_all_dep_objs(uint64_t tenant_id,
     LOG_WARN("too deep recusive", K(ret));
   } else {
     for (int64_t i = init_count; OB_SUCC(ret) && i < objs.count(); ++i) {
-      if (OB_FAIL(collect_all_dep_objs(tenant_id, objs.at(i).first, sql_proxy, objs))) {
+      if (OB_FAIL(collect_all_dep_objs_inner(tenant_id, root_obj_id, objs.at(i).first, sql_proxy, objs))) {
         LOG_WARN("failed to collect all dep objs", K(ret), K(objs.count()), K(init_count), K(i));
       }
     }
