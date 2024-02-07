269de7a960ca8b8585d9128fdf3a4168a1bf4cef
nroskill <nroskill@gmail.com>
2024-02-07 19:31:24 +0000

    [CP] [OPT] thread stack lock

--- deps/oblib/src/lib/thread/protected_stack_allocator.h
+++ deps/oblib/src/lib/thread/protected_stack_allocator.h
@@ -15,7 +15,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
-#include "lib/lock/ob_mutex.h"
+#include "lib/lock/ob_latch.h"
 
 namespace oceanbase {
 namespace lib {
@@ -62,10 +62,10 @@ public:
   public:
     Guard(StackMgr& mgr) : mgr_(mgr), cur_(nullptr)
     {
-      mgr_.mutex_.lock();
+      mgr_.rwlock_.rdlock(common::ObLatchIds::DEFAULT_SPIN_RWLOCK);
       cur_ = mgr_.dummy_.next_;
     }
-    ~Guard() { mgr_.mutex_.unlock(); }
+    ~Guard() { mgr_.rwlock_.unlock(); }
     ObStackHeader* operator*() { return (cur_ == &(mgr_.dummy_)) ? nullptr : cur_; }
     ObStackHeader* next()
     {
@@ -86,7 +86,7 @@ private:
   ObStackHeader *begin() { return dummy_.next_; }
   ObStackHeader *end() { return &dummy_; }
 private:
-  lib::ObMutex mutex_;
+  common::ObLatch rwlock_;
   ObStackHeader dummy_;
 };
 
