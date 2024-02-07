eb6065f4b2cf81b7e69fa1594d6e6dac4c915c14
obdev <obdev@oceanbase.com>
2024-02-07 16:00:42 +0000

    [CP] fix dup table virtual table core and clean unused log

--- src/storage/tx/ob_dup_table_util.cpp
+++ src/storage/tx/ob_dup_table_util.cpp
@@ -1827,11 +1827,9 @@ int ObDupTableLoopWorker::iterate_dup_ls(ObDupLSTabletSetStatIterator &collect_i
           DUP_TABLE_LOG(WARN, "dup ls handler not init", K(ret), K(cur_ls_id),
                         KPC(cur_dup_ls_handler));
         } else if (OB_FAIL(cur_dup_ls_handler->get_ls_tablet_set_stat(collect_iter))) {
-          DUP_TABLE_LOG(WARN, "collect lease mgr stat failed", K(ret), K(cur_ls_id),
+          DUP_TABLE_LOG(WARN, "collect tablet set stat failed", K(ret), K(cur_ls_id),
                         KPC(cur_dup_ls_handler));
         }
-        DUP_TABLE_LOG(WARN, "iter dup ls handler", K(ret), K(cur_ls_id),
-              KPC(cur_dup_ls_handler));
       }
     }
   }
@@ -1866,7 +1864,7 @@ int ObDupTableLoopWorker::iterate_dup_ls(ObDupLSTabletsStatIterator &collect_ite
           DUP_TABLE_LOG(WARN, "dup ls handler not init", K(ret), K(cur_ls_id),
                         KPC(cur_dup_ls_handler));
         } else if (OB_FAIL(cur_dup_ls_handler->get_ls_tablets_stat(collect_iter))) {
-          DUP_TABLE_LOG(WARN, "collect lease mgr stat failed", K(ret), K(cur_ls_id),
+          DUP_TABLE_LOG(WARN, "collect tablets stat failed", K(ret), K(cur_ls_id),
                         KPC(cur_dup_ls_handler));
         }
       }
