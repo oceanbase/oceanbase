128c9c514321535e653d6b470d74b8d7a70b700b
SanmuWangZJU <sanmuwang.ws@gmail.com>
2024-02-07 19:04:44 +0000

    [CP] [OBCDC] Support multi-version ls while using online schema

--- src/logservice/libobcdc/src/ob_log_ls_getter.h
+++ src/logservice/libobcdc/src/ob_log_ls_getter.h
@@ -16,33 +16,54 @@
 #include "lib/hash/ob_linear_hash_map.h"        // ObLinearHashMap
 #include "ob_log_systable_helper.h"         // ObLogSysTableHelper
 #include "ob_log_tenant.h"
+#include "ob_cdc_tenant_query.h"
 
 namespace oceanbase
 {
 namespace libobcdc
 {
+
+typedef ObArray<share::ObLSID> LSIDArray;
+
+class TenantLSQueryer : public ObCDCTenantQuery<LSIDArray>
+{
+public:
+  TenantLSQueryer(const int64_t snapshot_ts_ns, common::ObMySQLProxy &sql_proxy)
+    : ObCDCTenantQuery(sql_proxy), snapshot_ts_ns_(snapshot_ts_ns) {}
+  ~TenantLSQueryer() { snapshot_ts_ns_ = OB_INVALID_TIMESTAMP; }
+private:
+  int build_sql_statement_(const uint64_t tenant_id, ObSqlString &sql) override;
+  int parse_row_(common::sqlclient::ObMySQLResult &sql_result, ObCDCQueryResult<LSIDArray> &result) override;
+private:
+  static const char* QUERY_LS_INFO_SQL_FORMAT;
+private:
+  int64_t snapshot_ts_ns_;
+};
+
 class ObLogLsGetter
 {
 public:
   ObLogLsGetter();
   ~ObLogLsGetter();
-  int init(const common::ObIArray<uint64_t> &tenant_ids);
+  int init(const common::ObIArray<uint64_t> &tenant_ids, const int64_t start_tstamp_ns);
   void destroy();
 
   int get_ls_ids(
       const uint64_t tenant_id,
+      const int64_t snapshot_ts,
       common::ObIArray<share::ObLSID> &ls_id_array);
 
 private:
   int query_and_set_tenant_ls_info_(
-      const uint64_t tenant_id);
+      const uint64_t tenant_id,
+      const int64_t snapshot_ts);
 
   int query_tenant_ls_info_(
       const uint64_t tenant_id,
-      ObLogSysTableHelper::TenantLSIDs &tenant_ls_ids);
+      const int64_t snapshot_ts,
+      LSIDArray &ls_array);
 
 private:
-  typedef ObArray<share::ObLSID> LSIDArray;
   typedef common::ObLinearHashMap<TenantID, LSIDArray> TenantLSIDsCache;
 
   bool is_inited_;
