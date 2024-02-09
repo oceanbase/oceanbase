9b9d246b5a149d7d7a139a5e35b60c229fea8e23
obdev <obdev@oceanbase.com>
2024-02-09 21:31:24 +0000

    [CP] Enable re_est_cost for prefix sort

--- src/sql/resolver/dml/ob_hint.h
+++ src/sql/resolver/dml/ob_hint.h
@@ -144,6 +144,7 @@ struct ObGlobalHint {
 #define COMPAT_VERSION_4_0        (oceanbase::common::cal_version(4, 0, 0, 0))
 #define COMPAT_VERSION_4_2_1      (oceanbase::common::cal_version(4, 2, 1, 0))
 //#define COMPAT_VERSION_4_2_1_BP3  (oceanbase::common::cal_version(4, 2, 1, 3))
+#define COMPAT_VERSION_4_2_1_BP4  (oceanbase::common::cal_version(4, 2, 1, 4))
 #define COMPAT_VERSION_4_2_2      (oceanbase::common::cal_version(4, 2, 2, 0))
 #define COMPAT_VERSION_4_3_0      (oceanbase::common::cal_version(4, 3, 0, 0))
 #define LASTED_COMPAT_VERSION     COMPAT_VERSION_4_3_0
