2c5f36312150940956f140cc26dd40dab27e5353
obdev <obdev@oceanbase.com>
2024-02-19 08:28:58 +0000

    [CP] Fix correctness issues caused by ps parameterization

--- src/sql/plan_cache/ob_sql_parameterization.cpp
+++ src/sql/plan_cache/ob_sql_parameterization.cpp
@@ -873,6 +873,13 @@ int ObSqlParameterization::transform_tree(TransformTreeCtx &ctx,
           } else {
             ctx.not_param_ = not_param;
             ctx.ignore_scale_check_ = ignore_scale_check;
+            // select a + 1 as 'a' from t where b = ?; 'a' in SQL will be recognized as a constant by fast parser
+            // In the ps parameterization scenario, 'a' will be added to the param store as a fixed parameter in
+            // the execute phase. The param store has two parameters, causing correctness problems.
+            // Therefore, the scene ps parameterization ability of specifying aliases needs to be disabled.
+            if (T_ALIAS == root->type_ && NULL != root->str_value_) {
+              ctx.sql_info_->ps_need_parameterized_ = false;
+            }
             if (T_ALIAS == root->type_ && 0 == i) {
               // alias node的param_num_处理必须等到其第一个子节点转换完之后
               // select a + 1 as 'a'，'a'不能被参数化，但是它在raw_params数组内的下标必须是计算了1的下标之后才能得到
