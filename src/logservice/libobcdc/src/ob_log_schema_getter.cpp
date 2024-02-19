f8283be513f6dec347df73641a8472399f821416
SanmuWangZJU <sanmuwang.ws@gmail.com>
2024-02-19 08:06:26 +0000

    [CP] [OBCDC] Fix obcdc launch failed if tenant is not normal or dropped but not in tb_white_list

--- src/logservice/libobcdc/src/ob_log_schema_getter.cpp
+++ src/logservice/libobcdc/src/ob_log_schema_getter.cpp
@@ -930,8 +930,10 @@ int ObLogSchemaGetter::get_tenant_refreshed_schema_version(const uint64_t tenant
   return ret;
 }
 
-int ObLogSchemaGetter::check_if_tenant_is_dropping_or_dropped(const uint64_t tenant_id,
-    bool &is_tenant_dropping_or_dropped)
+int ObLogSchemaGetter::check_if_tenant_is_dropping_or_dropped(
+    const uint64_t tenant_id,
+    bool &is_tenant_dropping_or_dropped,
+    TenantSchemaInfo &tenant_schema_info)
 {
   int ret = OB_SUCCESS;
   ObSchemaGetterGuard guard;
@@ -952,6 +954,11 @@ int ObLogSchemaGetter::check_if_tenant_is_dropping_or_dropped(const uint64_t ten
     }
   } else {
     is_tenant_dropping_or_dropped = tenant_schema->is_dropping();
+    tenant_schema_info.reset(
+        tenant_id,
+        tenant_schema->get_schema_version(),
+        tenant_schema->get_tenant_name(),
+        tenant_schema->is_restore());
   }
 
   return ret;
--- src/logservice/libobcdc/src/ob_log_schema_getter.h
+++ src/logservice/libobcdc/src/ob_log_schema_getter.h
@@ -473,8 +473,10 @@ public:
   ///
   /// @retval OB_SUCCESS                   success
   /// @retval other error code             fail
-  virtual int check_if_tenant_is_dropping_or_dropped(const uint64_t tenant_id,
-      bool &is_tenant_dropping_or_dropped) = 0;
+  virtual int check_if_tenant_is_dropping_or_dropped(
+      const uint64_t tenant_id,
+      bool &is_tenant_dropping_or_dropped,
+      TenantSchemaInfo &tenant_schema_info) = 0;
 
   //// NOTE: Note that all subsequent additions to the interface should consider the return value case when the tenant does not exist.
   //// I: requires the schema module to return a specific error code OB_TENANT_HAS_BEEN_DROPPED when a tenant does not exist. libobcdc encounters this error code and only this error code will determine that the tenant has been deleted and exit the retry loop
@@ -617,8 +619,10 @@ public:
   }
   int get_tenant_refreshed_schema_version(const uint64_t tenant_id, int64_t &version);
 
-  int check_if_tenant_is_dropping_or_dropped(const uint64_t tenant_id,
-      bool &is_tenant_dropping_or_dropped);
+  int check_if_tenant_is_dropping_or_dropped(
+      const uint64_t tenant_id,
+      bool &is_tenant_dropping_or_dropped,
+      TenantSchemaInfo &tenant_schema_info);
 
 public:
   int init(common::ObMySQLProxy &mysql_proxy,
