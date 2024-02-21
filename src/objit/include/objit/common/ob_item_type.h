64768c3063d24a4048b3b8ea9ae4fb769c19e592
oceanoverflow <oceanoverflow@gmail.com>
2024-02-21 10:18:30 +0000

    add new pcode for change external storage dest

--- src/objit/include/objit/common/ob_item_type.h
+++ src/objit/include/objit/common/ob_item_type.h
@@ -2417,6 +2417,7 @@ typedef enum ObItemType
   T_ALTER_TABLE_FORCE,
   T_SHOW_PROCEDURE_CODE,
   T_SHOW_FUNCTION_CODE,
+  T_CHANGE_EXTERNAL_STORAGE_DEST,
   T_MAX //Attention: add a new type before T_MAX
 } ObItemType;
 
