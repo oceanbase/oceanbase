d78b950fe53de20dde23903b7669ba5a21dfe509
qingsuijiu <642782632@qq.com>
2024-03-14 07:15:05 +0000

    [CP] Handling the issue of 4662 masking the real error code.

--- src/sql/engine/px/exchange/ob_px_fifo_coord_op.cpp
+++ src/sql/engine/px/exchange/ob_px_fifo_coord_op.cpp
@@ -242,7 +242,8 @@ int ObPxFifoCoordOp::fetch_rows(const int64_t row_cnt)
   } else if (OB_UNLIKELY(OB_SUCCESS != ret)) {
     int ret_terminate = terminate_running_dfos(coord_info_.dfo_mgr_);
     LOG_WARN("QC get error code", K(ret), K(ret_terminate));
-    if (OB_ERR_SIGNALED_IN_PARALLEL_QUERY_SERVER == ret
+    if ((OB_ERR_SIGNALED_IN_PARALLEL_QUERY_SERVER == ret
+        || OB_GOT_SIGNAL_ABORTING == ret)
         && OB_SUCCESS != ret_terminate) {
       ret = ret_terminate;
     }
