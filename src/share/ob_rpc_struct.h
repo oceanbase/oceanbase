9fcaaaf5a78fb4301b2ef5056c7bc210650bc489
fanfangzhou <937806648@qq.com>
2024-03-20 03:45:21 +0000

    CreateTableRes add do_nothing_ place hold

--- src/share/ob_rpc_struct.h
+++ src/share/ob_rpc_struct.h
@@ -1041,18 +1041,21 @@ public:
   ObCreateTableRes() :
       table_id_(OB_INVALID_ID),
       schema_version_(OB_INVALID_VERSION),
-      task_id_(0)
+      task_id_(0),
+      do_nothing_(false)
   {}
   int assign(const ObCreateTableRes &other) {
     table_id_ = other.table_id_;
     schema_version_ = other.schema_version_;
     task_id_ = other.task_id_;
+    do_nothing_ = other.do_nothing_;
     return common::OB_SUCCESS;
   }
-  TO_STRING_KV(K_(table_id), K_(schema_version), K_(task_id));
+  TO_STRING_KV(K_(table_id), K_(schema_version), K_(task_id), K_(do_nothing));
   uint64_t table_id_;
   int64_t schema_version_;
   int64_t task_id_;
+  bool do_nothing_;
 };
 
 struct ObCreateTableLikeArg : public ObDDLArg
