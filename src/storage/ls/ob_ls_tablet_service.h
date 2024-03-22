0b9d35681446ada30194baf09fb2621a7566b276
godyangfight <godyangfight@gmail.com>
2024-03-22 08:46:43 +0000

    Fix restore transfer in tablet mistake rollback bug.

--- src/storage/ls/ob_ls_tablet_service.h
+++ src/storage/ls/ob_ls_tablet_service.h
@@ -196,7 +196,8 @@ public:
       ObTabletHandle &tablet_handle);
   int rollback_remove_tablet(
       const share::ObLSID &ls_id,
-      const common::ObTabletID &tablet_id);
+      const common::ObTabletID &tablet_id,
+      const share::SCN &transfer_start_scn);
 
   int get_tablet(
       const common::ObTabletID &tablet_id,
@@ -782,7 +783,11 @@ private:
   int create_empty_shell_tablet(
       const ObMigrationTabletParam &param,
       ObTabletHandle &tablet_handle);
-
+  int check_rollback_tablet_is_same_(
+      const share::ObLSID &ls_id,
+      const common::ObTabletID &tablet_id,
+      const share::SCN &transfer_start_scn,
+      bool &is_same);
 private:
   int direct_insert_rows(const uint64_t table_id,
                          const int64_t task_id,
