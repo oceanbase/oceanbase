75468d1276cab4a9bde37f85b51e77c5509dffbf
yishenglanlingzui <395329313@qq.com>
2024-04-02 15:48:05 +0000

    [CP] fix create outline with error -4016

--- src/sql/parser/ob_fast_parser.h
+++ src/sql/parser/ob_fast_parser.h
@@ -624,6 +624,12 @@ protected:
   int extend_alloc_sql_buffer();
 	int process_format_token();
 protected:
+  enum FoundInsertTokenStatus
+  {
+    NOT_FOUND_INSERT_TOKEN,
+    FOUND_INSERT_TOKEN_ONCE, // find one insert token
+    INVALID_TOKEN_STATUS,  // find insert token more than one time
+  };
 	ObRawSql raw_sql_;
 	char *no_param_sql_;
 	int64_t no_param_sql_len_;
@@ -646,7 +652,7 @@ public:
 	common::ObIAllocator &allocator_;
 	common::ObCharsetType charset_type_;
 	const ObCharsetInfo *charset_info_;
-	bool get_insert_;
+	FoundInsertTokenStatus found_insert_status_;
 	int64_t values_token_pos_;
 	ParseNextTokenFunc parse_next_token_func_;
 	ProcessIdfFunc process_idf_func_;
