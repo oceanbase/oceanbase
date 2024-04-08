73cc1ad0611844177957f31dd3be8f79fef3cb37
wangt1xiuyi <13547954130@163.com>
2024-04-08 07:24:46 +0000

    [CP] fix some optimizer stats bug

--- src/sql/das/ob_das_location_router.h
+++ src/sql/das/ob_das_location_router.h
@@ -331,6 +331,7 @@ public:
   }
   int save_success_task(const common::ObTabletID &succ_id)
   { return succ_tablet_list_.push_back(succ_id); }
+  bool is_refresh_location_error(int err_no) const;
   TO_STRING_KV(K(all_tablet_list_));
 private:
   int get_vt_svr_pair(uint64_t vt_id, const VirtualSvrPair *&vt_svr_pair);
