3082affb2edbcfca10fbb9464c12d95cdbbb9bd3
sdc <njucssdc@gmail.com>
2024-04-16 16:37:02 +0000

    [CP] fix shared hj sync close + channel sync deadlock

--- src/sql/engine/ob_operator.cpp
+++ src/sql/engine/ob_operator.cpp
@@ -1120,10 +1120,6 @@ int ObOperator::get_next_row()
         }
       } else if (OB_ITER_END == ret) {
         row_reach_end_ = true;
-        int tmp_ret = do_drain_exch();
-        if (OB_SUCCESS != tmp_ret) {
-          LOG_WARN("drain exchange data failed", K(tmp_ret));
-        }
         if (got_first_row_) {
           op_monitor_info_.last_row_time_ = oceanbase::common::ObClockGenerator::getClock();
         }
@@ -1144,6 +1140,12 @@ int ObOperator::get_next_row()
       }
     }
   }
+  if (OB_FAIL(ret)) {
+    int tmp_ret = do_drain_exch();
+    if (OB_SUCCESS != tmp_ret) {
+      LOG_WARN("drain exchange data failed", K(tmp_ret));
+    }
+  }
   end_ash_line_id_reg();
   end_cpu_time_counting();
   return ret;
@@ -1152,6 +1154,7 @@ int ObOperator::get_next_row()
 int ObOperator::get_next_batch(const int64_t max_row_cnt, const ObBatchRows *&batch_rows)
 {
   int ret = OB_SUCCESS;
+  bool need_drain = false;
   begin_cpu_time_counting();
   begin_ash_line_id_reg();
 
@@ -1260,10 +1263,7 @@ int ObOperator::get_next_batch(const int64_t max_row_cnt, const ObBatchRows *&ba
           got_first_row_ = true;
         }
         if (brs_.end_) {
-          int tmp_ret = do_drain_exch();
-          if (OB_SUCCESS != tmp_ret) {
-            LOG_WARN("drain exchange data failed", K(tmp_ret));
-          }
+          need_drain = true;
           op_monitor_info_.last_row_time_ = ObClockGenerator::getClock();
         }
       }
@@ -1284,6 +1284,12 @@ int ObOperator::get_next_batch(const int64_t max_row_cnt, const ObBatchRows *&ba
     }
   }
 
+  if (need_drain || OB_FAIL(ret)) {
+    int tmp_ret = do_drain_exch();
+    if (OB_SUCCESS != tmp_ret) {
+      LOG_WARN("drain exchange data failed", K(tmp_ret));
+    }
+  }
   end_ash_line_id_reg();
   end_cpu_time_counting();
   return ret;
