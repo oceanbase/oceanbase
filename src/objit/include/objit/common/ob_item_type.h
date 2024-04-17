fd27ea26a9f5b85912246d419a9be79a93ef2088
18523270951@163.com <18523270951@163.com>
2024-04-17 14:46:11 +0000

    placeholder for check table

--- src/objit/include/objit/common/ob_item_type.h
+++ src/objit/include/objit/common/ob_item_type.h
@@ -2472,6 +2472,7 @@ typedef enum ObItemType
   T_ALTER_LOGFILE_GROUP,
   T_DROP_LOGFILE_GROUP,
   T_SET_UNION_ALL,
+  T_SHOW_CHECK_TABLE,
 
   T_MAX //Attention: add a new type before T_MAX
 } ObItemType;
