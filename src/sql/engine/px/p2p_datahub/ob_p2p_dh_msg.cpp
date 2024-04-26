4c9e07bc3c3b1c200f6fc07e6749d6602f75a99c
obdev <obdev@oceanbase.com>
2024-04-26 04:17:52 +0000

    [CP] Fixed the deadlock issue in DM and P2P orthogonal scenarios.

--- src/sql/engine/px/p2p_datahub/ob_p2p_dh_msg.cpp
+++ src/sql/engine/px/p2p_datahub/ob_p2p_dh_msg.cpp
@@ -133,6 +133,21 @@ int ObP2PDatahubMsgBase::process_msg_internal(bool &need_free)
     need_free = true;
   } else {
     need_merge = false; // set success, not need to merge
+    int reg_dm_ret = OB_SUCCESS;
+#ifdef ERRSIM
+    int reg_dm_ret = OB_E(EventTable::EN_PX_P2P_MSG_REG_DM_FAILED) OB_SUCCESS;
+    if (OB_SUCCESS != reg_dm_ret) {
+      LOG_WARN("p2p msg reg dm failed by design", K(ret));
+      reg_dm_ret = OB_ALLOCATE_MEMORY_FAILED;
+    }
+#endif
+    if (OB_SUCCESS == reg_dm_ret) {
+      reg_dm_ret = ObDetectManagerUtils::p2p_datahub_register_check_item_into_dm(
+          register_dm_info_, dh_key, dm_cb_node_seq_id_);
+    }
+    if (OB_SUCCESS != reg_dm_ret) {
+      LOG_WARN("[DM] failed to register check item to dm", K(reg_dm_ret));
+    }
   }
 
   // merge filter
