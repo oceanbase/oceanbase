13bd9c88e55a16be26ab613e979094f34f3924d6
haohao022 <haohaohm@outlook.com>
2024-05-27 03:06:24 +0000

    [to #57003700] chore: alter get_cursor error message

--- src/sql/session/ob_sql_session_info.cpp
+++ src/sql/session/ob_sql_session_info.cpp
@@ -1504,7 +1504,7 @@ ObPLCursorInfo *ObSQLSessionInfo::get_cursor(int64_t cursor_id)
 {
   ObPLCursorInfo *cursor = NULL;
   if (OB_SUCCESS != pl_cursor_cache_.pl_cursor_map_.get_refactored(cursor_id, cursor)) {
-    LOG_WARN_RET(OB_ERR_UNEXPECTED, "get cursor info failed", K(cursor_id), K(get_sessid()));
+    LOG_INFO("get cursor info failed", K(cursor_id), K(get_sessid()));
   }
   return cursor;
 }
