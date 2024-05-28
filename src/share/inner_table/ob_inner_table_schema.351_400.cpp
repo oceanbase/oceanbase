ed5f4de50deeb0e2ebe77ab671fd7e5d29029a6b
LoLolobster <949673574@qq.com>
2024-05-28 04:16:08 +0000

    [INNER TABLE] Add column for quick restore related inner tables and views

--- src/share/inner_table/ob_inner_table_schema.351_400.cpp
+++ src/share/inner_table/ob_inner_table_schema.351_400.cpp
@@ -6090,6 +6090,25 @@ int ObInnerTableSchema::all_tenant_info_schema(ObTableSchema &table_schema)
       max_ls_id_default,
       max_ls_id_default); //default_value
   }
+
+  if (OB_SUCC(ret)) {
+    ObObj restore_data_mode_default;
+    restore_data_mode_default.set_varchar(ObString::make_string("NORMAL"));
+    ADD_COLUMN_SCHEMA_T("restore_data_mode", //column_name
+      ++column_id, //column_id
+      0, //rowkey_id
+      0, //index_id
+      0, //part_key_pos
+      ObVarcharType, //column_type
+      CS_TYPE_INVALID, //column_collation_type
+      128, //column_length
+      -1, //column_precision
+      -1, //column_scale
+      false, //is_nullable
+      false, //is_autoincrement
+      restore_data_mode_default,
+      restore_data_mode_default); //default_value
+  }
   table_schema.set_index_using_type(USING_BTREE);
   table_schema.set_row_store_type(ENCODING_ROW_STORE);
   table_schema.set_store_format(OB_STORE_FORMAT_DYNAMIC_MYSQL);
