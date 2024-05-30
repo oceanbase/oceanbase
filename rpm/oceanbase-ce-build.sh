40c8c3d3284c64558f56daf07a8b50f1eeb8efc7
wenxingsen <wenxingsen@qq.com>
2024-05-30 10:01:29 +0000

    [cp from master] adjust nolse package entrance

--- rpm/oceanbase-ce-build.sh
+++ rpm/oceanbase-ce-build.sh
@@ -8,7 +8,10 @@ RELEASE=$4
 CURDIR=$PWD
 TOP_DIR=`pwd`/../
 
-echo "[BUILD] args: TOP_DIR=${TOP_DIR} PROJECT_NAME=${PROJECT_NAME} VERSION=${VERSION} RELEASE=${RELEASE}"
+OB_DISABLE_LSE_OPTION=""
+[[ $OB_DISABLE_LSE == "1" ]] && OB_DISABLE_LSE_OPTION="-DOB_DISABLE_LSE=ON"
+
+echo "[BUILD] args: TOP_DIR=${TOP_DIR} PROJECT_NAME=${PROJECT_NAME} VERSION=${VERSION} RELEASE=${RELEASE} ${OB_DISABLE_LSE_OPTION}"
 
 cd ${TOP_DIR}
 ./build.sh clean
@@ -16,6 +19,7 @@ cd ${TOP_DIR}
     rpm                     \
     -DOB_RELEASEID=$RELEASE \
     -DBUILD_NUMBER=$RELEASE \
+    ${OB_DISABLE_LSE_OPTION}\
     --init                  \
     --make rpm || exit 1
 
