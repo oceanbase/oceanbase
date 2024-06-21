d8831113b0f6501e395453e22739c9ef4b965b5e
WeiXinChan <chenwx6728@163.com>
2024-06-21 13:25:29 +0000

    [Bugfix] fix htable lock memory leak

--- src/observer/table/ob_table_query_and_mutate_processor.cpp
+++ src/observer/table/ob_table_query_and_mutate_processor.cpp
@@ -140,10 +140,6 @@ int ObTableQueryAndMutateP::try_process()
     LOG_WARN("fail to get tablet id", K(ret), K(arg_.table_id_));
   } else if (OB_FAIL(get_ls_id(tablet_id_, ls_id))) {
     LOG_WARN("fail to get ls id", K(ret), K_(tablet_id));
-  } else if (is_hkv && OB_FAIL(HTABLE_LOCK_MGR->acquire_handle(lock_handle))) {
-    LOG_WARN("fail to get htable lock handle", K(ret));
-  } else if (is_hkv && OB_FAIL(ObHTableUtils::lock_htable_row(table_id_, query, *lock_handle, ObHTableLockMode::EXCLUSIVE))) {
-    LOG_WARN("fail to lock htable row", K(ret), K_(table_id), K(query));
   } else if (OB_FAIL(check_table_has_global_index(exist_global_index))) {
     LOG_WARN("fail to check global index", K(ret), K_(table_id));
   } else if (OB_FAIL(start_trans(false, /* is_readonly */
@@ -152,6 +148,10 @@ int ObTableQueryAndMutateP::try_process()
                                  get_timeout_ts(),
                                  exist_global_index))) {
     LOG_WARN("fail to start readonly transaction", K(ret));
+  } else if (is_hkv && OB_FAIL(HTABLE_LOCK_MGR->acquire_handle(lock_handle))) {
+    LOG_WARN("fail to get htable lock handle", K(ret));
+  } else if (is_hkv && OB_FAIL(ObHTableUtils::lock_htable_row(table_id_, query, *lock_handle, ObHTableLockMode::EXCLUSIVE))) {
+    LOG_WARN("fail to lock htable row", K(ret), K_(table_id), K(query));
   } else if (is_hkv && FALSE_IT(lock_handle->set_tx_id(get_trans_desc()->tid()))) {
   } else {
     ObTableQMParam qm_param(arg_.query_and_mutate_);
