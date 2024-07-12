c135a45003ad6152889d168f5cd1adf53e1e0716
jingtaoye35 <1255153887@qq.com>
2024-07-12 13:37:29 +0000

    support join join on on

--- src/sql/parser/parse_node.c
+++ src/sql/parser/parse_node.c
@@ -951,3 +951,36 @@ ParseNode *append_child(void *malloc_pool, int *error_code, ParseNode *left_node
   }
   return ret_node;
 }
+
+ParseNode *adjust_inner_join_inner(int *error_code, ParseNode *inner_join, ParseNode *table_node)
+{
+  ParseNode *ret_node = NULL;
+  if (OB_ISNULL(error_code)) {
+    (void)fprintf(stderr, "ERROR parser error code is NULL\n");
+  } else if (OB_ISNULL(inner_join) || OB_ISNULL(table_node)) {
+    *error_code = OB_PARSER_ERR_UNEXPECTED;
+  } else if (table_node->type_ == T_JOINED_TABLE && table_node->value_ != 1) {
+    // table_node is a join table and without a parenthese.
+    if (OB_ISNULL(table_node->children_[0])) {
+      *error_code = OB_PARSER_ERR_UNEXPECTED;
+    } else {
+      if (T_JOIN_RIGHT == table_node->children_[0]->type_) {
+        table_node->children_[2] = adjust_inner_join_inner(error_code, inner_join, table_node->children_[2]);
+      } else {
+        table_node->children_[1] = adjust_inner_join_inner(error_code, inner_join, table_node->children_[1]);
+      }
+    }
+    if (OB_PARSER_SUCCESS != *error_code) {
+      /* do nothing */
+    } else if (OB_ISNULL(table_node->children_[1]) ||
+               OB_ISNULL(table_node->children_[2])) {
+      *error_code = OB_PARSER_ERR_UNEXPECTED;
+    } else {
+      ret_node = table_node;
+    }
+  } else {
+    inner_join->children_[2] = table_node;
+    ret_node = inner_join;
+  }
+  return ret_node;
+}
