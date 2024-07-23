fcec0e5d734d60396640d30649a48dbf35b191b3
Revendell <2312237896@qq.com>
2024-07-23 17:03:42 +0000

    modify sysstat name

--- deps/oblib/src/lib/statistic_event/ob_stat_event.h
+++ deps/oblib/src/lib/statistic_event/ob_stat_event.h
@@ -582,8 +582,8 @@ STAT_EVENT_ADD_DEF(SS_MICRO_CACHE_DDL_PREWARM_BYTES, "ss_micro_cache ddl prewarm
 STAT_EVENT_ADD_DEF(SS_TMPFILE_CACHE_HIT, "ss_tmpfile_cache hit count", ObStatClassIds::CACHE, 240017, true, true, true)
 STAT_EVENT_ADD_DEF(SS_TMPFILE_CACHE_MISS, "ss_tmpfile_cache miss count", ObStatClassIds::CACHE, 240018, true, true, true)
 STAT_EVENT_ADD_DEF(SS_MICRO_CACHE_USED_DISK_SIZE, "ss_micro_cache total used disk size", ObStatClassIds::CACHE, 240019, true, true, true)
-STAT_EVENT_ADD_DEF(SS_PREFETCH_MACRO_CACHE_HIT, "ss_prefetch_macro_cache hit count", ObStatClassIds::CACHE, 240020, true, true, true)
-STAT_EVENT_ADD_DEF(SS_PREFETCH_MACRO_CACHE_MISS, "ss_prefetch_macro_cache miss count", ObStatClassIds::CACHE, 240021, true, true, true)
+STAT_EVENT_ADD_DEF(SS_MAJOR_MACRO_CACHE_HIT, "ss_major_macro_cache hit count", ObStatClassIds::CACHE, 240020, true, true, true)
+STAT_EVENT_ADD_DEF(SS_MAJOR_MACRO_CACHE_MISS, "ss_major_macro_cache miss count", ObStatClassIds::CACHE, 240021, true, true, true)
 
 //end
 STAT_EVENT_ADD_DEF(STAT_EVENT_ADD_END, "event add end", ObStatClassIds::DEBUG, 1, false, false, true)
@@ -693,7 +693,7 @@ STAT_EVENT_SET_DEF(SS_LOCAL_CACHE_TMPFILE_USED_DISK_SIZE_W, "ss_local_cache tmpf
 STAT_EVENT_SET_DEF(SS_LOCAL_CACHE_TMPFILE_USED_DISK_SIZE_R, "ss_local_cache tmpfile read cache used disk size", ObStatClassIds::CACHE, 245007, false, true, true)
 STAT_EVENT_SET_DEF(SS_LOCAL_CACHE_META_USED_DISK_SIZE, "ss_local_cache meta file used disk size", ObStatClassIds::CACHE, 245008, false, true, true)
 STAT_EVENT_SET_DEF(SS_LOCAL_CACHE_INCREMENTAL_DATA_USED_DISK_SIZE, "ss_local_cache incremental data used disk size", ObStatClassIds::CACHE, 245009, false, true, true)
-STAT_EVENT_SET_DEF(SS_PREFETCH_MACRO_CACHE_USED_DISK_SIZE, "ss_prefetch_macro_cache used disk size", ObStatClassIds::CACHE, 245010, false, true, true)
+STAT_EVENT_SET_DEF(SS_LOCAL_CACHE_MAJOR_MACRO_USED_DISK_SIZE, "ss_local_cache major macro used disk size", ObStatClassIds::CACHE, 245010, false, true, true)
 
 
 // END
