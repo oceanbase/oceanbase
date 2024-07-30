4abcf1da4a68009a2d3d731fdb82b0adf08433c1
YangEfei <yangyifei96@outlook.com>
2024-07-30 13:25:34 +0000

    [TABLELOCK] placeholder for the log_type of table lock splitting

--- src/logservice/ob_log_base_type.h
+++ src/logservice/ob_log_base_type.h
@@ -154,6 +154,9 @@ enum ObLogBaseType
   // for vector index
   VEC_INDEX_LOG_BASE_TYPE = 48,
 
+  // for table lock split
+  TABLE_LOCK_LOG_BASE_TYPE = 49,
+
   // pay attention!!!
   // add log type in log_base_type_to_string
   // max value
@@ -262,6 +265,8 @@ int log_base_type_to_string(const ObLogBaseType log_type,
     strncpy(str ,"SHARE_STORAGE_NRT_THROT_SERVICE", str_len);
   } else if (log_type == SHARED_STORAGE_PRE_WARM_LOG_BASE_TYPE) {
     strncpy(str ,"SHARED_STORAGE_PRE_WARM_LOG_BASE_TYPE", str_len);
+  } else if (log_type == TABLE_LOCK_LOG_BASE_TYPE) {
+    strncpy(str, "TABLE_LOCK_LOG_BASE_TYPE", str_len);
   } else if (log_type == VEC_INDEX_LOG_BASE_TYPE) {
     strncpy(str ,"VEC_INDEX_SERVICE", str_len);
   } else {
