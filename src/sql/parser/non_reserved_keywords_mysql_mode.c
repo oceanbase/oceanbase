3ac8382c6e32d77b0fec45593d6d7a95b74fe58e
swjtu-wenxiang <swjtu_wenxiang@sina.com>
2024-07-31 14:09:47 +0000

    [CP] [to #57727207]Word status not need to be SQL_KEYWORD in pl mysql mode.

--- src/sql/parser/non_reserved_keywords_mysql_mode.c
+++ src/sql/parser/non_reserved_keywords_mysql_mode.c
@@ -1097,7 +1097,6 @@ static const NonReservedKeyword Mysql_sql_keywords_in_pl[] =
   {"show", SHOW},
   {"slave", SLAVE},
   {"start", START},
-  {"status", STATUS},
   {"stop", STOP},
   {"table", TABLE},
   {"tables", TABLES},
