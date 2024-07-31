6b213ac761e3b87ccdc4d7c5135c865f3352f716
zhaoyiping0622 <zhaoyiping0622@gmail.com>
2024-07-31 02:41:57 +0000

    [CP] remove upgrade_virtual_schema when upgrade_post not across version

--- tools/upgrade/upgrade_pre.py
+++ tools/upgrade/upgrade_pre.py
@@ -1405,8 +1405,6 @@
 #  across_version = upgrade_across_version(cur)
 #  if across_version:
 #    run_upgrade_job(conn, cur, "UPGRADE_ALL", timeout)
-#  else:
-#    run_upgrade_job(conn, cur, "UPGRADE_VIRTUAL_SCHEMA", timeout)
 #
 #  run_root_inspection(cur, timeout)
 #####========******####======== actions begin ========####******========####
