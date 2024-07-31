7b550e117a22f3d30e8ac541d53dfb49ccd536c1
obdev <obdev@oceanbase.com>
2024-07-31 03:34:35 +0000

    upgrade 4_3_2_release version to 4.3.2.1

--- src/share/ob_upgrade_utils.h
+++ src/share/ob_upgrade_utils.h
@@ -174,7 +174,7 @@ public:
              const uint64_t cluster_version,
              uint64_t &data_version);
 public:
-  static const int64_t DATA_VERSION_NUM = 17;
+  static const int64_t DATA_VERSION_NUM = 18;
   static const uint64_t UPGRADE_PATH[];
 };
 
@@ -259,6 +259,7 @@ private:
   int post_upgrade_for_online_estimate_percent();
 };
 
+DEF_SIMPLE_UPGRARD_PROCESSER(4, 3, 2, 1)
 /* =========== special upgrade processor end   ============= */
 
 /* =========== upgrade processor end ============= */
