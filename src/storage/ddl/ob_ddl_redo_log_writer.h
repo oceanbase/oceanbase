466ba5117198d722ed6d8d67b5b9f597860fbbd8
obdev <obdev@oceanbase.com>
2024-08-15 05:34:31 +0000

    fix alloc concurrently not safe.

--- src/storage/ddl/ob_ddl_redo_log_writer.h
+++ src/storage/ddl/ob_ddl_redo_log_writer.h
@@ -124,7 +124,7 @@ public:
                       const int64_t task_id,
                       ObDDLNeedStopWriteChecker &checker,
                       int64_t &real_sleep_us);
-
+  ObIAllocator &get_allocator() { return allocator_; }
 private:
   struct SpeedHandleKey {
     public:
@@ -203,7 +203,7 @@ private:
   static const int64_t MAP_BUCKET_NUM  = 1024;
   bool is_inited_;
   common::hash::ObHashMap<SpeedHandleKey, ObDDLCtrlSpeedItem*> speed_handle_map_;
-  common::ObArenaAllocator allocator_;
+  common::ObConcurrentFIFOAllocator allocator_;
   common::ObBucketLock bucket_lock_;
   RefreshSpeedHandleTask refreshTimerTask_;
 };
