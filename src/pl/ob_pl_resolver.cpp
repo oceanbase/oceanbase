6d5b99f818d3068086a4b1ab2b7315d65c5a0367
haohao022 <haohaohm@outlook.com>
2024-08-28 18:08:50 +0000

    [CP] [to #2024082300104235906] add type deduce for record
    Co-authored-by: LiuYoung00 <liuyanglo_ol@163.com>

--- src/pl/ob_pl_resolver.cpp
+++ src/pl/ob_pl_resolver.cpp
@@ -12252,6 +12252,9 @@ int ObPLResolver::resolve_qualified_name(ObQualifiedName &q_name,
   //"case a when b xx when c xx" to "case when a == b then xx case when a == c then xx"
   if (OB_SUCC(ret)) {
     bool transformed = false;
+    if (!ObObjUDTUtil::ob_is_supported_sql_udt(expr->get_result_type().get_udt_id())) {
+      OZ (formalize_expr(*expr)); // bugfix: 53193337, need get real type in that case
+    }
     OZ(ObTransformPreProcess::transform_expr(unit_ast.get_expr_factory(),
                                               resolve_ctx_.session_info_, expr,
                                               transformed));
