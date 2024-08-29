6c1ab0ec49fc4ba4f46e0e6139da17248d658cfb
zzg19950727 <1071026277@qq.com>
2024-08-29 08:18:38 +0000

    [CP] fix log_exchange compute op_ordering bug

--- src/sql/optimizer/ob_log_exchange.cpp
+++ src/sql/optimizer/ob_log_exchange.cpp
@@ -363,7 +363,9 @@ int ObLogExchange::compute_op_ordering()
   } else if (is_producer()) {
     // for FULL_INPUT_SAMPLE, we cache all rows in transmit and send in random range
     // to avoid send to one worker at one time if input order is the same with %sort_keys_
-    is_local_order_ = FULL_INPUT_SAMPLE == sample_type_;
+    if (FULL_INPUT_SAMPLE == sample_type_) {
+      is_local_order_ = true;
+    }
   } else if (is_consumer()) {
     if (is_merge_sort_) {
       if (OB_UNLIKELY(sort_keys_.empty())) {
