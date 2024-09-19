452a37174818a6e5606450e505911666fcaab871
a1iive <h.hows@foxmail.com>
2024-09-19 07:45:54 +0000

    [VECTOR] modify DEFAULT_OUTBUF_CAPACITY

--- src/share/vector_index/ob_plugin_vector_index_serialize.h
+++ src/share/vector_index/ob_plugin_vector_index_serialize.h
@@ -187,7 +187,7 @@ public:
   int serialize(void *index, ObOStreamBuf::CbParam &cb_param, ObOStreamBuf::Callback &cb, const int64_t capacity = DEFAULT_OUTBUF_CAPACITY);
   int deserialize(void *&index, ObIStreamBuf::CbParam &cb_param, ObIStreamBuf::Callback &cb);
 private:
-  static const int64_t DEFAULT_OUTBUF_CAPACITY = 64LL * 1024LL; // 64KB
+  static const int64_t DEFAULT_OUTBUF_CAPACITY = 2LL * 1024LL * 1024LL; // 2MB
 
 private:
   bool is_inited_;
