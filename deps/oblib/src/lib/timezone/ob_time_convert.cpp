efe43914d2e744de809627341f0a37d1a2ff733a
hwx65 <1780011298@qq.com>
2024-09-23 20:17:46 +0000

    fix mysql_date related cases

--- deps/oblib/src/lib/timezone/ob_time_convert.cpp
+++ deps/oblib/src/lib/timezone/ob_time_convert.cpp
@@ -341,9 +341,6 @@ int ObTimeConverter::int_to_mdatetime(int64_t int_part, int64_t dec_part,
   dec_part = (dec_part + 500) / 1000;
   if (0 == int_part) {
     value = MYSQL_ZERO_DATETIME;
-    if (0 != dec_part) {
-      ret = OB_INVALID_DATE_VALUE;
-    }
   } else {
     ObTime ob_time(DT_TYPE_MYSQL_DATETIME);
     if (OB_FAIL(int_to_ob_time_with_date(int_part, ob_time, date_sql_mode))) {
