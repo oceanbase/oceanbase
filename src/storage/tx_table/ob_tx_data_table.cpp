3217713886d7183d8bff0d361a8c5ad8ad85a43b
ZenoWang <wzybuaasoft@163.com>
2024-10-10 09:46:24 +0000

    check tx data pre-process commit verions in lock protect

--- src/storage/tx_table/ob_tx_data_table.cpp
+++ src/storage/tx_table/ob_tx_data_table.cpp
@@ -784,13 +784,14 @@ int ObTxDataTable::get_upper_trans_version_before_given_scn(const SCN sstable_en
 
   if (OB_FAIL(ret)) {
   } else if (skip_calc) {
-  } else if (0 == calc_upper_trans_version_cache_.commit_versions_.array_.count()) {
-    STORAGE_LOG(ERROR, "Unexpected empty array.", K(calc_upper_trans_version_cache_));
   } else {
     TCRLockGuard lock_guard(calc_upper_trans_version_cache_.lock_);
-    if (!calc_upper_trans_version_cache_.commit_versions_.is_valid()) {
+    if (0 == calc_upper_trans_version_cache_.commit_versions_.array_.count()) {
+      ret = OB_EAGAIN;
+      STORAGE_LOG(WARN, "empty commit versions. may be a concurrent transfer.", K(calc_upper_trans_version_cache_));
+    } else if (!calc_upper_trans_version_cache_.commit_versions_.is_valid()) {
       ret = OB_ERR_UNEXPECTED;
-      STORAGE_LOG(ERROR, "invalid cache for upper trans version calculation", KR(ret));
+      STORAGE_LOG(WARN, "invalid cache for upper trans version calculation. ", KR(ret));
     } else if (OB_FAIL(calc_upper_trans_scn_(sstable_end_scn, upper_trans_version))) {
       STORAGE_LOG(WARN, "calc upper trans version failed", KR(ret), "ls_id", get_ls_id());
     } else {
