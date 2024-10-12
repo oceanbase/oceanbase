c1f35b1480d477af3cb812a60728c9bc32403a8f
qijiax <qijia_xu@163.com>
2024-10-12 05:08:26 +0000

    placeholder for unnest expr

--- src/objit/include/objit/common/ob_item_type.h
+++ src/objit/include/objit/common/ob_item_type.h
@@ -2612,6 +2612,8 @@ typedef enum ObItemType
   T_TRANSFORM_DISTINCT_AGG = 4743,
   T_NO_TRANSFORM_DISTINCT_AGG = 4744,
 
+  T_UNNEST_EXPRESSION = 4745,
+
   T_MAX //Attention: add a new type before T_MAX
 } ObItemType;
 
--- src/sql/engine/basic/ob_json_table_op.h
+++ src/sql/engine/basic/ob_json_table_op.h
@@ -48,6 +48,7 @@ enum table_type : int8_t {
   OB_JSON_TABLE = 1,
   OB_XML_TABLE = 2,
   OB_RB_ITERATE_TABLE = 3,
+  OB_UNNEST_TABLE = 4,
 };
 
 typedef enum JtNodeType {
