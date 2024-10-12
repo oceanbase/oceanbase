fb6e80f1f6866e98f9f467788eae6ee08e2da56f
Tyshawn <tuyunshan@gmail.com>
2024-10-12 03:55:30 +0000

    [BUG.FIX] fix duplicated memtable in tablet memtable array

--- src/storage/tablet/ob_table_store_util.cpp
+++ src/storage/tablet/ob_table_store_util.cpp
@@ -1072,7 +1072,10 @@ bool ObMemtableArray::exist_memtable_with_end_scn(const ObITable *table, const S
   // we need to make sure duplicate memtable was not added to tablet,
   // and ensure active memtable could be added to tablet
   bool is_exist = false;
-  if (table->get_end_scn() == end_scn && count_ >= 1) {
+  if (0 >= count_) {
+  } else if (table->get_end_scn() == end_scn || end_scn.is_max()) {
+    // Pay Attention!!!
+    // The end scn of memtable can only be max or a certain value.
     for (int64_t i = count_ - 1; i >= 0 ; --i) {
       const ObITable *memtable = memtable_array_[i];
       if (memtable == table) {
