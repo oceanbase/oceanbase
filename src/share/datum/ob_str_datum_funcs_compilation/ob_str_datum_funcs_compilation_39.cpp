7ccc9042c113f665b9cab5eaae2b4892f351aa02
Zach41 <zach_41@163.com>
2024-10-12 06:41:38 +0000

    Optimizer Compile

---
+++ src/share/datum/ob_str_datum_funcs_compilation/ob_str_datum_funcs_compilation_39.cpp
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2021 OceanBase
+ * OceanBase CE is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+
+#include "ob_str_datum_funcs_compilation.ipp"
+
+namespace oceanbase
+{
+namespace common
+{
+DEF_STR_FUNC_INIT(CS_TYPE_UTF8MB4_HU_0900_AS_CS, 117);
+DEF_STR_FUNC_INIT(CS_TYPE_UTF8MB4_HR_0900_AS_CS, 118);
+DEF_STR_FUNC_INIT(CS_TYPE_UTF8MB4_VI_0900_AS_CS, 119);
+} // end common
+} // end oceanbase
---
+++ src/sql/engine/expr/ob_expr_str_cmp_func_compile/ob_expr_str_cmp_func_part_26.cpp
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2021 OceanBase
+ * OceanBase CE is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+
+#include "ob_expr_str_cmp_func_common.ipp"
+
+namespace oceanbase
+{
+namespace sql
+{
+DEF_COMPILE_STR_FUNC_INIT(CS_TYPE_UTF8MB4_0900_AI_CI, 78);
+DEF_COMPILE_STR_FUNC_INIT(CS_TYPE_UTF8MB4_DE_PB_0900_AI_CI, 79);
+DEF_COMPILE_STR_FUNC_INIT(CS_TYPE_UTF8MB4_IS_0900_AI_CI, 80);
+} // end sql
+} // end oceanbase
