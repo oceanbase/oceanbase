fe6bffe4349d7c4b7d5347081484c93651495625
obdev <obdev@oceanbase.com>
2024-10-14 17:13:08 +0000

    [CP] [BUGFIX] Fix the issue where the shared property of the join filter is not set correctly.

--- src/sql/optimizer/ob_logical_operator.cpp
+++ src/sql/optimizer/ob_logical_operator.cpp
@@ -5022,12 +5022,13 @@ int ObLogicalOperator::allocate_normal_join_filter(const ObIArray<JoinFilterInfo
             join_filter_create->set_is_use_filter_shuffle(true);
             join_filter_use->set_is_use_filter_shuffle(true);
           }
-          if ((is_partition_wise_ || DistAlgo::DIST_PARTITION_NONE == join_dist_algo) && !right_has_exchange) {
-            join_filter_create->set_is_non_shared_join_filter();
-            join_filter_use->set_is_non_shared_join_filter();
-          } else {
+
+          if ((DistAlgo::DIST_BC2HOST_NONE == join_dist_algo) || right_has_exchange) {
             join_filter_create->set_is_shared_join_filter();
             join_filter_use->set_is_shared_join_filter();
+          } else {
+            join_filter_create->set_is_non_shared_join_filter();
+            join_filter_use->set_is_non_shared_join_filter();
           }
 
           if (OB_FAIL(ret)) {
