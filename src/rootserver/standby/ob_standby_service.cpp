fe0ba0499423d1b84a355238eaf7248aabdfff53
obdev <obdev@oceanbase.com>
2024-10-22 16:43:38 +0000

    drop the limit of new mview when set standby tenant restore until point

--- src/rootserver/standby/ob_standby_service.cpp
+++ src/rootserver/standby/ob_standby_service.cpp
@@ -449,7 +449,6 @@ int ObStandbyService::do_recover_tenant(
   ObTenantStatus tenant_status = TENANT_STATUS_MAX;
   ObLSRecoveryStatOperator ls_recovery_operator;
   ObLSRecoveryStat sys_ls_recovery;
-  bool is_contains_new_mv_tenant = false;
   if (OB_FAIL(check_inner_stat_())) {
     LOG_WARN("inner stat error", KR(ret), K_(inited));
   } else if (!obrpc::ObRecoverTenantArg::is_valid(recover_type, recovery_until_scn)
@@ -466,13 +465,6 @@ int ObStandbyService::do_recover_tenant(
     LOG_WARN("failed to get tenant status", KR(ret), K(tenant_id));
   } else if (OB_FAIL(GET_MIN_DATA_VERSION(tenant_id, tenant_version))) {
     LOG_WARN("failed to get tenant min version", KR(ret), K(tenant_id));
-  } else if (tenant_version >= DATA_VERSION_4_3_4_0
-              && OB_FAIL(ObMViewInfo::contains_major_refresh_mview(*sql_proxy_, tenant_id, is_contains_new_mv_tenant))) {
-    LOG_WARN("failed to contains_major_refresh_mview", KR(ret), K(tenant_id), K(tenant_version));
-  } else if (is_contains_new_mv_tenant) {
-    ret = OB_OP_NOT_ALLOW;
-    LOG_WARN("there is new mv in this tenant", K(tenant_id));
-    LOG_USER_ERROR(OB_OP_NOT_ALLOW, "there is new mv in this tenant, recover is");
   } else if (OB_FAIL(trans.start(sql_proxy_, exec_tenant_id))) {
     LOG_WARN("failed to start trans", KR(ret), K(exec_tenant_id), K(tenant_id));
   } else if (OB_FAIL(ObAllTenantInfoProxy::load_tenant_info(tenant_id, &trans, true, tenant_info))) {
