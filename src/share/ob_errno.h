6f6a1f5db8eb51cb552f6717328c308bf2ccbc70
doubleMocha <1085615789@qq.com>
2024-10-29 06:14:23 +0000

    add two error codes

--- src/share/ob_errno.h
+++ src/share/ob_errno.h
@@ -1938,6 +1938,8 @@ constexpr int OB_EXTERNAL_ODPS_COLUMN_TYPE_MISMATCH = -11051;
 constexpr int OB_EXTERNAL_ODPS_UNEXPECTED_ERROR = -11052;
 constexpr int OB_ERR_INCORRECT_STRING_VALUE_FOR_INET = -11062;
 constexpr int OB_EXTERNAL_TABLE_FORMAT_ERROR = -11063;
+constexpr int OB_EXTERNAL_ACCESS_PATH_ERROR = -11064;
+constexpr int OB_ORC_READ_ERROR = -11065;
 constexpr int OB_SP_RAISE_APPLICATION_ERROR = -20000;
 constexpr int OB_SP_RAISE_APPLICATION_ERROR_NUM = -21000;
 constexpr int OB_CLOB_ONLY_SUPPORT_WITH_MULTIBYTE_FUN = -22998;
@@ -4303,6 +4305,8 @@ constexpr int OB_ERR_INVALID_DATE_MSG_FMT_V2 = -4219;
 #define OB_JNI_OBJECT_NOT_FOUND_ERROR__USER_ERROR_MSG "object could not be found during jni operation"
 #define OB_ERR_INCORRECT_STRING_VALUE_FOR_INET__USER_ERROR_MSG "Incorrect string value for function %s"
 #define OB_EXTERNAL_TABLE_FORMAT_ERROR__USER_ERROR_MSG "missing format for external table"
+#define OB_EXTERNAL_ACCESS_PATH_ERROR__USER_ERROR_MSG "missing access path for external table"
+#define OB_ORC_READ_ERROR__USER_ERROR_MSG "fair to read orc file, message: %s"
 #define OB_SP_RAISE_APPLICATION_ERROR__USER_ERROR_MSG "%.*s"
 #define OB_SP_RAISE_APPLICATION_ERROR_NUM__USER_ERROR_MSG "error number argument to raise_application_error of '%d' is out of range"
 #define OB_CLOB_ONLY_SUPPORT_WITH_MULTIBYTE_FUN__USER_ERROR_MSG "CLOB or NCLOB in multibyte character set not supported"
@@ -9022,6 +9026,10 @@ constexpr int OB_ERR_INVALID_DATE_MSG_FMT_V2 = -4219;
 #define OB_ERR_INCORRECT_STRING_VALUE_FOR_INET__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -11062, Incorrect string value for function %s"
 #define OB_EXTERNAL_TABLE_FORMAT_ERROR__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -11063, missing format for external table"
 #define OB_EXTERNAL_TABLE_FORMAT_ERROR__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -11063, missing format for external table"
+#define OB_EXTERNAL_ACCESS_PATH_ERROR__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -11064, missing access path for external table"
+#define OB_EXTERNAL_ACCESS_PATH_ERROR__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -11064, missing access path for external table"
+#define OB_ORC_READ_ERROR__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -11065, fair to read orc file, message: %s"
+#define OB_ORC_READ_ERROR__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -11065, fair to read orc file, message: %s"
 #define OB_SP_RAISE_APPLICATION_ERROR__ORA_USER_ERROR_MSG "ORA%06ld: %.*s"
 #define OB_SP_RAISE_APPLICATION_ERROR__OBE_USER_ERROR_MSG "ORA%06ld: %.*s"
 #define OB_SP_RAISE_APPLICATION_ERROR_NUM__ORA_USER_ERROR_MSG "ORA-21000: error number argument to raise_application_error of '%d' is out of range"
@@ -9041,7 +9049,7 @@ constexpr int OB_ERR_INVALID_DATE_MSG_FMT_V2 = -4219;
 #define OB_ERR_INVALID_DATE_MSG_FMT_V2__ORA_USER_ERROR_MSG "ORA-01861: Incorrect datetime value for column '%.*s' at row %ld"
 #define OB_ERR_INVALID_DATE_MSG_FMT_V2__OBE_USER_ERROR_MSG "OBE-01861: Incorrect datetime value for column '%.*s' at row %ld"
 
-extern int g_all_ob_errnos[2361];
+extern int g_all_ob_errnos[2363];
 
   const char *ob_error_name(const int oberr);
   const char* ob_error_cause(const int oberr);
