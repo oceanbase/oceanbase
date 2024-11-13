71c844af61f80ab464781870a18a935e55a25038
ZenoWang <wzybuaasoft@163.com>
2024-11-13 06:14:29 +0000

    fix invalid static cast

--- src/storage/compaction/ob_partition_merge_policy.cpp
+++ src/storage/compaction/ob_partition_merge_policy.cpp
@@ -370,12 +370,12 @@ int ObPartitionMergePolicy::find_mini_merge_tables(
   // Freezing in the restart phase may not satisfy end >= last_max_sstable,
   // so the memtable cannot be filtered by scn
   // can only take out all frozen memtable
-  ObITabletMemtable *memtable = nullptr;
+  ObIMemtable *memtable = nullptr;
   const ObTabletID &tablet_id = tablet.get_tablet_meta().tablet_id_;
   bool has_release_memtable = false;
 
   for (int64_t i = 0; OB_SUCC(ret) && i < memtable_handles.count(); ++i) {
-    if (OB_ISNULL(memtable = static_cast<ObITabletMemtable *>(memtable_handles.at(i).get_table()))) {
+    if (OB_ISNULL(memtable = static_cast<ObIMemtable *>(memtable_handles.at(i).get_table()))) {
       ret = OB_ERR_SYS;
       LOG_ERROR("memtable must not null", K(ret), K(tablet));
     } else if (memtable->is_direct_load_memtable()) {
