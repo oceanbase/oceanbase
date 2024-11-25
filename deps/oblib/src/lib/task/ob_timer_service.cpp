6510ad501f8cd749a7ae0ae4243ba4568729773c
obdev <obdev@oceanbase.com>
2024-11-25 05:15:14 +0000

    [bugfix] reset timeout after run timer task

--- deps/oblib/src/lib/task/ob_timer_service.cpp
+++ deps/oblib/src/lib/task/ob_timer_service.cpp
@@ -85,6 +85,7 @@ void ObTimerTaskThreadPool::handle(void *task_token)
       ObTimerMonitor::get_instance().start_task(thread_id, start_time, token->delay_, token->task_);
     }
     token->task_->runTimerTask();
+    THIS_WORKER.set_timeout_ts(INT64_MAX); // reset timeout to INT64_MAX
     const int64_t end_time = ::oceanbase::common::ObTimeUtility::current_time();
     const int64_t elapsed_time = end_time - start_time;
     if (do_timeout_check) {
