3a8d959bde9a979c4fed34bc9ef06efd38f8f084
bit-dance <2634358021@qq.com>
2024-12-09 07:48:23 +0000

    [#2024120800106317937]Add place holder for pl_cg_mem_hold.

--- src/observer/virtual_table/ob_gv_sql.cpp
+++ src/observer/virtual_table/ob_gv_sql.cpp
@@ -596,6 +596,10 @@ int ObGVSql::fill_cells(const ObILibCacheObject *cache_obj, const ObPlanCache &p
       cells[i].set_int(mem_used);
       break;
     }
+    case share::ALL_VIRTUAL_PLAN_STAT_CDE::PL_CG_MEM_HOLD: {
+      cells[i].set_int(0);
+      break;
+    }
     case share::ALL_VIRTUAL_PLAN_STAT_CDE::EXECUTIONS: {
       if (!cache_stat_updated) {
         cells[i].set_null();
