07cb96d2b998b40d71bea7456b24ba702b0304e4
bit-dance <2634358021@qq.com>
2024-12-09 07:48:23 +0000

    [#2024120800106317937]Add place holder for pl_cg_mem_hold.

--- src/share/inner_table/ob_inner_table_schema_def.py
+++ src/share/inner_table/ob_inner_table_schema_def.py
@@ -8498,6 +8498,7 @@ def_table_schema(
       ('is_in_pc', 'bool'),
       ('erase_time', 'timestamp'),
       ('compile_time', 'uint'),
+      ('pl_cg_mem_hold', 'int'),
   ],
   vtable_route_policy = 'distributed',
   partition_columns = ['svr_ip', 'svr_port'],
@@ -34279,7 +34280,8 @@ def_table_schema(
            COMPILE_TIME,
            SCHEMA_VERSION,
            PS_STMT_ID,
-           DB_ID
+           DB_ID,
+           PL_CG_MEM_HOLD
     FROM oceanbase.__all_virtual_plan_stat WHERE OBJECT_STATUS = 0 AND TYPE > 5 AND TYPE < 11 AND is_in_pc=true
 """.replace("\n", " "),
     normal_columns = [
@@ -34315,7 +34317,8 @@ def_table_schema(
            COMPILE_TIME,
            SCHEMA_VERSION,
            PS_STMT_ID,
-           DB_ID
+           DB_ID,
+           PL_CG_MEM_HOLD
     FROM oceanbase.GV$OB_PL_CACHE_OBJECT WHERE SVR_IP =HOST_IP() AND SVR_PORT = RPC_PORT()
 """.replace("\n", " "),
 
@@ -67947,7 +67950,8 @@ def_table_schema(
            COMPILE_TIME AS COMPILE_TIME,
            SCHEMA_VERSION AS SCHEMA_VERSION,
            PS_STMT_ID AS PS_STMT_ID,
-           DB_ID AS DB_ID
+           DB_ID AS DB_ID,
+           PL_CG_MEM_HOLD AS PL_CG_MEM_HOLD
     FROM SYS.ALL_VIRTUAL_PLAN_STAT WHERE OBJECT_STATUS = 0 AND TYPE > 5 AND TYPE < 11 AND is_in_pc='1'
 """.replace("\n", " "),
     normal_columns = [
@@ -67984,7 +67988,8 @@ def_table_schema(
            COMPILE_TIME AS COMPILE_TIME,
            SCHEMA_VERSION AS SCHEMA_VERSION,
            PS_STMT_ID AS PS_STMT_ID,
-           DB_ID AS DB_ID
+           DB_ID AS DB_ID,
+           PL_CG_MEM_HOLD AS PL_CG_MEM_HOLD
     FROM SYS.GV$OB_PL_CACHE_OBJECT WHERE SVR_IP =HOST_IP() AND SVR_PORT = RPC_PORT()
 """.replace("\n", " "),
     normal_columns = [
