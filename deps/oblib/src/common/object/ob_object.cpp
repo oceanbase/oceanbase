4c6b7cd393e8a512417168bf1ad75a1c6aacfbbd
hanr881 <1741282579@qq.com>
2024-12-10 11:13:32 +0000

    [CP] to issue<2024112900105382231>:fix core when free rawtype obobj memory

--- deps/oblib/src/common/object/ob_object.cpp
+++ deps/oblib/src/common/object/ob_object.cpp
@@ -1446,13 +1446,17 @@ void* ObObj::get_deep_copy_obj_ptr()
       ptr = (void *)v_.string_;
     }
   } else if (ob_is_raw(this->get_type())) {
-    ptr = (void *)v_.string_;
+    if (val_len_ != 0) {
+      ptr = (void *)v_.string_;
+    }
   } else if (ob_is_number_tc(this->get_type())
             && 0 != nmb_desc_.len_
             && NULL != v_.nmb_digits_) {
     ptr = (void *)v_.nmb_digits_;
   } else if (ob_is_rowid_tc(this->get_type())) {
-    ptr = (void *)v_.string_;
+    if (val_len_ != 0) {
+      ptr = (void *)v_.string_;
+    }
   } else if (ob_is_lob_locator(this->get_type())) {
     ptr = (void *)v_.lob_locator_;
   } else {
