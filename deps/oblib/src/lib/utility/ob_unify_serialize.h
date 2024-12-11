c7cc62cf6e02bbf3c62cb12f0805ccc6b92a7307
zhjc1124 <zhjc1124@gmail.com>
2024-12-11 12:14:54 +0000

    fix CHECK_SERIALIZE_CHECK fail core

--- deps/oblib/src/lib/utility/ob_unify_serialize.h
+++ deps/oblib/src/lib/utility/ob_unify_serialize.h
@@ -229,8 +229,8 @@ struct EmptyUnisStruct
       }                                                                 \
       int64_t serial_size = pos - pos_bak;                               \
       int64_t tmp_pos = 0;                                               \
-      CHECK_SERIALIZE_SIZE(CLS, serial_size);                            \
       if (OB_SUCC(ret)) {                                                \
+        CHECK_SERIALIZE_SIZE(CLS, serial_size);                          \
         ret = NS_::encode_fixed_bytes_i64(buf + pos_bak - size_nbytes,   \
           size_nbytes, tmp_pos, serial_size);                            \
       }                                                                  \
