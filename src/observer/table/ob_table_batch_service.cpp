1fcf2c2aae2017edb8bd517a3e574d6cd0ddd8b7
shenyunlong.syl <ylshen0919@gmail.com>
2024-12-17 11:18:34 +0000

    [OBKV] fix observer coredump or report -4016 when execute multi-cf hbase put with lob column

--- src/observer/table/ob_table_batch_service.cpp
+++ src/observer/table/ob_table_batch_service.cpp
@@ -191,11 +191,17 @@ int ObTableBatchService::adjust_entities(ObTableBatchCtx &ctx)
   ObTableCtx &tb_ctx = ctx.tb_ctx_;
 
   // first entity has beed adjusted when create tb_ctx
-  for (int64_t i = 1; OB_SUCC(ret) && i < ctx.ops_->count(); ++i) {
-    const ObTableOperation &op = ctx.ops_->at(i);
-    tb_ctx.set_entity(&op.entity());
-    if (OB_FAIL(tb_ctx.adjust_entity())) {
-      LOG_WARN("fail to adjust entity", K(ret));
+  // but in hbase, the tb_ctx maybe reused and first entity
+  // also need to be adjusted
+  for (int64_t i = 0; OB_SUCC(ret) && i < ctx.ops_->count(); ++i) {
+    if (i == 0 && ctx.entity_type_ != ObTableEntityType::ET_HKV) {
+      // do noting
+    } else {
+      const ObTableOperation &op = ctx.ops_->at(i);
+      tb_ctx.set_entity(&op.entity());
+      if (OB_FAIL(tb_ctx.adjust_entity())) {
+        LOG_WARN("fail to adjust entity", K(ret));
+      }
     }
   }
 
@@ -773,7 +779,7 @@ int ObTableBatchService::htable_put(ObTableBatchCtx &ctx)
         const ObTableOperation &op = ctx.ops_->at(i);
         ObTableOperationResult single_op_result;
         tb_ctx.set_entity(&op.entity());
-        if (i > 0 && OB_FAIL(tb_ctx.adjust_entity())) { // first entity adjust in init_single_op_tb_ctx
+        if (OB_FAIL(tb_ctx.adjust_entity())) { // first entity adjust in init_single_op_tb_ctx
           LOG_WARN("fail to adjust entity", K(ret));
         } else if (OB_FAIL(ObTableOpWrapper::process_op_with_spec(tb_ctx, spec, single_op_result))) {
           LOG_WARN("fail to process op with spec", K(ret));
