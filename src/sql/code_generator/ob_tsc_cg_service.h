945a75580bdf4d078214436a715a1be76ca76875
saltonz <saltonzh@gmail.com>
2025-01-08 10:14:55 +0000

    [bugfix] fix complex filter with match against in remote execution

--- src/sql/code_generator/ob_tsc_cg_service.h
+++ src/sql/code_generator/ob_tsc_cg_service.h
@@ -182,6 +182,7 @@ private:
   int extract_doc_id_index_back_output_column_ids(const ObLogTableScan &op,
                                                   ObIArray<uint64_t> &output_cids);
   int filter_out_match_exprs(ObIArray<ObRawExpr*> &exprs);
+  int flatten_and_filter_match_exprs(ObRawExpr *expr, ObIArray<ObRawExpr *> &res_exprs);
   int append_fts_relavence_project_col(
       ObDASIRAuxLookupCtDef *aux_lookup_ctdef,
       ObDASIRScanCtDef *ir_scan_ctdef);
