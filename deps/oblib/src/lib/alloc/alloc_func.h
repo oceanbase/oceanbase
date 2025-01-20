34213ca16286d6e4b4c64bfd15e99a5cee994f57
tushicheng <18829573815@163.com>
2025-01-20 13:45:31 +0000

    set nway of ObTenantCtxAllocator::using_list

--- deps/oblib/src/lib/alloc/alloc_func.h
+++ deps/oblib/src/lib/alloc/alloc_func.h
@@ -62,6 +62,8 @@ int set_meta_obj_limit(uint64_t tenant_id, int64_t meta_obj_pct_lmt);
 int set_rpc_limit(uint64_t tenant_id, int64_t rpc_pct_lmt);
 
 bool errsim_alloc(const ObMemAttr &attr);
+
+int set_req_chunkmgr_parallel(uint64_t tenant_id, uint64_t ctx_id, int32_t parallel);
 } // end of namespace lib
 } // end of namespace oceanbase
 
