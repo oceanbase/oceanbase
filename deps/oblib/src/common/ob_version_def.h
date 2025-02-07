3dd3faecc9da8bd51d0fbf553fbf6dadee078ae6
lucky-sinx <2549261744@qq.com>
2025-02-07 11:18:39 +0000

    [cp]fix for comprocess info/kill hung bug

--- deps/oblib/src/common/ob_version_def.h
+++ deps/oblib/src/common/ob_version_def.h
@@ -191,6 +191,7 @@ cal_version(const uint64_t major, const uint64_t minor, const uint64_t major_pat
 #define LAST_BARRIER_DATA_VERSION DATA_VERSION_4_2_1_0
 
 #define PROXY_VERSION_4_2_3_0 (oceanbase::common::cal_version(4, 2, 3, 0))
+#define PROXY_VERSION_4_3_0_0 (oceanbase::common::cal_version(4, 3, 0, 0))
 #define PROXY_VERSION_4_3_3_0 (oceanbase::common::cal_version(4, 3, 3, 0))
 
 class VersionUtil
