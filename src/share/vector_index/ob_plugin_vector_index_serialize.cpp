7d908048409ed18934757d2d1ca895094a30c392
Carrot-77 <1012982871@qq.com>
2025-02-12 11:45:49 +0000

    [vector index] fix serialize and deserialize null data

--- src/share/vector_index/ob_plugin_vector_index_serialize.cpp
+++ src/share/vector_index/ob_plugin_vector_index_serialize.cpp
@@ -27,7 +27,9 @@ std::streamsize ObOStreamBuf::xsputn(const char* s, std::streamsize count)
 {
   std::streamsize written_size = 0;
   std::streamsize left_size = 0;
-  if (OB_ISNULL(s)) {
+  if (count == 0) {
+    // do nothing
+  } else if (OB_ISNULL(s)) {
     last_error_code_ = OB_INVALID_ARGUMENT;
   }
   while (is_valid() && is_success() && written_size < count) {
@@ -123,7 +125,9 @@ std::streamsize ObIStreamBuf::xsgetn(char* s, std::streamsize n)
 {
   std::streamsize get_size = 0;
   std::streamsize data_size = 0;
-  if (OB_ISNULL(s)) {
+  if (n == 0) {
+    // do nothing
+  } else if (OB_ISNULL(s)) {
     last_error_code_ = OB_INVALID_ARGUMENT;
   } else if (is_success() && !is_valid()) {
     last_error_code_ = do_callback();
