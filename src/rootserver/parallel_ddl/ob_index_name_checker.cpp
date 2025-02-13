30cb66e99c563de723eeb640833ea63af72a019a
fanfangzhou <937806648@qq.com>
2025-02-13 13:13:10 +0000

    fix unstable mysql test

--- src/rootserver/parallel_ddl/ob_index_name_checker.cpp
+++ src/rootserver/parallel_ddl/ob_index_name_checker.cpp
@@ -233,12 +233,11 @@ int ObIndexNameCache::try_load_cache_()
     } else if (!ObSchemaService::is_formal_version(schema_version)) {
       ret = OB_EAGAIN;
       LOG_WARN("schema version is informal, need retry", KR(ret), K(schema_status), K(schema_version));
-    } else if (OB_FAIL(ObShareUtil::get_ctx_timeout(GCONF.internal_sql_execute_timeout, timeout_ts))) {
+    } else if (OB_FAIL(ObShareUtil::get_abs_timeout(GCONF.internal_sql_execute_timeout, timeout_ts))) {
       LOG_WARN("fail to get timeout", KR(ret));
     } else {
       int64_t original_timeout_ts = THIS_WORKER.get_timeout_ts();
       THIS_WORKER.set_timeout_ts(timeout_ts);
-
       ObSchemaGetterGuard guard;
       int64_t start_time = ObTimeUtility::current_time();
       if (OB_FAIL(GSCHEMASERVICE.async_refresh_schema(tenant_id_, schema_version))) {
