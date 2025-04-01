88e4df7bd7cc76a042b7b06448bdc83c8b3a108d
hwx65 <1780011298@qq.com>
2025-04-01 08:14:14 +0000

    [CP] Fixed result of cast overlong string to number is incompatible with MySQL

--- deps/oblib/src/lib/number/ob_number_v2.cpp
+++ deps/oblib/src/lib/number/ob_number_v2.cpp
@@ -805,6 +805,12 @@ int ObNumber::from_v3_(const char *str, const int64_t length, IAllocator &alloca
         if (OB_DECIMAL_PRECISION_OVERFLOW == ret) {
           set_zero();
           ret = OB_SUCCESS;
+        } else if (OB_INTEGER_PRECISION_OVERFLOW == ret && is_mysql_mode()) {
+          ret = OB_SUCCESS;
+          d_.exp_ = ((d_.sign_ == ObNumber::POSITIVE) ? MAX_INTEGER_EXP : -MAX_INTEGER_EXP) + EXP_ZERO;
+          for (int i = 0; i < d.len_; i++) {
+            digits_[i] = BASE - 1;
+          }
         }
       }
     }
