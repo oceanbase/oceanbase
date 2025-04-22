aa56cd7a785f2caa3397a934a87005d3c3447acd
dontknow9179 <545187809@qq.com>
2025-04-22 05:46:54 +0000

    fix mysqltest: external table read enum and select into s3

--- src/sql/resolver/ob_resolver_utils.cpp
+++ src/sql/resolver/ob_resolver_utils.cpp
@@ -5246,6 +5246,7 @@ int ObResolverUtils::build_file_column_expr_for_parquet(
               file_column_expr->set_data_type(ObRawType);
             } else if (is_mysql_mode() && ob_is_enum_or_set_type(column_expr->get_data_type())) {
               file_column_expr->set_data_type(ObCharType);
+              file_column_expr->set_length(OB_MAX_MYSQL_VARCHAR_LENGTH);
             } else {
               file_column_expr->set_data_type(ObVarcharType);
             }
