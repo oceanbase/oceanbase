e865b261fba7448ad5402147152db9887dfb83f2
joseph12138 <17862707376@163.com>
2025-05-13 07:21:19 +0000

    add new thread type on master

--- src/share/ob_rpc_struct.h
+++ src/share/ob_rpc_struct.h
@@ -4409,6 +4409,7 @@ public:
     RECOVERY_LS_SERVICE,
     BALANCE_TASK_EXECUTE,
     DISASTER_RECOVERY_SERVICE,
+    ARBITRATION_SERVICE,
   };
   ObNotifyTenantThreadArg() : tenant_id_(OB_INVALID_TENANT_ID), thread_type_(INVALID_TYPE) {}
   ~ObNotifyTenantThreadArg() {}
