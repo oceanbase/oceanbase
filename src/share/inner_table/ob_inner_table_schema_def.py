f2c058d13d9522f4e1530ce5e309de513ed3f714
godyangfight <godyangfight@gmail.com>
2025-05-14 12:51:25 +0000

    Rename inner table name

--- src/share/inner_table/ob_inner_table_schema_def.py
+++ src/share/inner_table/ob_inner_table_schema_def.py
@@ -16835,7 +16835,7 @@ def_table_schema(**gen_oracle_mapping_virtual_table_def('15509', all_def_keyword
 # 15517: __all_virtual_ss_tablet_meta
 # 15518: __all_virtual_ss_ls_meta
 # 15519: __all_virtual_ss_sstable_mgr
-# 15520: __all_virtual_ss_ls_tablet_reorganization_table
+# 15520: __all_virtual_ss_ls_tablet_reorganization_info
 
 # 余留位置（此行之前占位）
 # 本区域定义的Oracle表名比较复杂，一般都采用gen_xxx_table_def()方式定义，占位建议采用基表表名占位
