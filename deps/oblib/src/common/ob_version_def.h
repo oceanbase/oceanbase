450d5626d78a1cb41fa3275ceb6878f0726afd75
obdev <obdev@oceanbase.com>
2025-05-15 09:48:11 +0000

    Upgrade 4_3_5_release version to 4.3.5 and remove some version-related case contents

--- deps/oblib/src/common/ob_version_def.h
+++ deps/oblib/src/common/ob_version_def.h
@@ -144,9 +144,10 @@ cal_version(const uint64_t major, const uint64_t minor, const uint64_t major_pat
 #define CLUSTER_VERSION_4_3_5_0 (oceanbase::common::cal_version(4, 3, 5, 0))
 #define CLUSTER_VERSION_4_3_5_1 (oceanbase::common::cal_version(4, 3, 5, 1))
 #define CLUSTER_VERSION_4_3_5_2 (oceanbase::common::cal_version(4, 3, 5, 2))
+#define CLUSTER_VERSION_4_3_5_3 (oceanbase::common::cal_version(4, 3, 5, 3))
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //TODO: If you update the above version, please update CLUSTER_CURRENT_VERSION.
-#define CLUSTER_CURRENT_VERSION CLUSTER_VERSION_4_3_5_2
+#define CLUSTER_CURRENT_VERSION CLUSTER_VERSION_4_3_5_3
 
 // ATTENSION !!!!!!!!!!!!!!!!!!!!!!!!!!!
 // 1. After 4.0, each cluster_version is corresponed to a data version.
@@ -189,7 +190,8 @@ cal_version(const uint64_t major, const uint64_t minor, const uint64_t major_pat
 #define DATA_VERSION_4_3_5_0 (oceanbase::common::cal_version(4, 3, 5, 0))
 #define DATA_VERSION_4_3_5_1 (oceanbase::common::cal_version(4, 3, 5, 1))
 #define DATA_VERSION_4_3_5_2 (oceanbase::common::cal_version(4, 3, 5, 2))
-#define DATA_CURRENT_VERSION DATA_VERSION_4_3_5_2
+#define DATA_VERSION_4_3_5_3 (oceanbase::common::cal_version(4, 3, 5, 3))
+#define DATA_CURRENT_VERSION DATA_VERSION_4_3_5_3
 // ATTENSION !!!!!!!!!!!!!!!!!!!!!!!!!!!
 // LAST_BARRIER_DATA_VERSION should be the latest barrier data version before DATA_CURRENT_VERSION
 #define LAST_BARRIER_DATA_VERSION DATA_VERSION_4_2_1_0
--- rpm/oceanbase.spec
+++ rpm/oceanbase.spec
@@ -1,4 +1,4 @@
 Name: %NAME
-Version:4.3.5.2
+Version:4.3.5.3
 Release: %RELEASE
 BuildRequires: binutils = 2.30
