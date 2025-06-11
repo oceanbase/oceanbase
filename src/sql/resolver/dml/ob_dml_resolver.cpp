f28b9f15e1755dcd0e67e23db18fac464de25b65
haohao022 <haohaohm@outlook.com>
2025-06-11 19:20:06 +0000

    [to #2025060500109376298] fix: add table info for ORA_ROWSCN when splicing sql

--- src/sql/resolver/dml/ob_dml_resolver.cpp
+++ src/sql/resolver/dml/ob_dml_resolver.cpp
@@ -14496,6 +14496,7 @@ int ObDMLResolver::resolve_ora_rowscn_pseudo_column(
     pseudo_column_expr->set_data_type(ObIntType);
     pseudo_column_expr->set_accuracy(ObAccuracy::MAX_ACCURACY[ObIntType]);
     pseudo_column_expr->set_table_id(table_item->table_id_);
+    pseudo_column_expr->set_table_name(table_item->get_object_name());
     real_ref_expr = pseudo_column_expr;
     OZ(pseudo_column_expr->add_relation_id(get_stmt()->get_table_bit_index(table_item->table_id_)));
     OZ(get_stmt()->get_pseudo_column_like_exprs().push_back(pseudo_column_expr));
