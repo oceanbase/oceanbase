642c59fea7de83426fdd7b17a54fe05b796be97e
obdev <obdev@oceanbase.com>
2025-06-12 12:36:01 +0000

    [CP] fix px dtl message memory leak problem

--- src/sql/engine/basic/ob_temp_table_insert_op.cpp
+++ src/sql/engine/basic/ob_temp_table_insert_op.cpp
@@ -113,6 +113,7 @@ int ObTempTableInsertOp::inner_close()
 
 void ObTempTableInsertOp::destroy()
 {
+  interm_result_ids_.reset();
   sql_mem_processor_.unregister_profile_if_necessary();
   destroy_mem_context();
   ObOperator::destroy();
