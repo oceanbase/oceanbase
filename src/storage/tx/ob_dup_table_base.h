7558062aeda6ff81f6a85cdb208ed003120423f8
KyrielightWei <weixx1203@outlook.com>
2025-06-16 06:47:20 +0000

    [CP] Consider the readable set when calculating the rec SCN of the dup table

--- src/storage/tx/ob_dup_table_base.h
+++ src/storage/tx/ob_dup_table_base.h
@@ -866,7 +866,7 @@ public:
     OB_UNIS_VERSION(1);
   };
 
-  TO_STRING_KV(K(dup_ls_meta_), K(lease_log_rec_scn_), K(start_replay_scn_),
+  TO_STRING_KV(K(dup_ls_meta_), K(lease_log_rec_scn_), K(tablet_log_rec_scn_), K(start_replay_scn_),
                K(readable_ckpt_base_scn_is_accurate_));
 
 public:
@@ -895,6 +895,7 @@ public:
   {
     dup_ls_meta_.reset();
     lease_log_rec_scn_.reset();
+    tablet_log_rec_scn_.reset();
     start_replay_scn_.reset();
     readable_ckpt_base_scn_is_accurate_ = true;
   }
@@ -903,6 +904,7 @@ private:
   SpinRWLock ckpt_rw_lock_;
   ObLSDupTableMeta dup_ls_meta_;
   share::SCN lease_log_rec_scn_;
+  share::SCN tablet_log_rec_scn_;
   share::SCN start_replay_scn_;
   // ckpt_base_scn means, when replay dup log with base_scn,
   // this node cotain all readable set the same with leader.
