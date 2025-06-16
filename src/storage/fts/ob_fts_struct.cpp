892c6d25fa256bb6b79fe677f108c89a138d83bb
SuperYoko <lipeng.yk@gmail.com>
2025-06-16 09:17:19 +0000

    [CP] 使用Datum & DatumFuncs来进行string对比,避免与memtable不同,产生duplicated row

---
+++ src/storage/fts/ob_fts_struct.cpp
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2025 OceanBase
+ * OceanBase is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+
+#include "storage/fts/ob_fts_struct.h"
+
+#include "lib/charset/ob_charset.h"
+#include "share/datum/ob_datum_funcs.h"
+#include "storage/ob_storage_util.h"
+
+namespace oceanbase
+{
+namespace storage
+{
+
+int ObFTWord::hash(uint64_t &hash_val) const
+{
+  int ret = OB_SUCCESS;
+  ObObjMeta meta;
+  sql::ObExprBasicFuncs *funcs = ObDatumFuncs::get_basic_func(meta.get_type(), meta.get_collation_type());
+  if (OB_ISNULL(funcs)) {
+    ret = OB_ERR_UNEXPECTED;
+  } else if (funcs->default_hash_ == nullptr) {
+    ret = OB_ERR_UNEXPECTED;
+  } else {
+    ret = funcs->default_hash_(word_, 0, hash_val);
+  }
+  return ret;
+}
+bool ObFTWord::operator==(const ObFTWord &other) const
+{
+  bool is_equal = false;
+  int ret = OB_SUCCESS;
+  int cmp_ret = 0;
+  ObDatumCmpFuncType func = get_datum_cmp_func(meta_, other.meta_);
+  if (func == nullptr) {
+    ob_abort();
+  } else if (OB_FAIL(func(word_, other.word_, cmp_ret))) {
+    ob_abort();
+  } else {
+    is_equal = (cmp_ret == 0);
+  }
+  return is_equal;
+}
+} // namespace storage
+} // namespace oceanbase
