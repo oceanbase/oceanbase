35d6a6623f49a1017722811a6302281bc0ceb8d5
chimyue <chimyue@gmail.com>
2025-06-16 14:18:20 +0000

    [CP] fix candi fd check exprs unique bug

--- src/sql/rewrite/ob_transform_utils.cpp
+++ src/sql/rewrite/ob_transform_utils.cpp
@@ -4195,13 +4195,18 @@ int ObTransformUtils::check_exprs_unique_on_table_items(const ObDMLStmt *stmt,
     check_helper.session_info_ = session_info;
     UniqueCheckInfo res_info;
     ObRelIds all_tables;
+    ObSEArray<ObRawExpr*, 1> dummy_conds;
     if (OB_FAIL(compute_tables_property(stmt, check_helper, table_items, conditions, res_info))) {
       LOG_WARN("failed to compute tables property", K(ret));
     } else if (OB_FAIL(get_rel_ids_from_tables(stmt, table_items, all_tables))) {
       LOG_WARN("failed to add members", K(ret));
-    } else if (!is_strict && OB_FAIL(append(res_info.fd_sets_, res_info.candi_fd_sets_))) {
-      // is strict, use fd_item_set & candi_fd_set check unique
-      LOG_WARN("failed to append fd item sets", K(ret));
+    } else if (!is_strict && OB_FAIL(append(res_info.not_null_, exprs))) {
+      LOG_WARN("failed to append", K(ret));
+    } else if (!is_strict && OB_FAIL(ObOptimizerUtil::enhance_fd_item_set(dummy_conds,
+                                                                          res_info.candi_fd_sets_,
+                                                                          res_info.fd_sets_,
+                                                                          res_info.not_null_))) {
+      LOG_WARN("failed to enhance fd item set", K(ret));
     } else if (OB_FAIL(ObOptimizerUtil::is_exprs_unique(exprs, all_tables, res_info.fd_sets_,
                                                         res_info.equal_sets_,
                                                         res_info.const_exprs_,
@@ -4313,13 +4318,18 @@ int ObTransformUtils::check_stmt_unique(const ObSelectStmt *stmt,
     check_helper.session_info_ = session_info;
     UniqueCheckInfo res_info;
     ObRelIds all_tables;
+    ObSEArray<ObRawExpr*, 1> dummy_conds;
     if (OB_FAIL(compute_stmt_property(stmt, check_helper, res_info, extra_flags))) {
       LOG_WARN("failed to compute stmt property", K(ret));
     } else if (OB_FAIL(stmt->get_from_tables(all_tables))) {
       LOG_WARN("failed to get from tables", K(ret));
-    } else if (!is_strict && OB_FAIL(append(res_info.fd_sets_, res_info.candi_fd_sets_))) {
-      // is strict, use fd_item_set & candi_fd_set check unique
-      LOG_WARN("failed to append fd item sets", K(ret));
+    } else if (!is_strict && OB_FAIL(append(res_info.not_null_, exprs))) {
+      LOG_WARN("failed to append", K(ret));
+    } else if (!is_strict && OB_FAIL(ObOptimizerUtil::enhance_fd_item_set(dummy_conds,
+                                                                          res_info.candi_fd_sets_,
+                                                                          res_info.fd_sets_,
+                                                                          res_info.not_null_))) {
+      LOG_WARN("failed to enhance fd item set", K(ret));
     } else if (OB_FAIL(ObOptimizerUtil::is_exprs_unique(exprs, all_tables, res_info.fd_sets_,
                                                         res_info.equal_sets_,
                                                         res_info.const_exprs_, is_unique))) {
