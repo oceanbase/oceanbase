698e997fd4443fffebd455ca2b6f86f074128c13
obdev <obdev@oceanbase.com>
2025-06-25 04:48:04 +0000

    Memory Leak on Failure

--- src/observer/net/ob_shared_storage_net_throt_service.cpp
+++ src/observer/net/ob_shared_storage_net_throt_service.cpp
@@ -106,9 +106,13 @@ int ObSharedStorageNetThrotManager::register_endpoint(const ObSSNTEndpointArg &e
         } else if (OB_ISNULL(value)) {
           ret = OB_ALLOCATE_MEMORY_FAILED;
           LOG_WARN("fail to new value", K(ret));
-        } else if (OB_SUCCESS != quota_plan_map->set_refactored(key, value, /*flag*/ 1)) {
+        } else if (OB_FAIL(quota_plan_map->set_refactored(key, value, /*flag*/ 1))) {
           LOG_WARN("fail to set quota_plan_map", K(ret));
         } else if (OB_FAIL(bucket_throt_map_.set_refactored(storage_key, quota_plan_map, /*flag*/ 1))) {
+          int tmp_ret = OB_SUCCESS;
+          if (OB_TMP_FAIL(quota_plan_map->erase_refactored(key))) {
+            LOG_WARN("erase key fail", K(storage_key));
+          }
           LOG_WARN("fail to set bucket_throt_map", K(ret));
         }
         if (ret != OB_SUCCESS) {
@@ -140,7 +144,7 @@ int ObSharedStorageNetThrotManager::clear_expired_infos()
     obrpc::ObStorageKeyLimitMap::iterator it = storage_key_limit_map_.begin();
     for (; it != storage_key_limit_map_.end(); ++it) {
       if (it->second.expire_time_ < current_time) {
-        delete_storage_keys.push_back(it->first);
+        (void)delete_storage_keys.push_back(it->first);
       }
     }
     for (int i = 0; i < delete_storage_keys.count(); ++i) {
@@ -179,7 +183,7 @@ int ObSharedStorageNetThrotManager::clear_expired_infos()
     for (; it != endpoint_infos_map_.end(); ++it) {
       ObEndpointInfos &endpoint_info = it->second;
       if (endpoint_info.expire_time_ < current_time) {
-        delete_addrs.push_back(it->first);
+        (void)delete_addrs.push_back(it->first);
       }
     }
     for (int i = 0; i < delete_addrs.count(); ++i) {
@@ -195,9 +199,9 @@ int ObSharedStorageNetThrotManager::clear_expired_infos()
         // badcase
         ret = OB_INVALID_CONFIG;
         LOG_WARN("value is nullptr", K(ret));
-        delete_storage_keys.push_back(iter->first);
+        (void)delete_storage_keys.push_back(iter->first);
       } else if (quota_plan_map->size() == 0) {
-        delete_storage_keys.push_back(iter->first);
+        (void)delete_storage_keys.push_back(iter->first);
       } else {
         ObQuotaPlanMap::iterator quota_it = quota_plan_map->begin();
         common::ObSEArray<ObSSNTKey, 10> delete_SSNT_keys;
@@ -205,10 +209,10 @@ int ObSharedStorageNetThrotManager::clear_expired_infos()
           ObSSNTKey &key = quota_it->first;
           ObSSNTValue *value = quota_it->second;
           if (OB_ISNULL(value)) {
-            delete_SSNT_keys.push_back(key);
+            (void)delete_SSNT_keys.push_back(key);
           } else if (value->expire_time_ < current_time) {
             ob_delete(value);
-            delete_SSNT_keys.push_back(key);
+            (void)delete_SSNT_keys.push_back(key);
           }
         }
         for (int i = 0; i < delete_SSNT_keys.count(); ++i) {
@@ -218,7 +222,7 @@ int ObSharedStorageNetThrotManager::clear_expired_infos()
         }
       }
       if (OB_NOT_NULL(quota_plan_map) && quota_plan_map->size() == 0) {
-        delete_storage_keys.push_back(iter->first);
+        (void)delete_storage_keys.push_back(iter->first);
       }
     }
     if (OB_FAIL(clear_storage_key(delete_storage_keys))) {
@@ -899,7 +903,7 @@ int ObSSNTAllocService::get_follower_register_arg(obrpc::ObSSNTEndpointArg &arg)
   for (common::hash::ObHashSet<ObTrafficControl::ObStorageKey>::iterator it = storage_set.begin();
        it != storage_set.end();
        ++it) {
-    arg.storage_keys_.push_back(it->first);
+    (void)arg.storage_keys_.push_back(it->first);
   }
   return ret;
 }
