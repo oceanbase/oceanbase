a14bf090c1dd759d3df60af067d9d9268965e0ee
JinmaoLi <ljm.csmaster@gmail.com>
2025-06-27 11:18:19 +0000

    fix privilege escalation of PL/SQL in SYS tenant

--- src/sql/ob_sql.cpp
+++ src/sql/ob_sql.cpp
@@ -1791,9 +1791,7 @@ int ObSql::handle_pl_execute(const ObString &sql,
     context.is_dynamic_sql_ = is_dynamic_sql;
     context.is_prepare_protocol_ = is_prepare_protocol;
     context.spm_ctx_.bl_key_.db_id_ = session.get_database_id();
-    context.disable_privilege_check_ = OB_SYS_TENANT_ID == session.get_priv_tenant_id()
-                                          ? PRIV_CHECK_FLAG_DISABLE
-                                          : PRIV_CHECK_FLAG_IN_PL;
+    context.disable_privilege_check_ = PRIV_CHECK_FLAG_IN_PL;
     pctx = ectx.get_physical_plan_ctx();
     int64_t local_tenant_schema_version = -1;
     int64_t local_sys_schema_version = -1;
