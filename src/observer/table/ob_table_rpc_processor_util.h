834c43206df5f3cbf26f982aa04d48a63d6a8a94
WeiXinChan <chenwx6728@163.com>
2025-07-01 04:12:05 +0000

    [Bugfix] fix statis bug

--- src/observer/table/ob_table_rpc_processor_util.h
+++ src/observer/table/ob_table_rpc_processor_util.h
@@ -108,218 +108,278 @@ public:
     switch (process_type) {
       // table single mutate
       case ObTableProccessType::TABLE_API_SINGLE_INSERT:
-        EVENT_INC(TABLEAPI_INSERT_COUNT);
-        EVENT_ADD(TABLEAPI_INSERT_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_INSERT_COUNT);
+          EVENT_ADD(TABLEAPI_INSERT_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_insert);
         audit_record.stmt_type_ = sql::stmt::T_KV_INSERT;
         break;
       case ObTableProccessType::TABLE_API_SINGLE_GET:
-        EVENT_INC(TABLEAPI_RETRIEVE_COUNT);
-        EVENT_ADD(TABLEAPI_RETRIEVE_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_RETRIEVE_COUNT);
+          EVENT_ADD(TABLEAPI_RETRIEVE_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_get);
         audit_record.stmt_type_ = sql::stmt::T_KV_GET;
         break;
       case ObTableProccessType::TABLE_API_SINGLE_DELETE:
-        EVENT_INC(TABLEAPI_DELETE_COUNT);
-        EVENT_ADD(TABLEAPI_DELETE_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_DELETE_COUNT);
+          EVENT_ADD(TABLEAPI_DELETE_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_delete);
         audit_record.stmt_type_ = sql::stmt::T_KV_DELETE;
         break;
       case ObTableProccessType::TABLE_API_SINGLE_UPDATE:
-        EVENT_INC(TABLEAPI_UPDATE_COUNT);
-        EVENT_ADD(TABLEAPI_UPDATE_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_UPDATE_COUNT);
+          EVENT_ADD(TABLEAPI_UPDATE_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_update);
         audit_record.stmt_type_ = sql::stmt::T_KV_UPDATE;
         break;
       case ObTableProccessType::TABLE_API_SINGLE_INSERT_OR_UPDATE:
-        EVENT_INC(TABLEAPI_INSERT_OR_UPDATE_COUNT);
-        EVENT_ADD(TABLEAPI_INSERT_OR_UPDATE_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_INSERT_OR_UPDATE_COUNT);
+          EVENT_ADD(TABLEAPI_INSERT_OR_UPDATE_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_insert_or_update);
         audit_record.stmt_type_ = sql::stmt::T_KV_INSERT_OR_UPDATE;
         break;
       case ObTableProccessType::TABLE_API_SINGLE_REPLACE:
-        EVENT_INC(TABLEAPI_REPLACE_COUNT);
-        EVENT_ADD(TABLEAPI_REPLACE_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_REPLACE_COUNT);
+          EVENT_ADD(TABLEAPI_REPLACE_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_replace);
         audit_record.stmt_type_ = sql::stmt::T_KV_REPLACE;
         break;
       case ObTableProccessType::TABLE_API_SINGLE_INCREMENT:
-        EVENT_INC(TABLEAPI_INCREMENT_COUNT);
-        EVENT_ADD(TABLEAPI_INCREMENT_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_INCREMENT_COUNT);
+          EVENT_ADD(TABLEAPI_INCREMENT_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_increment);
         audit_record.stmt_type_ = sql::stmt::T_KV_INCREMENT;
         break;
       case ObTableProccessType::TABLE_API_SINGLE_APPEND:
-        EVENT_INC(TABLEAPI_APPEND_COUNT);
-        EVENT_ADD(TABLEAPI_APPEND_TIME, elapsed_us);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_APPEND_COUNT);
+          EVENT_ADD(TABLEAPI_APPEND_TIME, elapsed_us);
+        }
         SET_AUDIT_SQL_STRING(single_append);
         audit_record.stmt_type_ = sql::stmt::T_KV_APPEND;
         break;
 
       // table batch mutate
       case ObTableProccessType::TABLE_API_MULTI_INSERT:
-        EVENT_INC(TABLEAPI_MULTI_INSERT_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_INSERT_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_INSERT_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_INSERT_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_INSERT_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_INSERT_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_insert);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_INSERT;
         break;
       case ObTableProccessType::TABLE_API_MULTI_GET:
-        EVENT_INC(TABLEAPI_MULTI_RETRIEVE_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_RETRIEVE_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_RETRIEVE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_RETRIEVE_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_RETRIEVE_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_RETRIEVE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_get);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_GET;
         break;
       case ObTableProccessType::TABLE_API_MULTI_DELETE:
-        EVENT_INC(TABLEAPI_MULTI_DELETE_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_DELETE_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_DELETE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_DELETE_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_DELETE_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_DELETE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_delete);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_DELETE;
         break;
       case ObTableProccessType::TABLE_API_MULTI_UPDATE:
-        EVENT_INC(TABLEAPI_MULTI_UPDATE_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_UPDATE_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_UPDATE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_UPDATE_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_UPDATE_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_UPDATE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_update);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_UPDATE;
         break;
       case ObTableProccessType::TABLE_API_MULTI_INSERT_OR_UPDATE:
-        EVENT_INC(TABLEAPI_MULTI_INSERT_OR_UPDATE_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_INSERT_OR_UPDATE_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_INSERT_OR_UPDATE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_INSERT_OR_UPDATE_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_INSERT_OR_UPDATE_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_INSERT_OR_UPDATE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_insert_or_update);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_INSERT_OR_UPDATE;
         break;
       case ObTableProccessType::TABLE_API_MULTI_REPLACE:
-        EVENT_INC(TABLEAPI_MULTI_REPLACE_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_REPLACE_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_REPLACE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_REPLACE_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_REPLACE_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_REPLACE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_replace);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_REPLACE;
         break;
       case ObTableProccessType::TABLE_API_MULTI_INCREMENT:
-        EVENT_INC(TABLEAPI_MULTI_INCREMENT_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_INCREMENT_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_INCREMENT_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_INCREMENT_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_INCREMENT_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_INCREMENT_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_increment);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_INCREMENT;
         break;
       case ObTableProccessType::TABLE_API_MULTI_APPEND:
-        EVENT_INC(TABLEAPI_MULTI_APPEND_COUNT);
-        EVENT_ADD(TABLEAPI_MULTI_APPEND_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_MULTI_APPEND_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_MULTI_APPEND_COUNT);
+          EVENT_ADD(TABLEAPI_MULTI_APPEND_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_MULTI_APPEND_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(multi_append);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_APPEND;
         break;
       case ObTableProccessType::TABLE_API_BATCH_RETRIVE:
-        EVENT_INC(TABLEAPI_BATCH_RETRIEVE_COUNT);
-        EVENT_ADD(TABLEAPI_BATCH_RETRIEVE_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_BATCH_RETRIEVE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_BATCH_RETRIEVE_COUNT);
+          EVENT_ADD(TABLEAPI_BATCH_RETRIEVE_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_BATCH_RETRIEVE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(batch_retrieve);
         audit_record.stmt_type_ = sql::stmt::T_KV_MULTI_GET;
         break;
       case ObTableProccessType::TABLE_API_BATCH_HYBRID:
-        EVENT_INC(TABLEAPI_BATCH_HYBRID_COUNT);
-        EVENT_ADD(TABLEAPI_BATCH_HYBRID_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_BATCH_HYBRID_INSERT_OR_UPDATE_ROW, rows); // @todo row count for each type
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_BATCH_HYBRID_COUNT);
+          EVENT_ADD(TABLEAPI_BATCH_HYBRID_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_BATCH_HYBRID_INSERT_OR_UPDATE_ROW, rows); // @todo row count for each type
+        }
         SET_AUDIT_SQL_STRING(batch_hybrid);
         audit_record.stmt_type_ = sql::stmt::T_KV_OTHER;
         break;
 
       // hbase mutate
       case ObTableProccessType::TABLE_API_HBASE_DELETE:
-        EVENT_INC(HBASEAPI_DELETE_COUNT);
-        EVENT_ADD(HBASEAPI_DELETE_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_DELETE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_DELETE_COUNT);
+          EVENT_ADD(HBASEAPI_DELETE_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_DELETE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_delete);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_DELETE;
         break;
       case ObTableProccessType::TABLE_API_HBASE_PUT:
-        EVENT_INC(HBASEAPI_PUT_COUNT);
-        EVENT_ADD(HBASEAPI_PUT_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_PUT_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_PUT_COUNT);
+          EVENT_ADD(HBASEAPI_PUT_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_PUT_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_put);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_PUT;
         break;
       case ObTableProccessType::TABLE_API_HBASE_CHECK_AND_DELETE:
-        EVENT_INC(HBASEAPI_CHECK_DELETE_COUNT);
-        EVENT_ADD(HBASEAPI_CHECK_DELETE_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_CHECK_DELETE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_CHECK_DELETE_COUNT);
+          EVENT_ADD(HBASEAPI_CHECK_DELETE_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_CHECK_DELETE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_check_and_delete);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_CHECK_AND_DELETE;
         break;
       case ObTableProccessType::TABLE_API_HBASE_CHECK_AND_PUT:
-        EVENT_INC(HBASEAPI_CHECK_PUT_COUNT);
-        EVENT_ADD(HBASEAPI_CHECK_PUT_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_CHECK_PUT_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_CHECK_PUT_COUNT);
+          EVENT_ADD(HBASEAPI_CHECK_PUT_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_CHECK_PUT_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_check_and_put);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_CHECK_AND_PUT;
         break;
       case ObTableProccessType::TABLE_API_HBASE_INCREMENT:
-        EVENT_INC(HBASEAPI_INCREMENT_COUNT);
-        EVENT_ADD(HBASEAPI_INCREMENT_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_INCREMENT_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_INCREMENT_COUNT);
+          EVENT_ADD(HBASEAPI_INCREMENT_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_INCREMENT_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_increment);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_INCREMENT;
         break;
       case ObTableProccessType::TABLE_API_HBASE_APPEND:
-        EVENT_INC(HBASEAPI_APPEND_COUNT);
-        EVENT_ADD(HBASEAPI_APPEND_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_APPEND_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_APPEND_COUNT);
+          EVENT_ADD(HBASEAPI_APPEND_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_APPEND_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_append);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_APPEND;
         break;
       case ObTableProccessType::TABLE_API_HBASE_HYBRID:
-        EVENT_INC(HBASEAPI_HYBRID_COUNT);
-        EVENT_ADD(HBASEAPI_HYBRID_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_HYBRID_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_HYBRID_COUNT);
+          EVENT_ADD(HBASEAPI_HYBRID_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_HYBRID_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_hybrid);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_OTHER;
         break;
 
       // table query
       case ObTableProccessType::TABLE_API_TABLE_QUERY:
-        EVENT_INC(TABLEAPI_QUERY_COUNT);
-        EVENT_ADD(TABLEAPI_QUERY_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_QUERY_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_QUERY_COUNT);
+          EVENT_ADD(TABLEAPI_QUERY_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_QUERY_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(table_query);
         audit_record.stmt_type_ = sql::stmt::T_KV_QUERY;
         break;
 
       // hbase query
       case ObTableProccessType::TABLE_API_HBASE_QUERY:
-        EVENT_INC(HBASEAPI_SCAN_COUNT);
-        EVENT_ADD(HBASEAPI_SCAN_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_SCAN_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_SCAN_COUNT);
+          EVENT_ADD(HBASEAPI_SCAN_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_SCAN_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_scan);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_SCAN;
         break;
 
       // table query sync
       case ObTableProccessType::TABLE_API_TABLE_QUERY_SYNC:
-        EVENT_INC(TABLEAPI_QUERY_COUNT);
-        EVENT_ADD(TABLEAPI_QUERY_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_QUERY_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_QUERY_COUNT);
+          EVENT_ADD(TABLEAPI_QUERY_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_QUERY_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(table_query_sync);
         audit_record.stmt_type_ = sql::stmt::T_KV_QUERY;
         break;
 
       // hbase query sync
       case ObTableProccessType::TABLE_API_HBASE_QUERY_SYNC:
-        EVENT_INC(HBASEAPI_SCAN_COUNT);
-        EVENT_ADD(HBASEAPI_SCAN_TIME, elapsed_us);
-        EVENT_ADD(HBASEAPI_SCAN_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(HBASEAPI_SCAN_COUNT);
+          EVENT_ADD(HBASEAPI_SCAN_TIME, elapsed_us);
+          EVENT_ADD(HBASEAPI_SCAN_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(hbase_scan_sync);
         audit_record.stmt_type_ = sql::stmt::T_HBASE_SCAN;
         break;
 
       // table query_and_mutate
       case ObTableProccessType::TABLE_API_QUERY_AND_MUTATE:
-        EVENT_INC(TABLEAPI_QUERY_AND_MUTATE_COUNT);
-        EVENT_ADD(TABLEAPI_QUERY_AND_MUTATE_TIME, elapsed_us);
-        EVENT_ADD(TABLEAPI_QUERY_AND_MUTATE_ROW, rows);
+        if (OB_SUCCESS == audit_record.status_) {
+          EVENT_INC(TABLEAPI_QUERY_AND_MUTATE_COUNT);
+          EVENT_ADD(TABLEAPI_QUERY_AND_MUTATE_TIME, elapsed_us);
+          EVENT_ADD(TABLEAPI_QUERY_AND_MUTATE_ROW, rows);
+        }
         SET_AUDIT_SQL_STRING(table_query_and_mutate);
         audit_record.stmt_type_ = sql::stmt::T_KV_QUERY_AND_MUTATE;
         break;
