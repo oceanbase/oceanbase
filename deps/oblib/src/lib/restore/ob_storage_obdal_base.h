5bc14369dd9687fbfb64f5d08b5d93e52c853a21
kpole <751711132@qq.com>
2025-07-11 23:49:13 +0000

    fix gcs timeout

--- deps/oblib/src/lib/restore/ob_storage_obdal_base.h
+++ deps/oblib/src/lib/restore/ob_storage_obdal_base.h
@@ -34,6 +34,7 @@ void fin_obdal_env();
 
 static constexpr int POOL_MAX_IDLE_PER_HOST = 32;         // the max idle http client count
 static constexpr int POOL_MAX_IDLE_TIME_S = 45;          // the max time of idle http client (unit s)
+static constexpr int CONNECT_TIMEOUT_S = 10;             // the max time of connect timeout (unit s)
 
 static constexpr int MAX_OBDAL_REGION_LENGTH = 128;
 static constexpr int MAX_OBDAL_ENDPOINT_LENGTH = 256;
