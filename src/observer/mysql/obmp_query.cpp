6b190662e93f0e570f1582a176a1708fad8b325b
obdev <obdev@oceanbase.com>
2025-07-15 07:10:29 +0000

    [CP] fix free spm baseline core when flush lib cache

--- src/observer/mysql/obmp_query.cpp
+++ src/observer/mysql/obmp_query.cpp
@@ -459,6 +459,7 @@ int ObMPQuery::process_single_stmt(const ObMultiStmtItem &multi_stmt_item,
 {
   int ret = OB_SUCCESS;
   FLTSpanGuard(mpquery_single_stmt);
+  ObReqTimeGuard req_timeinfo_guard;
   ctx_.spm_ctx_.reset();
   bool need_response_error = true;
   session.get_raw_audit_record().request_memory_used_ = 0;
