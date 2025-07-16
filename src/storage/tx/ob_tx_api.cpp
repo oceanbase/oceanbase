365bdc5a94c06c11984b4c9b94f16fbb3640525a
chinaxing <chen.yack@gmail.com>
2025-07-16 03:48:50 +0000

    [CP] fix callback check by session will let commit timeout incorrectly

--- src/storage/tx/ob_tx_api.cpp
+++ src/storage/tx/ob_tx_api.cpp
@@ -582,6 +582,7 @@ int ObTransService::submit_commit_tx(ObTxDesc &tx,
         tx.state_ == ObTxDesc::State::ACTIVE ||
         tx.state_ == ObTxDesc::State::IMPLICIT_ACTIVE)) {
       ObTxDesc::State state0 = tx.state_;
+      tx.commit_expire_ts_ = expire_ts;
       tx.state_ = ObTxDesc::State::IN_TERMINATE;
       // record trace_info
       if (OB_NOT_NULL(trace_info) &&
