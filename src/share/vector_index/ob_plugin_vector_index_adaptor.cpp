7a10d5729c556d32083be68deb4e75ef8d1f1fef
884244693 <884244693@qq.com>
2025-07-23 16:18:27 +0000

    [CP] [fix] query get no table id partial adapter

--- src/share/vector_index/ob_plugin_vector_index_adaptor.cpp
+++ src/share/vector_index/ob_plugin_vector_index_adaptor.cpp
@@ -2998,9 +2998,13 @@ int ObPluginVectorIndexAdaptor::query_result(ObLSID &ls_id,
       } else if (get_snapshot_key_prefix().empty() ||
           !row->storage_datums_[0].get_string().prefix_match(get_snapshot_key_prefix()))
       {
-        ctx->status_ = PVQ_REFRESH;
-        LOG_INFO("query result need refresh adapter, ls leader",
-          K(ret), K(ls_id), K(ctx->get_ls_leader()), K(get_snapshot_key_prefix()), K(row->storage_datums_[0].get_string()));
+        if (get_create_type() == CreateTypeComplete) {
+          ctx->status_ = PVQ_REFRESH;
+          LOG_INFO("query result need refresh adapter, ls leader",
+              K(ret), K(ls_id), K(ctx->get_ls_leader()), K(get_snapshot_key_prefix()), K(row->storage_datums_[0].get_string()));
+        } else if (OB_FAIL(deserialize_snap_data(query_cond, row))) {
+          LOG_WARN("failed to deserialize snap data", K(ret));
+        }
       }
     }
 
@@ -3021,6 +3025,46 @@ int ObPluginVectorIndexAdaptor::query_result(ObLSID &ls_id,
     LOG_WARN("failed to check if vector index need optimize", K(tmp_ret));
   }
 
+  return ret;
+}
+
+int ObPluginVectorIndexAdaptor::deserialize_snap_data(ObVectorQueryConditions *query_cond, blocksstable::ObDatumRow *row)
+{
+  int ret = OB_SUCCESS;
+  ObVectorIndexAlgorithmType index_type;
+  ObString key_prefix;
+  ObTableScanIterator *table_scan_iter = static_cast<ObTableScanIterator *>(query_cond->row_iter_);
+  if (OB_ISNULL(table_scan_iter) || OB_ISNULL(query_cond)) {
+    ret = OB_ERR_UNEXPECTED;
+    LOG_WARN("get null pointer.", K(ret), K(table_scan_iter), K(query_cond));
+  } else if (OB_ISNULL(row) || row->get_column_count() < 2) {
+    ret = OB_ERR_UNEXPECTED;
+    LOG_WARN("invalid row", K(ret), K(row));
+  } else if (OB_FAIL(ob_write_string(*allocator_, row->storage_datums_[0].get_string(), key_prefix))) {
+    LOG_WARN("failed to write string", K(ret), K(row->storage_datums_[0].get_string()));
+  } else if (OB_FAIL(ObPluginVectorIndexUtils::iter_table_rescan(*query_cond->scan_param_, table_scan_iter))) {
+    LOG_WARN("failed to rescan", K(ret));
+  } else {
+    ObArenaAllocator tmp_allocator("VectorAdaptor", OB_MALLOC_NORMAL_BLOCK_SIZE, tenant_id_);
+    ObHNSWDeserializeCallback::CbParam param;
+    param.iter_ = query_cond->row_iter_;
+    param.allocator_ = &tmp_allocator;
+    ObHNSWDeserializeCallback callback(static_cast<void*>(this));
+    ObIStreamBuf::Callback cb = callback;
+    ObVectorIndexSerializer index_seri(tmp_allocator);
+    TCWLockGuard lock_guard(snap_data_->mem_data_rwlock_);
+    ObString target_prefix;
+    if (!get_snapshot_key_prefix().empty() && key_prefix.prefix_match(get_snapshot_key_prefix()) && !snap_data_->rb_flag_) {
+      // skip deserialize, already been deserialized by other concurrent thread
+    } else if (OB_FAIL(index_seri.deserialize(snap_data_->index_, param, cb, tenant_id_))) {
+      LOG_WARN("serialize index failed.", K(ret));
+    } else if (OB_FALSE_IT(index_type = get_snap_index_type())) {
+    } else if (OB_FAIL(ObPluginVectorIndexUtils::get_split_snapshot_prefix(index_type, key_prefix, target_prefix))) {
+      LOG_WARN("fail to get split snapshot prefix", K(ret), K(index_type), K(key_prefix));
+    } else if (OB_FAIL(set_snapshot_key_prefix(target_prefix))) {
+      LOG_WARN("failed to set snapshot key prefix", K(ret), K(index_type), K(target_prefix));
+    }
+  }
   return ret;
 }
 
