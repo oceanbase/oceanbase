babde04cacbd4e455ad7e68edbd39888dba34f48
fforkboat <fforkboat@gmail.com>
2025-07-24 17:19:29 +0000

    [CP] fix CDB_MVIEW_RUNNING_JOBS bug

--- src/share/inner_table/ob_inner_table_schema.21651_21700.cpp
+++ src/share/inner_table/ob_inner_table_schema.21651_21700.cpp
@@ -213,7 +213,7 @@ int ObInnerTableSchema::cdb_mview_running_jobs_schema(ObTableSchema &table_schem
   table_schema.set_collation_type(ObCharset::get_default_collation(ObCharset::get_default_charset()));
 
   if (OB_SUCC(ret)) {
-    if (OB_FAIL(table_schema.set_view_definition(R"__(     SELECT       A.SVR_IP,       A.SVR_PORT,       A.TENANT_ID,       B.TABLE_NAME,       CAST (        CASE A.JOB_TYPE         WHEN 0 THEN 'INVALID'         WHEN 1 THEN 'COMPLETE REFRESH'         WHEN 2 THEN 'FAST REFRESH'         WHEN 3 THEN 'PURGE MLOG'         ELSE NULL        END AS CHAR(64)       ) AS JOB_TYPE,       A.SESSION_ID,       A.READ_SNAPSHOT,       A.PARALLEL,       A.JOB_START_TIME     FROM oceanbase.__all_virtual_mview_running_job A,          oceanbase.__all_virtual_table B     WHERE A.table_id = B.table_id )__"))) {
+    if (OB_FAIL(table_schema.set_view_definition(R"__(     SELECT       A.SVR_IP,       A.SVR_PORT,       A.TENANT_ID,       B.TABLE_NAME,       CAST (        CASE A.JOB_TYPE         WHEN 0 THEN 'INVALID'         WHEN 1 THEN 'COMPLETE REFRESH'         WHEN 2 THEN 'FAST REFRESH'         WHEN 3 THEN 'PURGE MLOG'         ELSE NULL        END AS CHAR(64)       ) AS JOB_TYPE,       A.SESSION_ID,       A.READ_SNAPSHOT,       A.PARALLEL,       A.JOB_START_TIME     FROM oceanbase.__all_virtual_mview_running_job A,          oceanbase.__all_virtual_table B     WHERE A.table_id = B.table_id     AND A.tenant_id = B.tenant_id )__"))) {
       LOG_ERROR("fail to set view_definition", K(ret));
     }
   }
