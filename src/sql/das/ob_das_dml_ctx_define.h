1946619334415f416f3c6e6eac5c4ac8b57a85d1
linbo-lin <1434442536@qq.com>
2025-07-29 21:19:46 +0000

    Add place holder for struct ObDASDMLBaseCtDef

--- src/sql/das/ob_das_dml_ctx_define.h
+++ src/sql/das/ob_das_dml_ctx_define.h
@@ -93,7 +93,8 @@ public:
       uint64_t is_update_pk_with_dop_           : 1; // update primary_table PK
       uint64_t is_main_table_in_fts_ddl_        : 1; // main table is in fts ddl for mode of unstable ftparser.
       uint64_t is_update_pk_                    : 1;
-      uint64_t reserved_                        : 48; //add new flag before reserved_
+      uint64_t is_vec_hnsw_index_vid_opt_       : 1;  // hnsw index vid opt for tables without pk
+      uint64_t reserved_                        : 47; //add new flag before reserved_
       uint64_t compat_version_                  : 4; //prohibited to insert new flags between compat_version_ and reserved_
     };
   };
