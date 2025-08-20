4d5a551f0a9e1d28f76dcbc49b9ee67f0549643d
obdev <obdev@oceanbase.com>
2025-08-20 09:44:29 +0000

    revert 延迟rpc请求分配di的时机，避免队列积压时无用的信息填满ash buffer

--- deps/oblib/src/lib/stat/ob_diagnostic_info.h
+++ deps/oblib/src/lib/stat/ob_diagnostic_info.h
@@ -541,10 +541,6 @@ public:
   {
     return di_rpc_cache_;
   }
-  bool rpc_throttle() const
-  {
-    return rpc_throttle_;
-  }
 
 private:
   union {
@@ -553,7 +549,6 @@ private:
     {
       bool mysql_obrequest_ref_ : 1;
       bool di_rpc_cache_ : 1;
-      bool rpc_throttle_ : 1;
     };
   };
 };
