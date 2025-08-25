8ae4baa59f55c742a92e2c5cd1a469dbaddfd335
bit-dance <2634358021@qq.com>
2025-08-25 15:13:43 +0000

    [CP] [#2025072100110047828]Fix set accuracy for array binding param.

--- src/sql/ob_sql_utils.cpp
+++ src/sql/ob_sql_utils.cpp
@@ -5493,7 +5493,17 @@ int ObSQLUtils::get_one_group_params(int64_t &actual_pos, ParamStore &src, Param
           }
         }
         OX (obj_params.push_back(*(data + actual_pos)));
-        OX (obj_params.at(obj_params.count() - 1).set_accuracy(coll->get_element_type().get_accuracy()));
+        if (OB_SUCC(ret)) {
+          if (SCALE_UNKNOWN_YET != coll->get_element_type().get_accuracy().get_scale()) {
+            OX (obj_params.at(obj_params.count() - 1).set_scale(coll->get_element_type().get_accuracy().get_scale()));
+          }
+          if (PRECISION_UNKNOWN_YET != coll->get_element_type().get_accuracy().get_precision()) {
+            OX (obj_params.at(obj_params.count() - 1).set_precision(coll->get_element_type().get_accuracy().get_precision()));
+          }
+          if (LENGTH_UNKNOWN_YET != coll->get_element_type().get_accuracy().get_length()) {
+            OX (obj_params.at(obj_params.count() - 1).set_length(coll->get_element_type().get_accuracy().get_length()));
+          }
+        }
       }
     }
   }
