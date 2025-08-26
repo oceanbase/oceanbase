9d607aca3aea9a620435b7e94f86c8db1b168ba9
obdev <obdev@oceanbase.com>
2025-08-26 05:49:44 +0000

    fix the bug of test_macro_cache_evict

--- mittest/shared_storage/simple_server/test_macro_cache_evict.cpp
+++ mittest/shared_storage/simple_server/test_macro_cache_evict.cpp
@@ -303,7 +303,7 @@ public:
       i = i+1;
       macro_block_free_size = disk_space_mgr->get_macro_cache_free_size();
     }
-    ASSERT_TRUE(macro_block_free_size < WRITE_IO_SIZE);
+    ASSERT_LT(macro_block_free_size, WRITE_IO_SIZE);
     // write to bucket   ->   check write through
     macro_cache_write(i, tenant_file_mgr, ObSSMacroCacheType::MACRO_BLOCK, test_type, true);
     // before evict
@@ -318,7 +318,7 @@ public:
     macro_block_used_size_af_evict = disk_space_mgr->macro_cache_stats_[static_cast<uint8_t>(ObSSMacroCacheType::MACRO_BLOCK)].used_;
     tmp_file_used_size_af_evict = disk_space_mgr->macro_cache_stats_[static_cast<uint8_t>(ObSSMacroCacheType::TMP_FILE)].used_;
     info_log(ObSSMacroCacheType::MACRO_BLOCK, -1, disk_space_mgr, macro_cache_mgr, test_type);
-    ASSERT_TRUE(macro_block_used_size_af_evict < macro_block_used_size);
+    ASSERT_LT(macro_block_used_size_af_evict, macro_block_used_size);
     // delete file
     delete_all_wr_file(tmp_file_num, tenant_file_mgr, ObSSMacroCacheType::TMP_FILE);
     delete_all_wr_file(i, tenant_file_mgr, ObSSMacroCacheType::MACRO_BLOCK);
@@ -362,7 +362,7 @@ public:
       i = i+1;
       meta_file_free_size = disk_space_mgr->get_macro_cache_free_size();
     }
-    ASSERT_TRUE(meta_file_free_size < WRITE_IO_SIZE);
+    ASSERT_LT(meta_file_free_size, WRITE_IO_SIZE);
     // write to bucket   ->   check write through
     macro_cache_write(i, tenant_file_mgr, ObSSMacroCacheType::META_FILE, test_type, true);
     // before evict
@@ -377,7 +377,7 @@ public:
     macro_block_used_size_af_evict = disk_space_mgr->macro_cache_stats_[static_cast<uint8_t>(ObSSMacroCacheType::MACRO_BLOCK)].used_;
     hot_tablet_used_size_af_evict = disk_space_mgr->macro_cache_stats_[static_cast<uint8_t>(ObSSMacroCacheType::HOT_TABLET_MACRO_BLOCK)].used_;
     info_log(ObSSMacroCacheType::META_FILE, -1, disk_space_mgr, macro_cache_mgr, test_type);
-    ASSERT_TRUE(macro_block_used_size_af_evict < macro_block_used_size);
+    ASSERT_LT(macro_block_used_size_af_evict, macro_block_used_size);
     // delete file
     delete_all_wr_file(hot_tablet_num, tenant_file_mgr, ObSSMacroCacheType::HOT_TABLET_MACRO_BLOCK);
     delete_all_wr_file(i, tenant_file_mgr, ObSSMacroCacheType::META_FILE);
@@ -518,7 +518,7 @@ public:
       cur_type_free_size = disk_space_mgr->get_macro_cache_free_size();
       i = i+1;
     }
-    ASSERT_TRUE(cur_type_free_size < WRITE_IO_SIZE);
+    ASSERT_LT(cur_type_free_size, WRITE_IO_SIZE);
     // write to bucket   ->   check write through
     macro_cache_write(i, tenant_file_mgr, cache_type, test_type, true);
     // before evict record
@@ -539,16 +539,16 @@ public:
     info_log(cache_type, -1, disk_space_mgr, macro_cache_mgr, test_type);
     switch (cache_type) {
       case ObSSMacroCacheType::MACRO_BLOCK:
-        ASSERT_TRUE(macro_block_used_size_af_evict < macro_block_used_size);
+        ASSERT_LT(macro_block_used_size_af_evict, macro_block_used_size);
         break;
       case ObSSMacroCacheType::META_FILE:
-        ASSERT_TRUE(macro_block_used_size_af_evict < macro_block_used_size);
+        ASSERT_LT(macro_block_used_size_af_evict, macro_block_used_size);
         break;
       case ObSSMacroCacheType::TMP_FILE:
-        ASSERT_TRUE(tmp_file_used_size_af_evict < tmp_file_used_size);
+        ASSERT_LT(tmp_file_used_size_af_evict, tmp_file_used_size);
         break;
       case ObSSMacroCacheType::HOT_TABLET_MACRO_BLOCK:
-        ASSERT_TRUE(hot_tablet_used_size_af_evict < hot_tablet_used_size);
+        ASSERT_LT(hot_tablet_used_size_af_evict, hot_tablet_used_size);
         break;
       default:
         ASSERT_TRUE(false);
@@ -585,12 +585,17 @@ public:
     info_log(ObSSMacroCacheType::MACRO_BLOCK, i, disk_space_mgr, macro_cache_mgr, test_type);
     // start write cache flush
     macro_cache_mgr->write_cache_ctrl_task_.is_inited_ = true;
-    sleep(60); // some time for write cache flush
     i = i + 1;
-    // after flush
-    write_cache_size_af_flush = macro_cache_mgr->get_write_cache_size();
+    // some time for write cache flush
+    for (int64_t j = 0; j < 80; j++) {
+      sleep(1);
+      write_cache_size_af_flush = macro_cache_mgr->get_write_cache_size();
+      if (write_cache_size_af_flush < write_cache_size) {
+        break;
+      }
+    }
     info_log(ObSSMacroCacheType::MACRO_BLOCK, -1, disk_space_mgr, macro_cache_mgr, test_type);
-    ASSERT_TRUE(write_cache_size_af_flush < write_cache_size);
+    ASSERT_LT(write_cache_size_af_flush, write_cache_size);
     // delete file
     delete_all_wr_file(i, tenant_file_mgr, ObSSMacroCacheType::MACRO_BLOCK);
     info_log(ObSSMacroCacheType::MACRO_BLOCK, -1, disk_space_mgr, macro_cache_mgr, test_type);
@@ -598,7 +603,7 @@ public:
   }
 
   public:
-    static const int64_t WRITE_IO_SIZE = 2 * 1024L * 1024L; // 2MB
+    static constexpr int64_t WRITE_IO_SIZE = 2 * 1024L * 1024L; // 2MB
     static const uint64_t MACRO_BLOCK_TABLET_ID = 200004; // tablet_id for MACRO_BLOCK
     static const uint64_t TMP_FILE_ID = 100; // tmp_file_id for TMP_FILE
     static const uint64_t HOT_TABLET_TABLET_ID = 200005; // tablet_id for HOT_TABLET_MACRO_BLOCK
