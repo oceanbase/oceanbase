527625ca03f07872e144b30ba0d93aea2f9a152a
884244693 <884244693@qq.com>
2025-09-11 15:20:50 +0000

    [CP] [fix] adapter memdata nullptr core cause by renew

--- src/share/vector_index/ob_plugin_vector_index_adaptor.h
+++ src/share/vector_index/ob_plugin_vector_index_adaptor.h
@@ -573,6 +573,7 @@ public:
   }
 
   int renew_single_snap_index(bool mem_saving_mode);
+  int renew_snapdata_in_lock();
   int set_adaptor_ctx_flag(ObVectorQueryAdaptorResultContext *ctx);
 
   ObString &get_index_identity() { return index_identity_; };
