cbf5761fa65e8f69d7ac2ba6db0939e64f5cf9cd
obdev <obdev@oceanbase.com>
2025-09-18 08:49:53 +0000

    replace, bitwise系列, crc32, from_base64表达式向量化2.0占位

--- src/sql/engine/expr/ob_expr_eval_functions.cpp
+++ src/sql/engine/expr/ob_expr_eval_functions.cpp
@@ -1879,6 +1879,17 @@ static ObExpr::EvalVectorFunc g_expr_eval_vector_functions[] = {
   ObExprMul::mul_decint128_int32_decint128_vector,                       /* 246 */
   ObExprMul::mul_decint256_decint256_int32_vector,                       /* 247 */
   ObExprMul::mul_decint256_int32_decint256_vector,                       /* 248 */
+  NULL, // ObExprReplace::eval_replace_vector,                           /* 249 */
+  NULL, // ObExprBitCount::calc_bitcount_expr_vector,                    /* 250 */
+  NULL, // ObExprBitNeg::calc_bitneg_expr_vector,                        /* 251 */
+  NULL, // ObExprBitLength::calc_bit_length_vector,                      /* 252 */
+  NULL, // ObBitwiseExprOperator::calc_bitwise_result2_mysql_vector<ObBitwiseExprOperator::BIT_AND>,          /* 253 */
+  NULL, // ObBitwiseExprOperator::calc_bitwise_result2_mysql_vector<ObBitwiseExprOperator::BIT_OR>,           /* 254 */
+  NULL, // ObBitwiseExprOperator::calc_bitwise_result2_mysql_vector<ObBitwiseExprOperator::BIT_XOR>,          /* 255 */
+  NULL, // ObBitwiseExprOperator::calc_bitwise_result2_mysql_vector<ObBitwiseExprOperator::BIT_LEFT_SHIFT>,          /* 256 */
+  NULL, // ObBitwiseExprOperator::calc_bitwise_result2_mysql_vector<ObBitwiseExprOperator::BIT_RIGHT_SHIFT>,          /* 257 */
+  NULL, // ObExprCrc32::calc_crc32_expr_vector,                          /* 258 */
+  NULL, // ObExprFromBase64::eval_from_base64_vector                     /* 259 */
 };
 
 REG_SER_FUNC_ARRAY(OB_SFA_SQL_EXPR_EVAL,
