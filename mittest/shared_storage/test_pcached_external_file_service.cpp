941bf12b4764d245cbd3a6291e3bdf4c34d31be5
obdev <obdev@oceanbase.com>
2025-09-28 11:21:34 +0000

    fix unstable external table disk cache mittests

--- mittest/shared_storage/test_pcached_external_file_service.cpp
+++ mittest/shared_storage/test_pcached_external_file_service.cpp
@@ -166,9 +166,6 @@ TEST_F(TestSSPCachedExternalFileService, test_prefetch_task_basic)
     LOG_INFO("check read task", K(accesser->timer_task_scheduler_));
 
     // check write task
-    ASSERT_EQ(1, accesser->running_prefetch_tasks_map_.size());
-    ASSERT_EQ(0, accesser->timer_task_scheduler_.ss_prefetch_timer_.pipeline_.async_read_list_.get_curr_total());
-    ASSERT_EQ(1, accesser->timer_task_scheduler_.ss_prefetch_timer_.pipeline_.async_write_list_.get_curr_total());
     const int64_t WRITE_TIME_LIMIT_US = ObTimeUtility::current_time() + MAX_WAIT_TIME_US;
     while (0 < accesser->timer_task_scheduler_.ss_prefetch_timer_.pipeline_.async_write_list_.get_curr_total()
         && ObTimeUtility::current_time() < WRITE_TIME_LIMIT_US) {
