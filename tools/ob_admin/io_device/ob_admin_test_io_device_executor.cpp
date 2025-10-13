85dbaec6a7d30d26a659d20131b167b9e62a0112
Gmldry <1187044372@qq.com>
2025-10-13 03:14:02 +0000

    Remove redundant initialization code of ob_admin

--- tools/ob_admin/io_device/ob_admin_test_io_device_executor.cpp
+++ tools/ob_admin/io_device/ob_admin_test_io_device_executor.cpp
@@ -42,26 +42,6 @@ int ObAdminTestIODeviceExecutor::execute(int argc, char *argv[])
   int ret = OB_SUCCESS;
   lib::set_memory_limit(4 * 1024 * 1024 * 1024LL);
   lib::set_tenant_memory_limit(500, 4 * 1024 * 1024 * 1024LL);
-  ObTenantBase *tenant_base = new ObTenantBase(OB_SERVER_TENANT_ID);
-  ObMallocAllocator *malloc = ObMallocAllocator::get_instance();
-  if (OB_ISNULL(malloc->get_tenant_ctx_allocator(OB_SERVER_TENANT_ID, 0))) {
-    if (OB_FAIL(malloc->create_and_add_tenant_allocator(OB_SERVER_TENANT_ID))) {
-      STORAGE_LOG(WARN, "failed to create_and_add_tenant_allocator", K(ret));
-    }
-  }
-
-  if (FAILEDx(tenant_base->init())) {
-    STORAGE_LOG(WARN, "failed to init tenant base", K(ret));
-  } else if (FALSE_IT(ObTenantEnv::set_tenant(tenant_base))) {
-  } else if (OB_FAIL(ObDeviceManager::get_instance().init_devices_env())) {
-    STORAGE_LOG(WARN, "init device manager failed", KR(ret));
-  } else if (OB_FAIL(ObIOManager::get_instance().init())) {
-    STORAGE_LOG(WARN, "failed to init io manager", K(ret));
-  } else if (OB_FAIL(ObIOManager::get_instance().start())) {
-    STORAGE_LOG(WARN, "failed to start io manager", K(ret));
-  }  else if (OB_FAIL(ObObjectStorageInfo::register_cluster_version_mgr(&ObClusterVersionBaseMgr::get_instance()))) {
-    STORAGE_LOG(WARN, "fail to register cluster version mgr", KR(ret));
-  }
 
   if (OB_FAIL(parse_cmd_(argc, argv))) {
     STORAGE_LOG_FILTER(ERROR, "failed to parse cmd", K(ret), K(argc), K(argv));
