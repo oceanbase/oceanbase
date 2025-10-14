cd6863bf116cfee8aa12f8c62e5b472405d9e7ce
obdev <obdev@oceanbase.com>
2025-10-14 04:43:54 +0000

    [to #2025091700111009661]Bugfix: Fix several bugs in associative array constructor with =>

--- src/sql/engine/expr/ob_expr_collection_construct.cpp
+++ src/sql/engine/expr/ob_expr_collection_construct.cpp
@@ -59,10 +59,12 @@ int ObExprCollectionConstruct::calc_result_typeN(ObExprResType &type,
                                                  ObExprTypeCtx &type_ctx) const
 {
   int ret = OB_SUCCESS;
-  UNUSED(type_ctx);
+  const ObCollectionConstructRawExpr *pl_expr
+          = static_cast<const ObCollectionConstructRawExpr*>(type_ctx.get_raw_expr());
+  CK(OB_NOT_NULL(pl_expr));
   for (int64_t i = 0; OB_SUCC(ret) && i < param_num; i++) {
     if ((ObExtendType == elem_type_.get_obj_type()
-          && types[i].get_type() != ObExtendType && types[i].get_type() != ObNullType)
+          && types[i].get_type() != ObExtendType && types[i].get_type() != ObNullType && !(0 == i % 2 && pl_expr->is_associative_array_with_param_assign_op_))
         ||(ObExtendType == types[i].get_type() && elem_type_.get_obj_type() != ObExtendType)) {
       ObSchemaGetterGuard schema_guard;
       int64_t tenant_id = type_ctx.get_session()->get_effective_tenant_id();
@@ -80,8 +82,6 @@ int ObExprCollectionConstruct::calc_result_typeN(ObExprResType &type,
       types[i].set_calc_accuracy(elem_type_.get_accuracy());
       types[i].set_calc_meta(elem_type_.get_meta_type());
     }
-    const ObCollectionConstructRawExpr *pl_expr
-          = static_cast<const ObCollectionConstructRawExpr*>(type_ctx.get_raw_expr());
     CK(OB_NOT_NULL(pl_expr));
     if(pl_expr->is_associative_array_with_param_assign_op_) {
       if(0 == i % 2) {
@@ -307,23 +307,30 @@ int ObExprCollectionConstruct::eval_collection_construct(const ObExpr &expr,
     OX (coll->set_not_null(info->not_null_));
 
     // recursive construction is not needed
-    if(!info->is_associative_array_with_param_assign_op_) {
-      OZ (ObSPIService::spi_set_collection(session->get_effective_tenant_id(),
-                                           ns,
-                                           *coll,
-                                           expr.arg_cnt_));
-      if (OB_SUCC(ret) && coll->is_associative_array() && expr.arg_cnt_ > 0) {
-        OX (coll->set_first(1));
-        OX (coll->set_last(coll->get_count()));
+    if (OB_SUCC(ret)) {
+      if(!info->is_associative_array_with_param_assign_op_) {
+        OZ (ObSPIService::spi_set_collection(session->get_effective_tenant_id(),
+                                             ns,
+                                             *coll,
+                                             expr.arg_cnt_));
+        if (OB_SUCC(ret) && coll->is_associative_array() && expr.arg_cnt_ > 0) {
+          OX (coll->set_first(1));
+          OX (coll->set_last(coll->get_count()));
+        }
       }
     }
-
     if (OB_SUCC(ret)) {
       if (info->elem_type_.get_meta_type().is_ext()) {
+        // for associative array with param assign op
+        int64_t index = OB_INVALID_INDEX;
+        int64_t search_end = OB_INVALID_INDEX;
+        bool is_repeated_key = false;
         for (int64_t i = 0; OB_SUCC(ret) && i < expr.arg_cnt_; ++i) {
           const ObDatum &d = expr.args_[i]->locate_expr_datum(ctx);
           ObObj v;
-          CK (OB_NOT_NULL(coll->get_data()));
+          if (!info->is_associative_array_with_param_assign_op_) {
+            CK (OB_NOT_NULL(coll->get_data()));
+          }
           OZ(d.to_obj(v, expr.args_[i]->obj_meta_, expr.args_[i]->obj_datum_map_));
           // check type match first if value is not null
           if (OB_SUCC(ret) && !v.is_null()) {
@@ -342,8 +349,12 @@ int ObExprCollectionConstruct::eval_collection_construct(const ObExpr &expr,
                 LOG_WARN("invalid argument. unexpected composite value", K(ret), K(v), KPC(coll));
               }
             } else {
-              TYPE_CHECK(v, info->elem_type_.get_meta_type().get_type());
-              OZ (check_match(v, coll->get_element_desc(), *ns));
+              if (0 == i % 2 && info->is_associative_array_with_param_assign_op_) {
+                TYPE_CHECK(v, info->index_type_.get_meta_type().get_type());
+              } else {
+                TYPE_CHECK(v, info->elem_type_.get_meta_type().get_type());
+                OZ (check_match(v, coll->get_element_desc(), *ns));
+              }
               if (OB_ERR_CALL_WRONG_ARG == ret) {
                 int tmp_ret = ret;
                 ret = OB_SUCCESS;
@@ -358,36 +369,113 @@ int ObExprCollectionConstruct::eval_collection_construct(const ObExpr &expr,
               }
             }
           }
-          if (OB_SUCC(ret) && info->not_null_ && !v.is_null()) {
+          if (OB_SUCC(ret) && info->not_null_ && !v.is_null()
+              && !(info->is_associative_array_with_param_assign_op_ && 0 == i % 2)) {
             ObObjParam v1 = v;
             OZ (ObSPIService::spi_check_composite_not_null(&v1));
           }
           if (OB_SUCC(ret)) {
-            if (v.is_null()) {
-              ObObj new_composite;
-              int64_t ptr = 0;
-              int64_t init_size = OB_INVALID_SIZE;
-              OZ (collection_type->get_element_type().newx(*coll->get_allocator(), ns, ptr));
-              OZ (collection_type->get_element_type().get_size(pl::PL_TYPE_INIT_SIZE, init_size));
-              OX (new_composite.set_extend(ptr, collection_type->get_element_type().get_type(), init_size));
-              OX (static_cast<ObObj*>(coll->get_data())[i] = new_composite);
-            } else if (pl::PL_OPAQUE_TYPE == v.get_meta().get_extend_type()
-                      || pl::PL_CURSOR_TYPE == v.get_meta().get_extend_type()
-                      || pl::PL_REF_CURSOR_TYPE == v.get_meta().get_extend_type()) {
-              OZ (pl::ObUserDefinedType::deep_copy_obj(*coll->get_allocator(),
-                                                       v,
-                                                       static_cast<ObObj*>(coll->get_data())[i],
-                                                       true));
+            if (info->is_associative_array_with_param_assign_op_) {
+              pl::ObPLAssocArray* assoc_array = static_cast<pl::ObPLAssocArray*>(coll);
+              CK(OB_NOT_NULL(assoc_array));
+
+              if (0 == i % 2) {
+                if(OB_SUCC(ret) && v.is_null()) {
+                  ret = OB_ERR_NUMERIC_OR_VALUE_ERROR;
+                  LOG_WARN("null key is not allowed", K(ret), K(v));
+                  LOG_USER_ERROR(OB_ERR_NUMERIC_OR_VALUE_ERROR, 23, "null key is not allowed");
+                }
+                OZ (ObSPIService::spi_pad_char_or_varchar(session,
+                                                      info->index_type_.get_obj_type(),
+                                                      info->index_type_.get_accuracy(),
+                                                      alloc,
+                                                      &v));
+                index = OB_INVALID_INDEX;
+                search_end = OB_INVALID_INDEX;
+                if(i != 0) {
+                  OZ(assoc_array->search_key(v, index, search_end, assoc_array->get_count()));
+                } else {
+                  OX(search_end = 0);
+                }
+                is_repeated_key = false;
+                if (OB_SUCC(ret) && 0 != i && index != OB_INVALID_INDEX) {
+                  // It means repeated key, there is no need to extend or insert key, record the index is enough.
+                  is_repeated_key = true;
+                } else {
+                  OZ((ObSPIService::spi_extend_assoc_array(session->get_effective_tenant_id(),
+                                                           session->get_pl_context()->get_current_ctx(),
+                                                           *alloc,
+                                                           *assoc_array,
+                                                           1)));
+                  OZ(assoc_array->insert_sort(v, assoc_array->get_count() - 1, search_end, assoc_array->get_count() - 1));
+                  OZ(assoc_array->update_first_last(OB_INVALID_INDEX == search_end ? 0 : search_end));
+
+                  CK (OB_NOT_NULL(assoc_array->get_key()));
+                  OZ (deep_copy_obj(*coll->get_allocator(),
+                                    v,
+                                    static_cast<ObObj*>(assoc_array->get_key())[i / 2]));
+                }
+              } else {
+                OZ (ObSPIService::spi_pad_char_or_varchar(session,
+                                                        info->elem_type_.get_obj_type(),
+                                                        info->elem_type_.get_accuracy(),
+                                                        alloc,
+                                                        &v));
+                if (v.is_null()) {
+                  ObObj new_composite;
+                  int64_t ptr = 0;
+                  int64_t init_size = OB_INVALID_SIZE;
+                  OZ (collection_type->get_element_type().newx(*coll->get_allocator(), ns, ptr));
+                  OZ (collection_type->get_element_type().get_size(pl::PL_TYPE_INIT_SIZE, init_size));
+                  OX (new_composite.set_extend(ptr, collection_type->get_element_type().get_type(), init_size));
+                  int64_t pos = is_repeated_key ? index : i / 2;
+                  if (is_repeated_key) {
+                    OZ (pl::ObUserDefinedType::destruct_objparam(*coll->get_allocator(),
+                                                                 static_cast<ObObj&>(static_cast<ObObj*>(coll->get_data())[pos]),
+                                                                 nullptr));
+                  }
+                  OX (static_cast<ObObj*>(coll->get_data())[pos] = new_composite);
+                } else {
+                  int64_t pos = is_repeated_key ? index : i / 2;
+                  if (is_repeated_key) {
+                    OZ (pl::ObUserDefinedType::destruct_objparam(*coll->get_allocator(),
+                                                                 static_cast<ObObj&>(static_cast<ObObj*>(assoc_array->get_data())[pos]),
+                                                                 nullptr));
+                  }
+                  OZ (pl::ObUserDefinedType::deep_copy_obj(*coll->get_allocator(),
+                                                           v,
+                                                           static_cast<ObObj*>(assoc_array->get_data())[pos],
+                                                           true));
+                }
+              }
             } else {
-              OZ (pl::ObUserDefinedType::deep_copy_obj(*coll->get_allocator(),
-                                                       v,
-                                                       static_cast<ObObj*>(coll->get_data())[i],
-                                                       true));
+              if (v.is_null()) {
+                ObObj new_composite;
+                int64_t ptr = 0;
+                int64_t init_size = OB_INVALID_SIZE;
+                OZ (collection_type->get_element_type().newx(*coll->get_allocator(), ns, ptr));
+                OZ (collection_type->get_element_type().get_size(pl::PL_TYPE_INIT_SIZE, init_size));
+                OX (new_composite.set_extend(ptr, collection_type->get_element_type().get_type(), init_size));
+                OX (static_cast<ObObj*>(coll->get_data())[i] = new_composite);
+              } else if (pl::PL_OPAQUE_TYPE == v.get_meta().get_extend_type()
+                        || pl::PL_CURSOR_TYPE == v.get_meta().get_extend_type()
+                        || pl::PL_REF_CURSOR_TYPE == v.get_meta().get_extend_type()) {
+                OZ (pl::ObUserDefinedType::deep_copy_obj(*coll->get_allocator(),
+                                                         v,
+                                                         static_cast<ObObj*>(coll->get_data())[i],
+                                                         true));
+              } else {
+                OZ (pl::ObUserDefinedType::deep_copy_obj(*coll->get_allocator(),
+                                                         v,
+                                                         static_cast<ObObj*>(coll->get_data())[i],
+                                                         true));
+              }
             }
           }
         }
       } else {
         if(info->is_associative_array_with_param_assign_op_) {
+          pl::ObPLAssocArray* assoc_array = static_cast<pl::ObPLAssocArray*>(coll);
           for (int64_t i = 0; OB_SUCC(ret) && i < expr.arg_cnt_; i += 2) {
             const ObDatum &d = expr.args_[i]->locate_expr_datum(ctx);
             ObObj key;
@@ -396,48 +484,72 @@ int ObExprCollectionConstruct::eval_collection_construct(const ObExpr &expr,
             const ObDatum &d_for_data = expr.args_[i + 1]->locate_expr_datum(ctx);
             ObObj v;
             OZ(d.to_obj(key, expr.args_[i]->obj_meta_, expr.args_[i]->obj_datum_map_));
+            if(OB_SUCC(ret) && key.is_null()) {
+              ret = OB_ERR_NUMERIC_OR_VALUE_ERROR;
+              LOG_WARN("null key is not allowed", K(ret), K(key));
+              LOG_USER_ERROR(OB_ERR_NUMERIC_OR_VALUE_ERROR, 23, "null key is not allowed");
+            }
             if (OB_SUCC(ret) && !key.is_null()) {
               TYPE_CHECK(key, info->index_type_.get_obj_type());
             }
-            OZ (ObSPIService::spi_pad_char_or_varchar(session,
-                                                      info->index_type_.get_obj_type(),
-                                                      info->index_type_.get_accuracy(),
-                                                      alloc,
-                                                      &key));
-
-            pl::ObPLAssocArray* assoc_array = static_cast<pl::ObPLAssocArray*>(coll);
             CK(OB_NOT_NULL(assoc_array));
             if(i != 0) {
               OZ(assoc_array->search_key(key, index, search_end, assoc_array->get_count()));
             } else {
               OX(search_end = 0);
             }
-            OZ((ObSPIService::spi_extend_assoc_array(session->get_effective_tenant_id(),
-                                                              session->get_pl_context()->get_current_ctx(),
-                                                              *alloc,
-                                                              *assoc_array,
-                                                              1)));
-            OZ(assoc_array->insert_sort(key, assoc_array->get_count() - 1, search_end, assoc_array->get_count() - 1));
-            OZ(assoc_array->update_first_last(OB_INVALID_INDEX == search_end ? 0 : search_end));
+            // check for repeated key in associative array, overwrite the value if key exists
+            if (OB_SUCC(ret) && 0 != i && index != OB_INVALID_INDEX) {
+              OZ (d_for_data.to_obj(v, expr.args_[i + 1]->obj_meta_, expr.args_[i + 1]->obj_datum_map_));
+              if (OB_SUCC(ret) && !v.is_null()) {
+                TYPE_CHECK(v, info->elem_type_.get_meta_type().get_type());
+              }
+              OZ (ObSPIService::spi_pad_char_or_varchar(session,
+                                                        info->elem_type_.get_obj_type(),
+                                                        info->elem_type_.get_accuracy(),
+                                                        alloc,
+                                                        &v));
+              CK (OB_NOT_NULL(assoc_array->get_data()));
+              // Free previously deep-copied memory at this slot to avoid leak when overwriting
+              OZ (pl::ObUserDefinedType::destruct_objparam(*coll->get_allocator(),
+                                                           static_cast<ObObj&>(static_cast<ObObj*>(assoc_array->get_data())[index]),
+                                                           nullptr));
+              OZ (deep_copy_obj(*coll->get_allocator(),
+                                v,
+                                static_cast<ObObj*>(assoc_array->get_data())[index]));
+            } else {
+              OZ((ObSPIService::spi_extend_assoc_array(session->get_effective_tenant_id(),
+                                                       session->get_pl_context()->get_current_ctx(),
+                                                       *alloc,
+                                                       *assoc_array,
+                                                       1)));
+              OZ(assoc_array->insert_sort(key, assoc_array->get_count() - 1, search_end, assoc_array->get_count() - 1));
+              OZ(assoc_array->update_first_last(OB_INVALID_INDEX == search_end ? 0 : search_end));
 
-            CK (OB_NOT_NULL(assoc_array->get_key()));
-            OZ (deep_copy_obj(*coll->get_allocator(),
-                              key,
-                              static_cast<ObObj*>(assoc_array->get_key())[i / 2]));
+              OZ (ObSPIService::spi_pad_char_or_varchar(session,
+                                                        info->index_type_.get_obj_type(),
+                                                        info->index_type_.get_accuracy(),
+                                                        alloc,
+                                                        &key));
+              CK (OB_NOT_NULL(assoc_array->get_key()));
+              OZ (deep_copy_obj(*coll->get_allocator(),
+                                key,
+                                static_cast<ObObj*>(assoc_array->get_key())[i / 2]));
 
-            OZ(d_for_data.to_obj(v, expr.args_[i + 1]->obj_meta_, expr.args_[i + 1]->obj_datum_map_));
-            if (OB_SUCC(ret) && !v.is_null()) {
-              TYPE_CHECK(v, info->elem_type_.get_meta_type().get_type());
+              OZ(d_for_data.to_obj(v, expr.args_[i + 1]->obj_meta_, expr.args_[i + 1]->obj_datum_map_));
+              if (OB_SUCC(ret) && !v.is_null()) {
+                TYPE_CHECK(v, info->elem_type_.get_meta_type().get_type());
+              }
+              OZ (ObSPIService::spi_pad_char_or_varchar(session,
+                                                        info->elem_type_.get_obj_type(),
+                                                        info->elem_type_.get_accuracy(),
+                                                        alloc,
+                                                        &v));
+              CK (OB_NOT_NULL(assoc_array->get_data()));
+              OZ (deep_copy_obj(*coll->get_allocator(),
+                                v,
+                                static_cast<ObObj*>(assoc_array->get_data())[i / 2]));
             }
-            OZ (ObSPIService::spi_pad_char_or_varchar(session,
-                                                      info->elem_type_.get_obj_type(),
-                                                      info->elem_type_.get_accuracy(),
-                                                      alloc,
-                                                      &v));
-            CK (OB_NOT_NULL(assoc_array->get_data()));
-            OZ (deep_copy_obj(*coll->get_allocator(),
-                              v,
-                              static_cast<ObObj*>(assoc_array->get_data())[i / 2]));
           }
         } else {
           for (int64_t i = 0; OB_SUCC(ret) && i < expr.arg_cnt_; ++i) {
