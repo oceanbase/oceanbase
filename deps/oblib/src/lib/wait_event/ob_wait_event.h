196a29d3af2374d2ffef460683869e245b9ef97d
godyangfight <godyangfight@gmail.com>
2025-10-14 03:49:47 +0000

    master add some values for placeholder

--- deps/oblib/src/lib/wait_event/ob_wait_event.h
+++ deps/oblib/src/lib/wait_event/ob_wait_event.h
@@ -153,6 +153,7 @@ WAIT_EVENT_DEF(MYSQL_CONN_GET_LOCK, 18115, "mysql conn get lock", "", "", "", CO
 WAIT_EVENT_DEF(MYSQL_CONN_DBLINK_POOL_LOCK, 18116, "mysql conn dblink pool lock", "", "", "", CONCURRENCY, true, true)
 WAIT_EVENT_DEF(BACKUP_VALIDATE_SERVICE_COND_WAIT, 18117, "backup validate service condition wait", "address", "", "", CONCURRENCY, true, true)
 WAIT_EVENT_DEF(HMS_CLIENT_POOL_COND_WAIT, 18118, "hms client pool condition wait", "address", "", "", CONCURRENCY, true, true)
+WAIT_EVENT_DEF(HA_CREATE_TABLET_COND_WAIT, 18119, "ha create tablet condition wait", "address", "", "", CONCURRENCY, false, true)
 
 //sleep
 WAIT_EVENT_DEF(BANDWIDTH_THROTTLE_SLEEP, 20000, "sleep: bandwidth throttle sleep wait", "sleep_interval", "", "", NETWORK, true, true)
