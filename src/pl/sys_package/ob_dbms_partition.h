140d1dc990d3377d125293606af1630f84149830
obdev <obdev@oceanbase.com>
2025-10-31 10:19:16 +0000

    [CP] fix: use ObPLExecCtx in dbms partition sys package

--- src/pl/sys_package/ob_dbms_partition.h
+++ src/pl/sys_package/ob_dbms_partition.h
@@ -12,16 +12,18 @@
 
 #ifndef OCEANBASE_SRC_PL_SYS_PACKAGE_DBMS_PARTITION_H_
 #define OCEANBASE_SRC_PL_SYS_PACKAGE_DBMS_PARTITION_H_
-#include "sql/engine/ob_exec_context.h"
+
+#include "sql/ob_sql_define.h"
 
 namespace oceanbase
 {
 namespace pl
 {
+class ObPLExecCtx;
 class ObDBMSPartition
 {
 public:
-  static int manage_dynamic_partition(sql::ObExecContext &ctx, sql::ParamStore &params, common::ObObj &result);
+  static int manage_dynamic_partition(ObPLExecCtx &ctx, sql::ParamStore &params, common::ObObj &result);
 private:
   static int get_and_check_params_(const sql::ParamStore &params, common::ObString &precreate_time_str, common::ObArray<common::ObString> &time_unit_strs);
 };
