3fece76d71f40c24903e498f776d424a08cf7841
obdev <obdev@oceanbase.com>
2025-11-10 07:47:36 +0000

    [CP] 修复日志流副本迁出再迁入同节点有概率失败的问题

--- src/logservice/ob_log_service.cpp
+++ src/logservice/ob_log_service.cpp
@@ -472,7 +472,7 @@ int ObLogService::remove_ls(const ObLSID &id,
     // In abnormal case(create ls failed, need remove ls directlly), there is no possibility for dead lock.
     log_handler.stop();
     restore_handler.stop();
-    if (!enable_logservice_ && OB_FAIL(palf_env_->remove(id.id()))) {
+    if (OB_FAIL(palf_env_->remove(id.id()))) {
       CLOG_LOG(WARN, "failed to remove from palf_env_", K(ret), K(id));
     } else {
       FLOG_INFO("ObLogService remove_ls success", K(ret), K(id));
