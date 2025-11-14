00d22b32d49baf9089da0145ef2a55f099d30436
hy-guo <fqboyg@gmail.com>
2025-11-14 10:49:53 +0000

    [CP] allocate limit 1 for const group by with empty aggr item

--- src/sql/resolver/dml/ob_hint.h
+++ src/sql/resolver/dml/ob_hint.h
@@ -390,8 +390,10 @@ struct ObGlobalHint {
 #define COMPAT_VERSION_4_3_5_BP2  (oceanbase::common::cal_version(4, 3, 5, 2))
 #define COMPAT_VERSION_4_3_5_BP3  (oceanbase::common::cal_version(4, 3, 5, 3))
 #define COMPAT_VERSION_4_3_5_BP4  (oceanbase::common::cal_version(4, 3, 5, 4))
+#define COMPAT_VERSION_4_3_5_BP5  (oceanbase::common::cal_version(4, 3, 5, 5))
 #define COMPAT_VERSION_4_4_0      (oceanbase::common::cal_version(4, 4, 0, 0))
 #define COMPAT_VERSION_4_4_1      (oceanbase::common::cal_version(4, 4, 1, 0))
+#define COMPAT_VERSION_4_4_2      (oceanbase::common::cal_version(4, 4, 2, 0))
 #define COMPAT_VERSION_4_5_0      (oceanbase::common::cal_version(4, 5, 0, 0))
 #define LASTED_COMPAT_VERSION     COMPAT_VERSION_4_5_0
   static bool is_valid_opt_features_version(uint64_t version)
