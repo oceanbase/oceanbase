5045b71287f4f6e785865ad87c0a2c09476394e8
Cai-Yao <729673078@qq.com>
2025-11-17 07:48:33 +0000

    master placeholder for CK aggr func

--- src/objit/include/objit/common/ob_item_type.h
+++ src/objit/include/objit/common/ob_item_type.h
@@ -975,6 +975,10 @@ typedef enum ObItemType
   T_FUN_SYS_AI_PROMPT = 1929,
   T_FUN_SYS_VEC_VISIBLE = 1930, // vector index table 5
   T_FUN_SYS_MAX_PT = 1931,
+  T_FUN_GROUPCONCAT = 1932,
+  T_FUN_VARSAMP = 1933,
+  T_FUN_STDDEVSAMP = 1934,
+  T_FUN_UNIQ = 1935,
 
   ///< @note add new sys function type before this line
   T_FUN_SYS_END = 2000,
@@ -3147,7 +3151,8 @@ extern const char *get_type_name(int type);
                          ((op) >= T_FUN_SYS_BIT_AND && (op) <= T_FUN_SYS_BIT_XOR) || \
                          (op) == T_FUN_INNER_PREFIX_MAX || \
                          (op) == T_FUN_INNER_PREFIX_MIN || \
-                         ((op) >= T_FUN_ARG_MIN && (op) <= T_FUN_ARG_MAX))
+                         ((op) >= T_FUN_ARG_MIN && (op) <= T_FUN_ARG_MAX) || \
+                         ((op) >= T_FUN_GROUPCONCAT) && ((op) <= T_FUN_UNIQ))
 #define MAYBE_ROW_OP(op) ((op) >= T_OP_EQ && (op) <= T_OP_NE)
 #define IS_PSEUDO_COLUMN_TYPE(op) \
   ((op) == T_LEVEL || (op) == T_CONNECT_BY_ISLEAF || (op) == T_CONNECT_BY_ISCYCLE || (op) == T_ORA_ROWSCN)
