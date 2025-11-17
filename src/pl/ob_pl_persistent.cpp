c3e926408fb327b5b610282c1ebbbc49575c8770
bit-dance <2634358021@qq.com>
2025-11-17 11:50:12 +0000

    [CP] [#2025101400111403985]Fix profiler use wrong key_id as unit_id for type and trigger.

--- src/pl/ob_pl_persistent.cpp
+++ src/pl/ob_pl_persistent.cpp
@@ -15,6 +15,7 @@
 #include "ob_pl_code_generator.h"
 #include "ob_pl_compile.h"
 #include "share/ob_version.h"
+#include "ob_pl_package.h"
 
 namespace oceanbase
 {
@@ -286,9 +287,15 @@ int ObRoutinePersistentInfo::decode_dll(ObSQLSessionInfo &session_info,
                   OX (routine->set_ret_type(routine_ast->get_ret_type()));
                   OZ (routine->set_types(routine_ast->get_user_type_table()));
                   if (OB_SUCC(ret) && cg.get_profile_mode()) {
-                    OX (routine->set_profiler_unit_info(key_id_, routine->get_proc_type()));
+                    uint64_t object_id = key_id_;
+                    if (share::schema::ObUDTObjectType::is_object_id(key_id_)) {
+                      object_id = static_cast<const ObPLPackage&>(unit).get_id();
+                    } else if (share::schema::ObTriggerInfo::is_trigger_body_package_id(key_id_)) {
+                      object_id = share::schema::ObTriggerInfo::get_package_trigger_id(key_id_);
+                    }
+                    OX (routine->set_profiler_unit_info(object_id, routine->get_proc_type()));
                     OZ (SMART_CALL(
-                          ObPLCodeGenerator::set_profiler_unit_info_recursive(*routine)));
+                      ObPLCodeGenerator::set_profiler_unit_info_recursive(*routine)));
                   }
                 } else {
                   // simple routine(generate by generate_simpile interface), skip encode header byte
