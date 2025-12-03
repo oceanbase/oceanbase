489e47c4c78cd9150a496bb48d4b17249281a638
obdev <obdev@oceanbase.com>
2025-12-03 07:21:19 +0000

    fix prefix_len

--- src/sql/resolver/ddl/ob_create_table_resolver.cpp
+++ src/sql/resolver/ddl/ob_create_table_resolver.cpp
@@ -3001,22 +3001,27 @@ int ObCreateTableResolver::resolve_index_node(const ParseNode *node)
                                                                           reinterpret_cast<int*>(&index_keyname_)))) {
                   LOG_WARN("failed to resolve index type by parse node", K(ret));
                 }
+              }
 #ifdef OB_BUILD_SHARED_STORAGE
-              } else if (GCTX.is_shared_storage_mode()
-                         && (MULTI_KEY == index_keyname_ || MULTI_UNIQUE_KEY == index_keyname_)
-                         && tenant_data_version < DATA_VERSION_4_3_5_2) {
-                  ret = OB_NOT_SUPPORTED;
-                  LOG_WARN("multivalue search index isn't supported in shared storage mode", K(ret));
-                  LOG_USER_ERROR(OB_NOT_SUPPORTED, "multivalue search index in shared storage mode is");
+              if (OB_SUCC(ret) && GCTX.is_shared_storage_mode()
+                  && (MULTI_KEY == index_keyname_ || MULTI_UNIQUE_KEY == index_keyname_)
+                  && tenant_data_version < DATA_VERSION_4_3_5_2) {
+                ret = OB_NOT_SUPPORTED;
+                LOG_WARN("multivalue search index isn't supported in shared storage mode", K(ret));
+                LOG_USER_ERROR(OB_NOT_SUPPORTED, "multivalue search index in shared storage mode is");
+              }
 #endif
-              } else if (NULL != index_column_node->children_[1]) {
-                sort_item.prefix_len_ = static_cast<int32_t>(index_column_node->children_[1]->value_);
-                if (0 == sort_item.prefix_len_) {
-                  ret = OB_KEY_PART_0;
-                  LOG_USER_ERROR(OB_KEY_PART_0, column_name.length(), column_name.ptr());
+              // set prefix_len for all index types
+              if (OB_SUCC(ret)) {
+                if (NULL != index_column_node->children_[1]) {
+                  sort_item.prefix_len_ = static_cast<int32_t>(index_column_node->children_[1]->value_);
+                  if (0 == sort_item.prefix_len_) {
+                    ret = OB_KEY_PART_0;
+                    LOG_USER_ERROR(OB_KEY_PART_0, column_name.length(), column_name.ptr());
+                  }
+                } else {
+                  sort_item.prefix_len_ = 0;
                 }
-              } else {
-                sort_item.prefix_len_ = 0;
               }
             }
           }
