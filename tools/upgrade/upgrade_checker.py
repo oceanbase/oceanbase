ccd14a87f7bb2c6d10a0cf08262d6740858ecdf6
obdev <obdev@oceanbase.com>
2025-12-03 12:18:05 +0000

    支持新的升级路径，调整upgrade checker的版本检查以及yaml文件的升级路径

--- tools/upgrade/upgrade_checker.py
+++ tools/upgrade/upgrade_checker.py
@@ -18,7 +18,7 @@ if sys.version_info.major == 3:
 
 class UpgradeParams:
   log_filename = 'upgrade_checker.log'
-  old_version = '4.2.5.1'
+  old_version = '4.2.5.7'
 
 class PasswordMaskingFormatter(logging.Formatter):
   def format(self, record):
