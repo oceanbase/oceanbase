22fded8f92604a4232434179e6bfd1bc11bb42d9
hanr881 <1741282579@qq.com>
2025-12-08 03:17:18 +0000

    to issue<2025101700111476108>: fix alter routine compile debug issue

--- src/sql/session/ob_basic_session_info.cpp
+++ src/sql/session/ob_basic_session_info.cpp
@@ -7083,12 +7083,13 @@ void ObExecEnv::reset()
 
 bool ObExecEnv::operator==(const ObExecEnv &other) const
 {
-  return sql_mode_ == other.sql_mode_
-      && charset_client_ == other.charset_client_
-      && collation_connection_ == other.collation_connection_
-      && collation_database_ == other.collation_database_
-      && plsql_ccflags_ == other.plsql_ccflags_
-      && plsql_optimize_level_ == other.plsql_optimize_level_;
+  bool is_mysql_mode = lib::is_mysql_mode();
+  return (is_mysql_mode ? (sql_mode_ == other.sql_mode_) : true)
+      && (charset_client_ == other.charset_client_)
+      && (collation_connection_ == other.collation_connection_)
+      && (collation_database_ == other.collation_database_)
+      && (is_mysql_mode ? true : (plsql_ccflags_ == other.plsql_ccflags_))
+      && (plsql_optimize_level_ == other.plsql_optimize_level_);
 }
 
 bool ObExecEnv::operator!=(const ObExecEnv &other) const
