ac000488288cabb976bdf19efcfd0eac64ccfdfc
Tsunaou <895254752@qq.com>
2025-12-08 09:49:46 +0000

    [CP] fix alter table mode override other table_mode flags

--- src/rootserver/ob_ddl_operator.cpp
+++ src/rootserver/ob_ddl_operator.cpp
@@ -4529,8 +4529,8 @@ int ObDDLOperator::update_aux_table(
           new_aux_table_schema.set_store_format(new_table_schema.get_store_format());
           new_aux_table_schema.set_progressive_merge_round(new_table_schema.get_progressive_merge_round());
           new_aux_table_schema.set_storage_format_version(new_table_schema.get_storage_format_version());
-          // index table should only inherit table mode and table state flag from data table
-          new_aux_table_schema.set_table_mode(new_table_schema.get_table_mode_flag());
+          // index table should only inherit table mode flag and table state flag from data table
+          new_aux_table_schema.set_table_mode_flag(new_table_schema.get_table_mode_flag());
           new_aux_table_schema.set_table_state_flag(new_table_schema.get_table_state_flag());
           new_aux_table_schema.set_duplicate_attribute(new_table_schema.get_duplicate_scope(), new_table_schema.get_duplicate_read_consistency());
           new_aux_table_schema.set_enable_macro_block_bloom_filter(new_table_schema.get_enable_macro_block_bloom_filter());
--- src/sql/resolver/ddl/ob_ddl_resolver.cpp
+++ src/sql/resolver/ddl/ob_ddl_resolver.cpp
@@ -1948,11 +1948,6 @@ int ObDDLResolver::resolve_table_option(const ParseNode *option_node, const bool
               ret = OB_NOT_SUPPORTED;
               LOG_USER_ERROR(OB_NOT_SUPPORTED, "set vertical partition table as queuing table mode");
               SQL_RESV_LOG(WARN, "Vertical partition table cannot set queuing table mode", K(ret));
-            } else { // 暂不支持用户在alter table时变更PK_MODE
-              // 设置Table当前的PK_MODE，组装最终态TableMode
-              table_mode_.pk_mode_ = tbl_schema->get_table_mode_struct().pk_mode_;
-              table_mode_.pk_exists_ = tbl_schema->get_table_mode_struct().pk_exists_;
-              table_mode_.table_organization_mode_ = tbl_schema->get_table_mode_struct().table_organization_mode_;
             }
           }
         }
