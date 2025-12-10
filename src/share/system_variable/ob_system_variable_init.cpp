cb0953849717cf8ef08e2be6be17dca38d3046fc
obdev <obdev@oceanbase.com>
2025-12-10 05:20:00 +0000

    [CP] ef_search上限调整至160000，适配vsag

--- src/share/system_variable/ob_system_variable_init.cpp
+++ src/share/system_variable/ob_system_variable_init.cpp
@@ -7035,7 +7035,7 @@ static struct VarsInit{
       ObSysVars[503].name_ = "ob_hnsw_ef_search" ;
       ObSysVars[503].data_type_ = ObUInt64Type ;
       ObSysVars[503].min_val_ = "1" ;
-      ObSysVars[503].max_val_ = "1000" ;
+      ObSysVars[503].max_val_ = "160000" ;
       ObSysVars[503].flags_ = ObSysVarFlag::SESSION_SCOPE ;
       ObSysVars[503].id_ = SYS_VAR_OB_HNSW_EF_SEARCH ;
       cur_max_var_id = MAX(cur_max_var_id, static_cast<int64_t>(SYS_VAR_OB_HNSW_EF_SEARCH)) ;
--- src/share/vector_index/ob_plugin_vector_index_adaptor.h
+++ src/share/vector_index/ob_plugin_vector_index_adaptor.h
@@ -977,7 +977,7 @@ private:
   constexpr static uint32_t VEC_INDEX_SNAP_DATA_SYNC_THRESHOLD = 1;
   constexpr static uint32_t VEC_INDEX_ADAPTER_MAX_IDLE_COUNT = 3;
   constexpr static uint32_t VEC_INDEX_HNSWSQ_BUILD_COUNT_THRESHOLD = 10000;
-  constexpr static int64_t  VSAG_MAX_EF_SEARCH = 1000;
+  constexpr static int64_t  VSAG_MAX_EF_SEARCH = 160000;
 };
 
 class ObPluginVectorIndexAdapterGuard
