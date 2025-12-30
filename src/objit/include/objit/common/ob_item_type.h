c18905ae73c19454c69d00c5490e523ccfd75822
ZoruaSama <zoruasama@qq.com>
2025-12-30 03:50:27 +0000

    增加parseDateTime函数的master占位

--- src/objit/include/objit/common/ob_item_type.h
+++ src/objit/include/objit/common/ob_item_type.h
@@ -1079,6 +1079,7 @@ typedef enum ObItemType
   T_FUN_SYS_TO_UNIX_TIMESTAMP = 2095,
   T_FUN_SYS_VOID = 2096,
   T_FUN_COLLECT_FILE_LIST = 2097,
+  T_FUN_SYS_PARSE_DATE_TIME = 2098,
   T_MAX_OP = 3000,
 
   //pseudo column, to mark the group iterator id
