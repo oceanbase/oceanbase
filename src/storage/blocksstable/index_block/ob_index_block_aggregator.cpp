44fe3bfe529c12fa6fa6f630029733af16accf18
zs8730011 <470860127@qq.com>
2026-01-05 16:37:39 +0000

    [CP] fix bm25 max score param skip index on all nop data
    Co-authored-by: saltonz <saltonzh@gmail.com>

--- src/storage/blocksstable/index_block/ob_index_block_aggregator.cpp
+++ src/storage/blocksstable/index_block/ob_index_block_aggregator.cpp
@@ -932,9 +932,9 @@ void ObIMultiColAggregator::reuse()
   if (agg_result_row_ != nullptr) {
     for (int64_t i = 0; i < result_idxes_.count(); ++i) {
       const int64_t result_idx = result_idxes_.at(i);
-      if (OB_LIKELY(result_idx > 0 && result_idx < agg_result_row_->get_agg_col_cnt())) {
-        agg_result_row_->get_agg_datum_row().storage_datums_[i].reuse();
-        agg_result_row_->get_agg_datum_row().storage_datums_[i].set_null();
+      if (OB_LIKELY(result_idx >= 0 && result_idx < agg_result_row_->get_agg_col_cnt())) {
+        agg_result_row_->get_agg_datum_row().storage_datums_[result_idx].reuse();
+        agg_result_row_->get_agg_datum_row().storage_datums_[result_idx].set_null();
       }
     }
   }
@@ -979,7 +979,7 @@ int ObBM25ParamAggregator::init(
         result_idxes_.at(TOKEN_FREQ_IDX) = i;
         col_agg_metas_.at(TOKEN_FREQ_IDX) = idx_col_meta;
         agg_result_row.get_agg_datum_row().storage_datums_[i].reuse();
-        agg_result_row.get_agg_datum_row().storage_datums_[i].set_null();
+        agg_result_row.get_agg_datum_row().storage_datums_[i].set_uint(0);
       }
     } else if (idx_col_meta.get_col_type() == SK_IDX_BM25_MAX_SCORE_DOC_LEN) {
       if (OB_UNLIKELY(found_doc_length)) {
@@ -994,7 +994,7 @@ int ObBM25ParamAggregator::init(
         result_idxes_.at(DOC_LENGTH_IDX) = i;
         col_agg_metas_.at(DOC_LENGTH_IDX) = idx_col_meta;
         agg_result_row.get_agg_datum_row().storage_datums_[i].reuse();
-        agg_result_row.get_agg_datum_row().storage_datums_[i].set_null();
+        agg_result_row.get_agg_datum_row().storage_datums_[i].set_uint(0);
       }
     }
   }
@@ -1025,6 +1025,16 @@ void ObBM25ParamAggregator::reset()
 void ObBM25ParamAggregator::reuse()
 {
   ObIMultiColAggregator::reuse();
+  if (nullptr != agg_result_row_ && result_idxes_.count() >= 2) {
+    const int64_t token_freq_result_idx = result_idxes_.at(TOKEN_FREQ_IDX);
+    const int64_t doc_length_result_idx = result_idxes_.at(DOC_LENGTH_IDX);
+    if (OB_LIKELY(token_freq_result_idx >= 0 && token_freq_result_idx < agg_result_row_->get_agg_col_cnt())) {
+      agg_result_row_->get_agg_datum_row().storage_datums_[token_freq_result_idx].set_uint(0);
+    }
+    if (OB_LIKELY(doc_length_result_idx >= 0 && doc_length_result_idx < agg_result_row_->get_agg_col_cnt())) {
+      agg_result_row_->get_agg_datum_row().storage_datums_[doc_length_result_idx].set_uint(0);
+    }
+  }
   curr_max_score_ = 0.0;
 }
 
