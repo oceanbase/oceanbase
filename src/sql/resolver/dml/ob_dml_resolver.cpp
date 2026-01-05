c2062ef775ee49f2bfc66f4995ee26766461c6f2
yinyj17 <yinyijun92@gmail.com>
2026-01-05 16:39:35 +0000

    [CP] support disable shared expr extraction by hint

--- src/sql/resolver/dml/ob_dml_resolver.cpp
+++ src/sql/resolver/dml/ob_dml_resolver.cpp
@@ -1827,6 +1827,7 @@ int ObDMLResolver::resolve_sql_expr(const ParseNode &node, ObRawExpr *&expr,
       }
     }
     if (OB_SUCC(ret) &&
+        !params_.disable_shared_expr_ &&
         current_scope_ != T_INSERT_SCOPE &&
         current_scope_ != T_UPDATE_SCOPE &&
         !is_hierarchical_query &&
