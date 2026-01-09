a631839ad234f1e9fc6b076e427ae05c4338575d
zs8730011 <470860127@qq.com>
2026-01-09 03:20:36 +0000

    fix: null pointer crash in ObSSTableIndexScanner and wrong min-finding logic in ObSRBlockMaxTopKIterImpl

--- src/storage/retrieval/ob_sparse_daat_iter.cpp
+++ src/storage/retrieval/ob_sparse_daat_iter.cpp
@@ -759,7 +759,7 @@ int ObSRBlockMaxTopKIterImpl::next_pivot_range(const double &score_threshold, in
       minimum_max_domain_id = max_score_tuple->max_domain_id_;
     } else if (OB_FAIL(domain_id_cmp_.compare(*max_score_tuple->max_domain_id_, *minimum_max_domain_id, cmp_ret))) {
       LOG_WARN("failed to compare domain id", K(ret));
-    } else if (0 < cmp_ret) {
+    } else if (0 > cmp_ret) {
       minimum_max_domain_id = max_score_tuple->max_domain_id_;
     }
   }
@@ -932,7 +932,7 @@ int ObSRBlockMaxTopKIterImpl::try_generate_next_range_from_merge_heap(
         curr_range_reach_end = true;
       } else if (OB_FAIL(domain_id_cmp_.compare(*max_score_tuple->max_domain_id_, *minimum_max_domain_id, cmp_ret))) {
         LOG_WARN("failed to compare max domain id with minimum max domain id", K(ret));
-      } else if (0 < cmp_ret) {
+      } else if (0 > cmp_ret) {
         minimum_max_domain_id = max_score_tuple->max_domain_id_;
       }
 
