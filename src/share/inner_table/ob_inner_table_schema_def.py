78dd3d94956cd01476c7ec1b6a27c31c228be53b
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2026-01-27 10:54:49 +0000

    [SCHEMA_RECYCLE] add inner table placeholder

--- src/share/inner_table/ob_inner_table_schema_def.py
+++ src/share/inner_table/ob_inner_table_schema_def.py
@@ -8706,6 +8706,7 @@ def_table_schema(
 # 582: __all_sync_standby_status
 # 583: __all_routine_load_job
 # 584: __all_ss_gc_reserved_snapshot
+# 585: __all_table_archive_history
 
 # 余留位置（此行之前占位）
 # 本区域占位建议：采用真实表名进行占位
