eac53eb1baf720f44fab0ec08d32460d11b1b7f3
obdev <obdev@oceanbase.com>
2026-01-28 11:48:05 +0000

    [CP] flatten cnnop in parser

--- src/sql/engine/expr/ob_expr_concat.cpp
+++ src/sql/engine/expr/ob_expr_concat.cpp
@@ -196,8 +196,7 @@ int ObExprConcat::calc_result_typeN(ObExprResType &type,
                                     ObExprTypeCtx &type_ctx) const
 {
   int ret = OB_SUCCESS;
-  if (OB_UNLIKELY(param_num <= 0
-                  || (lib::is_oracle_mode() && param_num != 2))) {
+  if (OB_UNLIKELY(param_num <= 0)) {
     ret = OB_INVALID_ARGUMENT_NUM;
     LOG_WARN("invalid argument number", K(ret), K(param_num));
   }
@@ -206,7 +205,7 @@ int ObExprConcat::calc_result_typeN(ObExprResType &type,
 
   //类型 + 字符集推导
   if (lib::is_oracle_mode()) {
-    ObSEArray<ObExprResType*, 2, ObNullAllocator> params;
+    ObSEArray<ObExprResType*, 2> params;
     for (int64_t i = 0; i < param_num; ++i) {
       OZ (params.push_back(&types[i]));
     }
@@ -238,7 +237,11 @@ int ObExprConcat::calc_result_typeN(ObExprResType &type,
   //结果的长度推导
   if (OB_SUCC(ret)) {
     if (lib::is_oracle_mode()) {
-      type.set_length(types[0].get_calc_length() + types[1].get_calc_length());
+      ObLength max_len = 0;
+      for (int64_t i = 0; i < param_num; ++i) {
+        max_len += types[i].get_calc_length();
+      }
+      type.set_length(max_len);    
     } else {
       ObLength max_len = 0;
       for (int64_t i = 0; i < param_num; ++i) {
@@ -350,9 +353,6 @@ int ObExprConcat::cg_expr(ObExprCGCtx &, const ObRawExpr &, ObExpr &expr) const
 {
   int ret = OB_SUCCESS;
   CK(expr.arg_cnt_ > 0);
-  if (lib::is_oracle_mode()) {
-    CK(2 == expr.arg_cnt_);
-  }
   if (OB_SUCC(ret)) {
     expr.eval_func_ = &eval_concat;
   }
