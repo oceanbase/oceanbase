b4ddedae2a635277e80c3718c3b3a630eff32e9b
callmezhuoran <callmezhuo@foxmail.com>
2026-01-29 09:50:38 +0000

    fix bug, dump ddl failed when no_logging

--- src/storage/ddl/ob_tablet_ddl_kv.cpp
+++ src/storage/ddl/ob_tablet_ddl_kv.cpp
@@ -1252,10 +1252,8 @@ int ObDDLKV::set_macro_block(
         }
       }
       if (OB_FAIL(ret)) {
-      } else if (DDL_MB_SS_EMPTY_DATA_TYPE == macro_block.block_type_) {
-        /* skip, emtpy type do not have data macro*/
       } else if (OB_FAIL(ddl_memtable->insert_block_meta_tree(macro_block.block_handle_, data_macro_meta, macro_block.end_row_id_))) {
-        LOG_WARN("insert block meta tree faield", K(ret));
+        LOG_WARN("insert block meta tree failed", K(ret));
       } else {
         // FIXME : @suzhi.yt support incremental direct load in ss mode
         if (is_inc_minor_ddl_kv()) {
