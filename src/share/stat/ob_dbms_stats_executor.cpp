99fb6bf95d11c7489f11d0872dfa27cf65f23ce9
ChangerR <dingjinchengyx@163.com>
2026-02-11 11:47:15 +0000

    [CP] fix gather online stat cause -6005 conflict

--- src/share/stat/ob_dbms_stats_executor.cpp
+++ src/share/stat/ob_dbms_stats_executor.cpp
@@ -837,6 +837,7 @@ int ObDbmsStatsExecutor::do_gather_stats(ObExecContext &ctx,
   int ret = OB_SUCCESS;
   ObSEArray<ObOptTableStat *, 4> tmp_all_tstats;
   int64_t start_time = 0;
+  sqlclient::ObISQLConnection *conn = trans.get_connection();
   if (OB_FAIL(THIS_WORKER.check_status())) {
     LOG_WARN("check status failed", KR(ret));
   } else if (OB_FAIL(param.partition_infos_.assign(gather_partition_infos))) {
@@ -855,7 +856,7 @@ int ObDbmsStatsExecutor::do_gather_stats(ObExecContext &ctx,
     // avoid memory use too much, write current gather stats.
     LOG_WARN("failed to merge split gather tab stats", K(ret));
   } else if (OB_FALSE_IT(start_time = ObTimeUtility::current_time())) {
-  } else if (OB_FAIL(ObDbmsStatsUtils::split_batch_write(
+  } else if (OB_FAIL(ObDbmsStatsUtils::split_batch_write_with_trx_lock_timeout(
                  ctx, trans.get_connection(), is_all_columns_gather ? all_tstats : tmp_all_tstats, all_cstats))) {
     LOG_WARN("failed to split batch write", K(ret));
   } else if (OB_FAIL(audit.add_flush_stats_audit(ObTimeUtility::current_time() - start_time))) {
@@ -1580,8 +1581,6 @@ int ObDbmsStatsExecutor::update_online_stat(ObExecContext &ctx,
         LOG_WARN("failed to scale col stats", K(ret));
       } else if (OB_FAIL(ObDbmsStatsUtils::split_batch_write(ctx, conn, table_stats, column_stats, false, true))) {
         LOG_WARN("fail to update stat", K(ret), K(table_stats), K(column_stats));
-      } else if (OB_FAIL(ObBasicStatsEstimator::update_last_modified_count(conn, param))) {
-        LOG_WARN("failed to update last modified count", K(ret));
       } else {
         succ_to_write_stats = true;
       }
