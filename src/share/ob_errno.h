1ce931bca541f38995ec6f47280dc9f1b9532110
ZoruaSama <zoruasama@qq.com>
2026-02-11 12:21:57 +0000

    新增OB_NON_RO_SELECT_DISABLE_TIMER错误码

--- src/share/ob_errno.h
+++ src/share/ob_errno.h
@@ -273,6 +273,9 @@ constexpr int OB_MIGRATE_TX_DATA_NOT_CONTINUES = -4408;
 constexpr int OB_INVALID_ZERO_DATE = -4409;
 constexpr int OB_LS_NOT_IN_LEARNER_LIST = -4410;
 constexpr int OB_MEMORY_LEAK = -4411;
+constexpr int OB_OUT_OF_MAX_EXECUTION_TIME = -4412;
+constexpr int OB_ERR_MAX_EXECUTION_TIME_TRUNCATED = -4413;
+constexpr int OB_NON_RO_SELECT_DISABLE_TIMER = -4414;
 constexpr int OB_IMPORT_NOT_IN_SERVER = -4505;
 constexpr int OB_CONVERT_ERROR = -4507;
 constexpr int OB_BYPASS_TIMEOUT = -4510;
@@ -2455,6 +2458,9 @@ constexpr int OB_ERR_DATA_TOO_LONG_MSG_FMT_V2 = -5167;
 #define OB_INVALID_ZERO_DATE__USER_ERROR_MSG "invalid zero date in no_zero_date mode"
 #define OB_LS_NOT_IN_LEARNER_LIST__USER_ERROR_MSG "ls not in learner list"
 #define OB_MEMORY_LEAK__USER_ERROR_MSG "observer has memory leak"
+#define OB_OUT_OF_MAX_EXECUTION_TIME__USER_ERROR_MSG "Query execution was interrupted, maximum statement execution time exceeded. maybe you can adjust the session variable max_execution_time or max_execution_time hint, and try again"
+#define OB_ERR_MAX_EXECUTION_TIME_TRUNCATED__USER_ERROR_MSG "Max execution time is truncated to the range [0, 4294967295]"
+#define OB_NON_RO_SELECT_DISABLE_TIMER__USER_ERROR_MSG "Select is not a read only statement, disabling timer"
 #define OB_IMPORT_NOT_IN_SERVER__USER_ERROR_MSG "Import not in service"
 #define OB_CONVERT_ERROR__USER_ERROR_MSG "Convert error"
 #define OB_BYPASS_TIMEOUT__USER_ERROR_MSG "Bypass timeout"
@@ -5350,6 +5356,12 @@ constexpr int OB_ERR_DATA_TOO_LONG_MSG_FMT_V2 = -5167;
 #define OB_LS_NOT_IN_LEARNER_LIST__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -4410, ls not in learner list"
 #define OB_MEMORY_LEAK__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -4411, observer has memory leak"
 #define OB_MEMORY_LEAK__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -4411, observer has memory leak"
+#define OB_OUT_OF_MAX_EXECUTION_TIME__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -4412, Query execution was interrupted, maximum statement execution time exceeded. maybe you can adjust the session variable max_execution_time or max_execution_time hint, and try again"
+#define OB_OUT_OF_MAX_EXECUTION_TIME__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -4412, Query execution was interrupted, maximum statement execution time exceeded. maybe you can adjust the session variable max_execution_time or max_execution_time hint, and try again"
+#define OB_ERR_MAX_EXECUTION_TIME_TRUNCATED__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -4413, Max execution time is truncated to the range [0, 4294967295]"
+#define OB_ERR_MAX_EXECUTION_TIME_TRUNCATED__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -4413, Max execution time is truncated to the range [0, 4294967295]"
+#define OB_NON_RO_SELECT_DISABLE_TIMER__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -4414, Select is not a read only statement, disabling timer"
+#define OB_NON_RO_SELECT_DISABLE_TIMER__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -4414, Select is not a read only statement, disabling timer"
 #define OB_IMPORT_NOT_IN_SERVER__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -4505, Import not in service"
 #define OB_IMPORT_NOT_IN_SERVER__OBE_USER_ERROR_MSG "OBE-00600: internal error code, arguments: -4505, Import not in service"
 #define OB_CONVERT_ERROR__ORA_USER_ERROR_MSG "ORA-00600: internal error code, arguments: -4507, Convert error"
@@ -9649,7 +9661,7 @@ constexpr int OB_ERR_DATA_TOO_LONG_MSG_FMT_V2 = -5167;
 #define OB_ERR_DATA_TOO_LONG_MSG_FMT_V2__ORA_USER_ERROR_MSG "ORA-12899: value too large for column %.*s (actual: %ld, maximum: %ld)"
 #define OB_ERR_DATA_TOO_LONG_MSG_FMT_V2__OBE_USER_ERROR_MSG "OBE-12899: value too large for column %.*s (actual: %ld, maximum: %ld)"
 
-extern int g_all_ob_errnos[2519];
+extern int g_all_ob_errnos[2522];
 
   const char *ob_error_name(const int oberr);
   const char* ob_error_cause(const int oberr);
