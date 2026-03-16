4b88407b742bd66b873daca44d93a416ccb86ace
liucc1997 <1192520566@qq.com>
2026-03-16 04:44:08 +0000

    [CP] [OMT] add async /proc cpu sampler for high tenant count scenario

--- src/share/ob_thread_define.h
+++ src/share/ob_thread_define.h
@@ -215,4 +215,5 @@ TG_DEF(TmpFileFlush, TFFlush, TIMER)
 TG_DEF(LogFetcherBGWorker, LogFetcherBGW, TIMER)
 TG_DEF(MaBlkBFLoader, MBFLoad, THREAD_POOL, 1)
 TG_DEF(TLD_HTimer, TLD_HTimer, TIMER)
+TG_DEF(OMTProcCpuSampler, OmtProcCpuSampler, THREAD_POOL, 1)
 #endif
