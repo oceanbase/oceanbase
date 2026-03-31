0046df8cb7288b56e1f41d7303ee6e22919c2ce2
obdev <obdev@oceanbase.com>
2026-03-31 12:44:46 +0000

    [CP] [vector index] fix vector index memdata sync issues

--- src/share/vector_index/ob_plugin_vector_index_adaptor.cpp
+++ src/share/vector_index/ob_plugin_vector_index_adaptor.cpp
@@ -3938,13 +3938,13 @@ int ObPluginVectorIndexAdaptor::check_need_sync_to_follower_or_do_opt_task(ObPlu
         || current_bitmap_count > follower_sync_statistics_.vbitmap_count_ + VEC_INDEX_INCR_DATA_SYNC_THRESHOLD
         || current_snapshot_count != follower_sync_statistics_.snap_count_) { // use scn_ in memdata for compare
       need_sync = true;
-      LOG_INFO("need sync to follower",
-        K(follower_sync_statistics_), K(current_incr_count), K(current_bitmap_count),
-        K(current_snapshot_count), KPC(this));
+      LOG_INFO("vector index memdata sync need trigger",
+        K(tenant_id_), K(data_tablet_id_), K(data_table_id_),
+        K(follower_sync_statistics_), K(current_incr_count), K(current_bitmap_count), K(current_snapshot_count));
     } else {
-      LOG_DEBUG("not need sync to follower",
-        K(follower_sync_statistics_), K(current_incr_count), K(current_bitmap_count),
-        K(current_snapshot_count), KPC(this));
+      LOG_DEBUG("vector index memdata sync skip trigger",
+        K(tenant_id_), K(data_tablet_id_), K(data_table_id_),
+        K(follower_sync_statistics_), K(current_incr_count), K(current_bitmap_count), K(current_snapshot_count));
     }
     if (is_leader && OB_FAIL(check_can_sync_to_follower(mgr, current_snapshot_count, need_sync))) {
       LOG_WARN("fail to check can sync to follower", K(ret));
