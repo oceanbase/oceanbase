df5afcde47066327d8ad23bca8d5030934f785c0
chen-zirui-tomato <18107732576@163.com>
2026-04-12 16:23:40 +0000

    [CP] Disable streaming plans for complex connections.

--- src/sql/optimizer/ob_log_subplan_filter.cpp
+++ src/sql/optimizer/ob_log_subplan_filter.cpp
@@ -251,6 +251,7 @@ int ObLogSubPlanFilter::do_re_est_cost(EstimateCostInfo &param, double &card, do
   ObLogicalOperator *child = NULL;
   double sel = 1.0;
   ObSEArray<ObBasicCostInfo, 4> cost_infos;
+  bool complex_filter_flag = false;
   if (OB_ISNULL(get_plan()) || OB_ISNULL(child = get_child(ObLogicalOperator::first_child))
       || OB_UNLIKELY(param.need_parallel_ < 1 || param.need_batch_rescan_)) {
     ret = OB_ERR_UNEXPECTED;
@@ -262,9 +263,11 @@ int ObLogSubPlanFilter::do_re_est_cost(EstimateCostInfo &param, double &card, do
                                                              get_plan()->get_selectivity_ctx(),
                                                              get_filter_exprs(),
                                                              sel,
-                                                             get_plan()->get_predicate_selectivities()))) {
+                                                             get_plan()->get_predicate_selectivities(),
+                                                             &complex_filter_flag))) {
     LOG_WARN("failed to calc selectivity", K(ret));
-  } else if (sel <= OB_DOUBLE_EPSINON || param.need_row_count_ >= child->get_card() * sel) {
+  } else if (sel <= OB_DOUBLE_EPSINON || param.need_row_count_ >= child->get_card() * sel
+    || (ObEnableOptRowGoal::AUTO == get_plan()->get_optimizer_context().get_enable_opt_row_goal() && complex_filter_flag)) {
     param.need_row_count_ = -1;
   } else {
     param.need_row_count_ /= sel;
