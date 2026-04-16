f2a41e2d5a4a71b8d6e2681becd20078b6c79ff2
JLY2015 <1623359870@qq.com>
2026-04-16 01:50:51 +0000

    [vector index] fix rebuild old index error -4007

--- src/rootserver/ob_index_builder.cpp
+++ src/rootserver/ob_index_builder.cpp
@@ -766,7 +766,8 @@ int ObIndexBuilder::submit_build_index_task(
   } else if ((index_schema->is_rowkey_doc_id() || index_schema->is_vec_rowkey_vid_type()) && new_fetched_snapshot <= 0) {
     ret = OB_ERR_UNEXPECTED;
     LOG_WARN("the fts/vec snapshot version should be more than zero", K(ret), K(new_fetched_snapshot));
-  } else if (data_schema->is_auto_partitioned_table()
+  } else if (!create_index_arg.is_rebuild_index_ && //  The rebuild target is to rebuild the old vector indexes. The old version supports creating vector indexes on auto partition tables, so we need to keep the behavior consistent with the old index behavior here.
+             data_schema->is_auto_partitioned_table()
       && OB_FAIL(ObSplitPartitionHelper::check_split_supported_index(*data_schema, *index_schema))) {
     LOG_WARN("failed to check split supported index", K(ret), KPC(index_schema));
   } else {
