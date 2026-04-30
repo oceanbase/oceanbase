e9cfd5a8622a947268876bcefa079e308bdcaa30
liucc1997 <1192520566@qq.com>
2026-04-30 03:21:28 +0000

    placeholder for SANDBOX_LOCK

--- deps/oblib/src/lib/stat/ob_latch_define.h
+++ deps/oblib/src/lib/stat/ob_latch_define.h
@@ -994,7 +994,8 @@ LATCH_DEF(OB_INSPECTION_SERVICE_LOCK, 962, "ob inspection service lock", LATCH_F
 LATCH_DEF(OB_SCHEMA_HISTORY_RECYCLE_SERVICE_LOCK, 963, "ob schema history recycle service lock", LATCH_FIFO, 2000, 0, true)
 LATCH_DEF(FT_DICT_BUILD_SLOTS_LOCK, 964, "fulltext dict build slots lock", LATCH_FIFO, 2000, 0, true)
 LATCH_DEF(FT_DICT_ROW_SCN_CACHE_LOCK, 965, "fulltext dict row scn cache lock", LATCH_FIFO, 2000, 0, true)
-LATCH_DEF(LATCH_END, 966, "latch end", LATCH_FIFO, 2000, 0, true)
+LATCH_DEF(SANDBOX_LOCK, 966, "ob_sandbox and sandbox manager lock", LATCH_FIFO, 2000, 0, true)
+LATCH_DEF(LATCH_END, 967, "latch end", LATCH_FIFO, 2000, 0, true)
 
 #endif
 
