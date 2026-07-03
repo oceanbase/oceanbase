e058f6f17a6eb308ddb40d415ca742faa3ec674a
helloamateur <magiccheery@163.com>
2026-07-03 13:21:33 +0000

    [CP] fix colletion type default value

--- deps/oblib/src/common/object/ob_object.h
+++ deps/oblib/src/common/object/ob_object.h
@@ -1489,7 +1489,8 @@ public:
   inline void reset();
   //when in not strict sql mode, build default value refer to data type
   // @string_cs_type: default value of string type need set collation type, since the space will be trim when compaction, see ObStorageSchema::trim
-  int build_not_strict_default_value(int16_t precision, const ObCollationType string_cs_type);
+  int build_not_strict_default_value(int16_t precision, const ObCollationType string_cs_type,
+                                     bool is_collection_vector_type = false);
   static ObObj make_min_obj();
   static ObObj make_max_obj();
   static ObObj make_nop_obj();
