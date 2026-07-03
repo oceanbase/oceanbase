c225b89f1291f73b53310a9a63e90c9e8b328833
juson1994 <547085379@qq.com>
2026-07-03 20:58:56 +0000

    fix 4377 bugs for vector index query after delete and switch leader

--- src/share/vector_index/ob_plugin_vector_index_utils.h
+++ src/share/vector_index/ob_plugin_vector_index_utils.h
@@ -255,6 +255,11 @@ private:
                                        SCN &target_scn,
                                        ObIAllocator &allocator,
                                        ObPluginVectorIndexTaskCtx *task_ctx = nullptr);
+  static int create_refresh_adaptor(ObLSID &ls_id,
+                                    ObPluginVectorIndexService *vector_index_service,
+                                    ObPluginVectorIndexAdaptor *old_adapter,
+                                    ObPluginVectorIndexAdaptor *&new_adapter,
+                                    void *&adpt_buff);
   static int find_segment_in_old_meta(ObVectorIndexSegmentMeta &new_seg_meta,
                                       const ObVectorIndexMeta &old_meta);
   static int fill_mem_context_detail_info(ObPluginVectorIndexService *service, ObIArray<ObLSTabletPair> &tablet_ids, char *buf, int64_t buf_len, int64_t &pos);
