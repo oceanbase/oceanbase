1614112feb0f0190a259a7f7e687bebbd52f7009
obdev <obdev@oceanbase.com>
2026-08-31 21:30:17 +0000

    修复 create tenant 内加锁顺序与非 DDL 线程的unit运维操作不一致导致的死锁问题

--- src/share/ob_unit_table_operator.cpp
+++ src/share/ob_unit_table_operator.cpp
@@ -1122,6 +1122,29 @@ int ObUnitTableOperator::get_units_by_resource_pools(
   return ret;
 }
 
+int ObUnitTableOperator::get_all_servers_by_pool_names(
+    const common::ObIArray<share::ObResourcePoolName> &pool_names,
+    common::ObIArray<common::ObAddr> &servers)
+{
+  int ret = OB_SUCCESS;
+  ObArray<ObUnit> units;
+  servers.reset();
+  if (OB_UNLIKELY(pool_names.empty())) {
+    ret = OB_INVALID_ARGUMENT;
+    LOG_WARN("invalid empty pool names", KR(ret), K(pool_names));
+  } else if (OB_FAIL(get_units_by_resource_pools(pool_names, units))) {
+    LOG_WARN("fail to get units by resource pools", KR(ret), K(pool_names));
+  } else {
+    for (int64_t i = 0; OB_SUCC(ret) && i < units.count(); ++i) {
+      const ObUnit &unit = units.at(i);
+      if (OB_FAIL(servers.push_back(unit.server_))) {
+        LOG_WARN("fail to push back server", KR(ret), K(unit));
+      }
+    }
+  }
+  return ret;
+}
+
 // what is_strict means?
 // it means need to get all the server has the tenant's unit.
 // when a unit just finished migrate from A server to B server,
