d9f636b2fd3bcf9b690376767feb1424aa2bb741
hanr881 <1741282579@qq.com>
2026-08-31 18:18:34 +0000

    to issue<2026071700117519137>:use standalone schema guard when executing ddl using pl

--- src/sql/ob_spi.cpp
+++ src/sql/ob_spi.cpp
@@ -2021,6 +2021,7 @@ int ObSPIService::spi_inner_execute(ObPLExecCtx *ctx,
       stmt::StmtType stmt_type = static_cast<stmt::StmtType>(type);
       ObString sqlstr(sql);
       lib::MemoryContext mem_context = nullptr;
+      bool is_ddl_stmt = ObStmt::is_ddl_stmt(stmt_type, true);
 
       if (is_forall && !session->is_enable_batched_multi_statement()) {
         /* forall need rollback to for loop */
@@ -2057,7 +2058,7 @@ int ObSPIService::spi_inner_execute(ObPLExecCtx *ctx,
               *(ctx->exec_ctx_), *(session), *spi_result, audit_record, ret, (sql != NULL ? sql : ps_sql), retry_ctrl, trace_id_guard, static_cast<stmt::StmtType>(type));
             OZ (spi_result->init_result_set(*session));
 
-            ObSPIRetryCtrlGuard retry_guard(retry_ctrl, *spi_result, *session, ctx, ret, is_retry);
+            ObSPIRetryCtrlGuard retry_guard(retry_ctrl, *spi_result, *session, ctx, ret, is_retry, !is_ddl_stmt);
 
             OX (session->get_pl_sqlcode_info()->set_sqlcode(OB_SUCCESS));
 
@@ -2124,21 +2125,21 @@ int ObSPIService::spi_inner_execute(ObPLExecCtx *ctx,
 
 
       if (OB_SUCC(ret)
-          && (ObStmt::is_ddl_stmt(stmt_type, true)
+          && (is_ddl_stmt
               || ObStmt::is_tcl_stmt(stmt_type)
               || (lib::is_mysql_mode() && session->get_local_autocommit()))) {
         OX (session->set_pl_can_retry(false));
       }
-      if (OB_SUCC(ret) && ObStmt::is_ddl_stmt(stmt_type, true)) {
+      if (OB_SUCC(ret) && is_ddl_stmt) {
         CK (OB_NOT_NULL(session->get_pl_top_context()));
         OX (session->get_pl_top_context()->set_disable_pl_exec_cache(true));
       }
 
       if (OB_SUCC(ret) && 0 == param_count) {
-        if (ObStmt::is_ddl_stmt(stmt_type, true)
+        if (is_ddl_stmt
             || ObStmt::is_tcl_stmt(stmt_type)
             || ObStmt::is_savepoint_stmt(stmt_type)) {
-          if (ObStmt::is_ddl_stmt(stmt_type, true)) {
+          if (is_ddl_stmt) {
             OZ (force_refresh_schema(session->get_effective_tenant_id()), sql);
           }
           recreate_implicit_savapoint_if_need(ctx, ret);
