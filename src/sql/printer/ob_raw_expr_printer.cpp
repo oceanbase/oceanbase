5d571c6f7237a31b445b77a0ccae78ebaf0c9d66
PatZhuang <pat_zhuang@outlook.com>
2026-09-01 07:35:41 +0000

    [CP] fix: skip printing expr name of `inner_row_cmp_value` in expr printer

--- src/sql/printer/ob_raw_expr_printer.cpp
+++ src/sql/printer/ob_raw_expr_printer.cpp
@@ -3697,7 +3697,6 @@ int ObRawExprPrinter::print(ObSysFunRawExpr *expr)
       }
       case T_FUN_SYS_INNER_ROW_CMP_VALUE: {
         CK(3 == expr->get_param_count());
-        DATA_PRINTF("inner_row_cmp_value");
         PRINT_EXPR(expr->get_param_expr(2));
         break;
       }
