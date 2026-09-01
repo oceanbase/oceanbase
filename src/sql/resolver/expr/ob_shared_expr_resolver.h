d29c24fd2c0b55c47fd68f5125b5856e2c1d1923
PatZhuang <pat_zhuang@outlook.com>
2026-09-01 07:31:06 +0000

    [CP] fix: disable expr sharing for non-deterministic exprs

--- src/sql/resolver/expr/ob_shared_expr_resolver.h
+++ src/sql/resolver/expr/ob_shared_expr_resolver.h
@@ -89,15 +89,16 @@ private:
                             ObRawExpr *&new_expr);
   inline bool is_blacklist_share_expr(const ObRawExpr &expr)
   {
-    return expr.is_column_ref_expr() ||
-           expr.is_aggr_expr() ||
-           expr.is_win_func_expr() ||
-           expr.is_query_ref_expr() ||
-           expr.is_exec_param_expr() ||
-           expr.is_pseudo_column_expr() ||
-           expr.get_expr_type() == T_OP_ROW ||
-           expr.get_expr_type() == T_QUESTIONMARK ||
-           expr.is_var_expr();
+    return expr.is_column_ref_expr()
+           || expr.is_aggr_expr()
+           || expr.is_win_func_expr()
+           || expr.is_query_ref_expr()
+           || expr.is_exec_param_expr()
+           || expr.is_pseudo_column_expr()
+           || expr.get_expr_type() == T_OP_ROW
+           || expr.get_expr_type() == T_QUESTIONMARK
+           || expr.is_var_expr()
+           || !expr.check_is_deterministic_expr();
   }
   inline bool is_blacklist_share_child(const ObRawExpr &expr)
   {
