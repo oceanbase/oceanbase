299a8b71ae43ccfd75241ce535f572ed62fd3201
LeonChaoHi <15201161716@163.com>
2026-09-08 07:28:54 +0000

    Wake up transfer service after transaction commit

--- src/rootserver/ob_balance_task_execute_service.h
+++ src/rootserver/ob_balance_task_execute_service.h
@@ -79,7 +79,8 @@ private:
   int process_current_task_status_(const share::ObBalanceTask &task,
                                    const share::ObBalanceJob &job,
                                    ObMySQLTransaction &trans,
-                                   bool &skip_next_status);
+                                   bool &skip_next_status,
+                                   bool &need_wakeup_transfer_service);
   int cancel_current_task_status_(const share::ObBalanceTask &task,
                                   const share::ObBalanceJob &job,
                                   ObMySQLTransaction &trans,
@@ -99,7 +100,8 @@ private:
   int execute_transfer_in_trans_(const share::ObBalanceTask &task,
                                  const share::ObBalanceJob &job,
                                  ObMySQLTransaction &trans,
-                                 bool &all_part_transferred);
+                                 bool &all_part_transferred,
+                                 bool &need_wakeup_transfer_service);
   int get_and_update_merge_ls_part_list_(
       ObMySQLTransaction &trans,
       const share::ObBalanceTask &task,
