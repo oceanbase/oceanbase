438422249f790d06b4876a8581fc5f361cd77aa3
obdev <obdev@oceanbase.com>
2026-09-08 20:24:33 +0000

    [CP] 支持使用opt_param hint修改配置项_enable_px_task_rebalance

--- src/sql/resolver/dml/ob_hint.cpp
+++ src/sql/resolver/dml/ob_hint.cpp
@@ -957,7 +957,8 @@ bool ObOptParamHint::is_param_val_valid(const OptParamType param_type, const ObO
     case ENABLE_MERGE_INTO:
     case ENABLE_PLAN_EXPIRATION_BY_EXEC_FEEDBACK:
     case VEC_BATCH_ACCUM_ENABLED:
-    case PRESERVE_ORDER_FOR_GROUPBY: {
+    case PRESERVE_ORDER_FOR_GROUPBY:
+    case _ENABLE_PX_TASK_REBALANCE: {
       is_valid = val.is_varchar() && (0 == val.get_varchar().case_compare("true")
                                       || 0 == val.get_varchar().case_compare("false"));
       break;
--- src/sql/resolver/dml/ob_hint.h
+++ src/sql/resolver/dml/ob_hint.h
@@ -261,6 +261,7 @@ struct ObOptParamHint
     DEF(PARTITION_ORDERED,)                         \
     DEF(ENABLE_ADAPTIVE_AUTO_DOP,)                \
     DEF(EXTERNAL_TABLE_FILE_SIZE_LOAD_BALANCE,)    \
+    DEF(_ENABLE_PX_TASK_REBALANCE,)                \
 
 
   DECLARE_ENUM(OptParamType, opt_param, OPT_PARAM_TYPE_DEF, static);
