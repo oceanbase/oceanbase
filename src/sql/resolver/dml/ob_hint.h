7105abc797f3d5f53f67cecfdc9010866bf6fd5d
xianyu-w <707512433@qq.com>
2026-09-10 04:26:30 +0000

    Raise optimizer_features_enable default to 4.4.2.4

--- src/sql/resolver/dml/ob_hint.h
+++ src/sql/resolver/dml/ob_hint.h
@@ -503,7 +503,8 @@ struct ObGlobalHint {
 #define COMPAT_VERSION_4_4_2_BP1  (oceanbase::common::cal_version(4, 4, 2, 1))
 #define COMPAT_VERSION_4_4_2_BP2  (oceanbase::common::cal_version(4, 4, 2, 2))
 #define COMPAT_VERSION_4_4_2_BP3  (oceanbase::common::cal_version(4, 4, 2, 3))
-#define LASTED_COMPAT_VERSION     COMPAT_VERSION_4_4_2_BP3
+#define COMPAT_VERSION_4_4_2_BP4  (oceanbase::common::cal_version(4, 4, 2, 4))
+#define LASTED_COMPAT_VERSION     COMPAT_VERSION_4_4_2_BP4
   static bool is_valid_opt_features_version(uint64_t version)
   { return COMPAT_VERSION_4_0 <= version && (LASTED_COMPAT_VERSION >= version || CLUSTER_CURRENT_VERSION >= version); }
 
