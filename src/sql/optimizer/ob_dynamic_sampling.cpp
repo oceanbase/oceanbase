e633d48483b693ef4edf239b6fb3039acb9c0390
xianyu-w <707512433@qq.com>
2026-09-18 10:25:10 +0000

    [CP] fix cardinality estimation bugs

--- src/sql/optimizer/ob_dynamic_sampling.cpp
+++ src/sql/optimizer/ob_dynamic_sampling.cpp
@@ -806,6 +806,8 @@ int ObDynamicSampling::add_basic_hint_info(ObSqlString &basic_hint_str,
     LOG_WARN("failed to append", K(ret));
   } else if (OB_FAIL(basic_hint_str.append(" OPT_PARAM('APPROX_COUNT_DISTINCT_PRECISION', 10) "))) {
     LOG_WARN("failed to append", K(ret));
+  } else if (OB_FAIL(basic_hint_str.append(" OPT_PARAM('_ENABLE_STORAGE_CARDINALITY_ESTIMATION', 'FALSE') "))) {
+    LOG_WARN("failed to append", K(ret));
   } else if (OB_FAIL(basic_hint_str.append("*/"))) {//hint end
     LOG_WARN("failed to append", K(ret));
   } else {
